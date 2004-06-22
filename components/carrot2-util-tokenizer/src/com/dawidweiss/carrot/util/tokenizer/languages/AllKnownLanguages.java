0095955a0d2e5c8c5b9d11ebb5fd8b45e3627296
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2004-06-22 10:57:55 +0000

    The array of languages is now static. Added an ISO code lookup method.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@225 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- components/carrot2-util-tokenizer/src/com/dawidweiss/carrot/util/tokenizer/languages/AllKnownLanguages.java
+++ components/carrot2-util-tokenizer/src/com/dawidweiss/carrot/util/tokenizer/languages/AllKnownLanguages.java
@@ -1,10 +1,10 @@
 /*
- * Created on 2004-04-02
- *
- * To change the template for this generated file go to
+ * Created on 2004-04-02 To change the template for this generated file go to
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
 package com.dawidweiss.carrot.util.tokenizer.languages;
+
+import java.util.*;
 
 import com.dawidweiss.carrot.core.local.linguistic.Language;
 import com.dawidweiss.carrot.util.tokenizer.languages.dutch.Dutch;
@@ -15,30 +15,66 @@ import com.dawidweiss.carrot.util.tokenizer.languages.italian.Italian;
 import com.dawidweiss.carrot.util.tokenizer.languages.polish.Polish;
 import com.dawidweiss.carrot.util.tokenizer.languages.spanish.Spanish;
 
-
 /**
- * A factory that allows access to all known languages
- * implemented in this module.
+ * A factory that allows access to all known languages implemented in this
+ * module.
  * 
  * @author Dawid Weiss
  * @version $Revision$
  */
-public class AllKnownLanguages {
+public class AllKnownLanguages
+{
+
+    /** All known languages */
+    private static Language [] languageArray;
+
+    /** A mapping between ISO codes and Language instances */
+    private static Map languages;
+
+    /** Initialize the data */
+    static
+    {
+        languageArray = new Language[]
+        { 
+         	new English(), 
+         	new Polish(), 
+         	new Dutch(), 
+         	new French(), 
+         	new German(),
+         	new Italian(), 
+         	new Spanish() 
+        };
+
+        languages = new HashMap();
+        for (int i = 0; i < languageArray.length; i++)
+        {
+            languages.put(languageArray[i].getIsoCode(), languageArray[i]);
+        }
+    }
 
     /** Disallow instantiation */
-    private AllKnownLanguages() {
+    private AllKnownLanguages()
+    {
     }
-    
-    public static final Language [] getLanguages() {
-        return new Language [] {
-                new English(),
-                new Polish(),
-                new Dutch(),
-                new French(),
-                new German(),
-                new Italian(),
-                new Spanish()
-        };
+
+    /**
+     * @return
+     */
+    public static final Language [] getLanguages()
+    {
+        return languageArray;
     }
-    
+
+    /**
+     * Returns a {@link Language}instance for the given ISO code. If no known
+     * language corresponds to the ISO code <code>null</code> will be
+     * returned.
+     * 
+     * @param isoCode
+     * @return a {@link Language}instance for the given ISO code
+     */
+    public static final Language getLanguageForIsoCode(String isoCode)
+    {
+        return (Language) languages.get(isoCode);
+    }
 }
