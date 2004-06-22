e3d3cc11de1a644655ca6afa0b6da5a9e160ddb6
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2004-06-22 16:00:18 +0000

    Added support for counting and storing original versions of stemmed tokens.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@235 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/components/carrot2-filter-lingo-reloaded/src/com/stachoodev/carrot/filter/lingo/model/ModelUtils.java
+++ carrot2/components/carrot2-filter-lingo-reloaded/src/com/stachoodev/carrot/filter/lingo/model/ModelUtils.java
@@ -11,6 +11,7 @@ import org.apache.commons.collections.*;
 import org.apache.commons.collections.map.*;
 
 import com.dawidweiss.carrot.core.local.linguistic.tokens.*;
+import com.dawidweiss.carrot.util.common.*;
 
 /**
  * @author stachoo
@@ -23,8 +24,8 @@ public class ModelUtils
      * @param frequencyMap
      */
     public static void addToFrequencyMap(TokenSequence tokenSequence,
-            String propertyName, Map frequencyMap, double multiplier,
-            Set tokensUsed, short filterMask)
+        String propertyName, Map frequencyMap, double multiplier,
+        Set tokensUsed, short filterMask)
     {
         // Count token frequencies
         for (int i = 0; i < tokenSequence.getLength(); i++)
@@ -48,19 +49,73 @@ public class ModelUtils
                 continue;
             }
 
+            // Support for document-frequency counting
             if (tokensUsed != null && tokensUsed.contains(stem))
             {
+                // If we have not seen that variant before - store it
+                if (!tokensUsed.contains(token.toString()))
+                {
+                    tokensUsed.add(token.toString());
+
+                    // Stemmed token
+                    ExtendedToken originalExtendedToken;
+                    Map originalExtendedTokens = (Map) ((ExtendedToken) frequencyMap
+                        .get(stem))
+                        .getProperty(ExtendedToken.PROPERTY_ORIGINAL_TOKENS);
+                    if (!originalExtendedTokens.containsKey(token.toString()))
+                    {
+                        originalExtendedToken = new ExtendedToken(token);
+                        originalExtendedToken.setDoubleProperty(propertyName,
+                            multiplier);
+                        originalExtendedTokens.put(token.toString(),
+                            originalExtendedToken);
+                    }
+                    else
+                    {
+                        originalExtendedToken = (ExtendedToken) (originalExtendedTokens)
+                            .get(token.toString());
+                        originalExtendedToken.setDoubleProperty(propertyName,
+                            originalExtendedToken
+                                .getDoubleProperty(propertyName)
+                                + multiplier);
+                    }
+                }
+
                 continue;
             }
 
             if (frequencyMap.containsKey(stem))
             {
+                // Stemmed token
                 ExtendedToken extendedToken = (ExtendedToken) frequencyMap
-                        .get(stem);
+                    .get(stem);
 
+                // Increase the frequency of the stemmed token
                 extendedToken.setDoubleProperty(propertyName, extendedToken
-                        .getDoubleProperty(propertyName)
-                        + multiplier);
+                    .getDoubleProperty(propertyName)
+                    + multiplier);
+
+                // Add/increase frequency of the original token
+                ExtendedToken originalExtendedToken;
+                Map originalExtendedTokens = (Map) extendedToken
+                    .getProperty(ExtendedToken.PROPERTY_ORIGINAL_TOKENS);
+                if (!originalExtendedTokens.containsKey(token.toString()))
+                {
+                    originalExtendedToken = new ExtendedToken(token);
+                    originalExtendedToken.setDoubleProperty(propertyName,
+                        multiplier);
+                    originalExtendedTokens.put(token.toString(),
+                        originalExtendedToken);
+                }
+                else
+                {
+                    originalExtendedToken = (ExtendedToken) (originalExtendedTokens)
+                        .get(token.toString());
+                    originalExtendedToken.setDoubleProperty(propertyName,
+                        originalExtendedToken
+                            .getDoubleProperty(propertyName)
+                            + multiplier);
+                }
             }
             else
             {
@@ -69,6 +124,17 @@ public class ModelUtils
                 extendedToken = new ExtendedToken(new TokenStem(token));
                 extendedToken.setDoubleProperty(propertyName, multiplier);
                 frequencyMap.put(stem, extendedToken);
+
+                // Create a temporary hash map for counting original terms
+                Map originalExtendedTokens = new HashMap();
+                ExtendedToken originalExtendedToken = new ExtendedToken(token);
+                originalExtendedToken.setDoubleProperty(propertyName,
+                    multiplier);
+                originalExtendedTokens.put(token.toString(),
+                    originalExtendedToken);
+                extendedToken.setProperty(
+                    ExtendedToken.PROPERTY_ORIGINAL_TOKENS,
+                    originalExtendedTokens);
             }
 
             if (tokensUsed != null)
@@ -83,10 +149,10 @@ public class ModelUtils
      * @param frequencyMap
      */
     public static void addToFrequencyMap(TokenSequence tokenSequence,
-            String propertyName, Map frequencyMap, double multiplier)
+        String propertyName, Map frequencyMap, double multiplier)
     {
         addToFrequencyMap(tokenSequence, propertyName, frequencyMap,
-                multiplier, null, (short) 0);
+            multiplier, null, (short) 0);
     }
 
     /**
@@ -101,5 +167,57 @@ public class ModelUtils
         OrderedMap orderedMap = new LinkedMap();
 
         return orderedMap;
+    }
+
+    /**
+     * @param tokenFrequencies
+     * @return
+     */
+    public static List frequencyMapToList(HashMap tokenFrequencies,
+        String frequencyProperty, double frequencyThreshold)
+    {
+        // Create the final list
+        ArrayList list = new ArrayList(tokenFrequencies.values());
+        Comparator comparator = PropertyHelper.getComparatorForDoubleProperty(
+            frequencyProperty, true);
+        Collections.sort(list, comparator);
+
+        // A fake ExtendedToken we will use to binary-search the token list
+        ExtendedToken thresholdToken = new ExtendedToken(null);
+        thresholdToken.setDoubleProperty(frequencyProperty, frequencyThreshold);
+
+        // NB: binarySearch requires that the list be sorted ascendingly, but
+        // as long the supplied comparator is consistent with the list's
+        // ordering the method will work as expected
+        int index = Collections.binarySearch(list, thresholdToken, comparator);
+
+        if (index < 0)
+        {
+            index = -index;
+        }
+        else
+        {
+            while (((ExtendedToken) list.get(index))
+                .getDoubleProperty(frequencyProperty) >= frequencyThreshold)
+            {
+                index++;
+            }
+        }
+
+        List finalList = list.subList(0, index - 1);
+
+        // Convert original token hash maps into sorted lists
+        for (Iterator tokens = finalList.iterator(); tokens.hasNext();)
+        {
+            ExtendedToken token = (ExtendedToken) tokens.next();
+            Map originalTokenMap = (Map) token
+                .getProperty(ExtendedToken.PROPERTY_ORIGINAL_TOKENS);
+            List tokenList = new ArrayList(originalTokenMap.values());
+            Collections.sort(tokenList, comparator);
+            token
+                .setProperty(ExtendedToken.PROPERTY_ORIGINAL_TOKENS, tokenList);
+        }
+
+        return finalList;
     }
 }
