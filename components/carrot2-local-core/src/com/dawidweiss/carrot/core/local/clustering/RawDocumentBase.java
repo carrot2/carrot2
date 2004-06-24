3a2d9f7919d86098b6414e4f662f20a95ee8f68e
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2004-06-24 19:37:27 +0000

    Refactor: PropertyHelper is used now, PropertyProvider interface implemented.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@251 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- components/carrot2-local-core/src/com/dawidweiss/carrot/core/local/clustering/RawDocumentBase.java
+++ components/carrot2-local-core/src/com/dawidweiss/carrot/core/local/clustering/RawDocumentBase.java
@@ -1,104 +1,119 @@
-
 /*
- * Carrot2 Project
- * Copyright (C) 2002-2004, Dawid Weiss
- * Portions (C) Contributors listed in carrot2.CONTRIBUTORS file.
- * All rights reserved.
- *
- * Refer to the full license file "carrot2.LICENSE"
- * in the root folder of the CVS checkout or at:
- * http://www.cs.put.poznan.pl/dweiss/carrot2.LICENSE
- *
- * Sponsored by: CCG, Inc.
+ * Carrot2 Project Copyright (C) 2002-2004, Dawid Weiss Portions (C)
+ * Contributors listed in carrot2.CONTRIBUTORS file. All rights reserved. Refer
+ * to the full license file "carrot2.LICENSE" in the root folder of the CVS
+ * checkout or at: http://www.cs.put.poznan.pl/dweiss/carrot2.LICENSE Sponsored
+ * by: CCG, Inc.
  */
 
 package com.dawidweiss.carrot.core.local.clustering;
 
-import java.io.Reader;
-
-import java.util.HashMap;
-
+import com.dawidweiss.carrot.util.common.*;
 
 /**
  * An abstract implementation of some of the basic methods of the {@link
  * RawDocument} interface.
  * 
  * @author Dawid Weiss
+ * @author stachoo
  * @version $Revision$
  */
-public abstract class RawDocumentBase implements RawDocument {
+public abstract class RawDocumentBase implements RawDocument, PropertyProvider
+{
+    /** Stores this document's properties */
+    protected PropertyHelper propertyHelper;
+
     /**
-     * Lazily initialized hashmap with properties of this document.
+     * Initializes the internal property storage.
      */
-    private HashMap properties;
+    public RawDocumentBase()
+    {
+        propertyHelper = new PropertyHelper();
+    }
     
-    /**
-     * Provides a template of implementation of the named property getter.
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.dawidweiss.carrot.core.local.clustering.RawDocument#getSnippet()
      */
-    public Object getProperty(String name) {
-        // the synchronized block should be before 'properties != null'
-        // for 100% correct concurrent solution... but it seems unlikely
-        // that pointer access would cause us much trouble here.
-        if (properties != null) {
-            synchronized (this) {
-                Object value = properties.get(name);
+    public String getSnippet()
+    {
+        return (String) propertyHelper.getProperty(PROPERTY_SNIPPET);
+    }
 
-                if (value != null) {
-                    return value;
-                }
-            }
-        }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.dawidweiss.carrot.core.local.clustering.RawDocument#getUrl()
+     */
+    public String getUrl()
+    {
+        return (String) propertyHelper.getProperty(PROPERTY_URL);
+    }
 
-        if (RawDocument.PROPERTY_URL.equals(name)) {
-            return getUrl();
-        } else if (RawDocument.PROPERTY_SNIPPET.equals(name)) {
-            return getSnippet();
-        } else if (RawDocument.PROPERTY_CONTENT_READER.equals(name)) {
-        	return getContent();
-        } else {
-            return null;
-        }
+    /* (non-Javadoc)
+     * @see com.dawidweiss.carrot.util.common.PropertyProvider#getProperty(java.lang.String)
+     */
+    public Object getProperty(String name)
+    {
+        return propertyHelper.getProperty(name);
+    }
+
+    /* (non-Javadoc)
+     * @see com.dawidweiss.carrot.util.common.PropertyProvider#setProperty(java.lang.String, java.lang.Object)
+     */
+    public Object setProperty(String propertyName, Object value)
+    {
+        return propertyHelper.setProperty(propertyName, value);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.dawidweiss.carrot.util.common.PropertyProvider#getDoubleProperty(java.lang.String)
+     */
+    public double getDoubleProperty(String propertyName)
+    {
+        return propertyHelper.getDoubleProperty(propertyName);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.dawidweiss.carrot.util.common.PropertyProvider#getIntProperty(java.lang.String)
+     */
+    public int getIntProperty(String propertyName)
+    {
+        return propertyHelper.getIntProperty(propertyName);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.dawidweiss.carrot.util.common.PropertyProvider#setDoubleProperty(java.lang.String,
+     *      double)
+     */
+    public Object setDoubleProperty(String propertyName, double value)
+    {
+        return propertyHelper.setDoubleProperty(propertyName, value);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.dawidweiss.carrot.util.common.PropertyProvider#setIntProperty(java.lang.String,
+     *      int)
+     */
+    public Object setIntProperty(String propertyName, int value)
+    {
+        return propertyHelper.setIntProperty(propertyName, value);
     }
 
     /**
-     * @return Should return a reader to the contents of this document. The
-     *         default implementation returns <code>null</code>.
-	 */
-	private Reader getContent() {
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.dawidweiss.carrot.core.local.clustering.RawDocument#getSnippet()
-	 */
-	public String getSnippet() {
-		if (properties != null)
-		{
-		    return (String) properties.get(PROPERTY_SNIPPET);
-		}
-		else
-		{
-		    return null;
-		}
-	}
-
-	/**
-     * Sets a value for a named property in this document.
+     * @return Returns 'no score' constant.
      */
-    public Object setProperty(String propertyName, Object value) {
-        synchronized (this) {
-            if (properties == null) {
-                properties = new HashMap();
-            }
-
-            return properties.put(propertyName, value);
-        }
-    }
-    
-    /**
-     * @return Returns 'no score' constant. 
-     */
-    public float getScore() {
+    public float getScore()
+    {
         return -1;
     }
 }
