a2b9b1a87652958439d8baac766f285ad028912e
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2004-06-24 19:27:37 +0000

    DEFAULT_FILTER_MASK moved here.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@243 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/components/carrot2-filter-lingo-reloaded/src/com/stachoodev/carrot/filter/lingo/model/FeatureSelectionStrategy.java
+++ carrot2/components/carrot2-filter-lingo-reloaded/src/com/stachoodev/carrot/filter/lingo/model/FeatureSelectionStrategy.java
@@ -5,6 +5,8 @@ package com.stachoodev.carrot.filter.lingo.model;
 
 import java.util.*;
 
+import com.dawidweiss.carrot.core.local.linguistic.tokens.*;
+
 /**
  * Defines the interface of an algorithm performing feature selection.
  * 
@@ -12,6 +14,15 @@ import java.util.*;
  */
 public interface FeatureSelectionStrategy
 {
+    /**
+     * Defines token types that will be skipped during the selection process.
+     * These are: stop words, symbols, punctuation marks and tokens of an
+     * unknown type.
+     */
+    public static short DEFAULT_FILTER_MASK = TypedToken.TOKEN_FLAG_STOPWORD
+        | TypedToken.TOKEN_TYPE_SYMBOL | TypedToken.TOKEN_TYPE_UNKNOWN
+        | TypedToken.TOKEN_TYPE_PUNCTUATION;
+
     /**
      * Returns a list of {@link ExtendedToken}s selected by the algorithm.
      * Tokens must be sorted decreasingly according to the utility measure used
