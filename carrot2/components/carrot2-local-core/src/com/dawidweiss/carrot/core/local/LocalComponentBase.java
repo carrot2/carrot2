ed8ce25231298abaefefc3d09fc0200badd74a40
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2004-07-02 16:06:44 +0000

    Added: getName() and getDescription() methods
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@295 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/components/carrot2-local-core/src/com/dawidweiss/carrot/core/local/LocalComponentBase.java
+++ carrot2/components/carrot2-local-core/src/com/dawidweiss/carrot/core/local/LocalComponentBase.java
@@ -1,14 +1,10 @@
-
 /*
- * Carrot2 Project
- * Copyright (C) 2002-2004, Dawid Weiss
- * Portions (C) Contributors listed in carrot2.CONTRIBUTORS file.
- * All rights reserved.
- *
- * Refer to the full license file "carrot2.LICENSE"
- * in the root folder of the CVS checkout or at:
- * http://www.cs.put.poznan.pl/dweiss/carrot2.LICENSE
- *
+ * Carrot2 Project Copyright (C) 2002-2004, Dawid Weiss Portions (C)
+ * Contributors listed in carrot2.CONTRIBUTORS file. All rights reserved.
+ * 
+ * Refer to the full license file "carrot2.LICENSE" in the root folder of the
+ * CVS checkout or at: http://www.cs.put.poznan.pl/dweiss/carrot2.LICENSE
+ * 
  * Sponsored by: CCG, Inc.
  */
 
@@ -16,79 +12,92 @@ package com.dawidweiss.carrot.core.local;
 
 import java.util.Set;
 
-
 /**
- * A reference implementation of a {@link LocalComponent} interface. This class
+ * A reference implementation of a {@link LocalComponent}interface. This class
  * is abstract, but provides empty implementations of {@link
  * LocalComponent#init(LocalControllerContext)} and capabilities-related
- * methods. Some request context property conversion-related helper methods
- * are also provided.
- *
+ * methods. Some request context property conversion-related helper methods are
+ * also provided.
+ * 
  * @author Dawid Weiss
  * @version $Revision$
  */
-public abstract class LocalComponentBase implements LocalComponent {
+public abstract class LocalComponentBase implements LocalComponent
+{
     /**
      * Provides an empty implementation.
-     *
+     * 
      * @param context A controller context instance.
-     *
+     * 
      * @see LocalComponent#init(LocalControllerContext)
      */
     public void init(LocalControllerContext context)
-        throws InstantiationException {
+        throws InstantiationException
+    {
     }
 
     /**
-     * Provides an implementation that requires no  capabilities of the
-     * successor component.
+     * Provides an implementation that requires no capabilities of the successor
+     * component.
      */
-    public Set getRequiredSuccessorCapabilities() {
+    public Set getRequiredSuccessorCapabilities()
+    {
         return java.util.Collections.EMPTY_SET;
     }
 
     /**
-     * Provides an implementation that requires no  capabilities of the
+     * Provides an implementation that requires no capabilities of the
      * predecessor component.
      */
-    public Set getRequiredPredecessorCapabilities() {
+    public Set getRequiredPredecessorCapabilities()
+    {
         return java.util.Collections.EMPTY_SET;
     }
 
     /**
      * Provides an implementation that has no capabilities (an empty set).
      */
-    public Set getComponentCapabilities() {
+    public Set getComponentCapabilities()
+    {
         return java.util.Collections.EMPTY_SET;
     }
 
     /**
      * Returns an integer value of a named attribute <code>paramName</code>
-     * from the request context <code>context</code>.  If the request context
+     * from the request context <code>context</code>. If the request context
      * has no mapping for the named attribute, the <code>defaultValue</code>
-     * is returned.   If the conversion is not possible (because the value is
-     * not an integer number), the default value is returned.
-     *
+     * is returned. If the conversion is not possible (because the value is not
+     * an integer number), the default value is returned.
+     * 
      * @param context Request context used to look up the value of a named
-     *        attribute.
+     *            attribute.
      * @param paramName Name of the attribute.
      * @param defaultValue Default value returned if the attribute is not
-     *        available, or conversion failed.
-     *
+     *            available, or conversion failed.
+     * 
      * @return An integer value of the named parameter, or the default value.
      */
     protected final int getIntFromRequestContext(RequestContext context,
-        String paramName, int defaultValue) {
+        String paramName, int defaultValue)
+    {
         Object value = context.getRequestParameters().get(paramName);
 
-        if (value == null) {
+        if (value == null)
+        {
             return defaultValue;
-        } else {
-            try {
+        }
+        else
+        {
+            try
+            {
                 return Integer.parseInt((String) value);
-            } catch (NumberFormatException e) {
+            }
+            catch (NumberFormatException e)
+            {
                 return defaultValue;
-            } catch (ClassCastException e) {
+            }
+            catch (ClassCastException e)
+            {
                 return defaultValue;
             }
         }
@@ -100,6 +109,30 @@ public abstract class LocalComponentBase implements LocalComponent {
      * @see com.dawidweiss.carrot.core.local.LocalComponent#setProperty(java.lang.String,
      *      java.lang.String)
      */
-    public void setProperty(String key, String value) {
+    public void setProperty(String key, String value)
+    {
+    }
+
+    /**
+     * The default implementation returning <code>null</code>. In concrete
+     * implementations override this method to provide the appropriate name.
+     * 
+     * @return <code>null</code> name
+     */
+    public String getName()
+    {
+        return null;
+    }
+
+    /**
+     * The default implementation returning <code>null</code>. In concrete
+     * implementations override this method to provide the appropriate
+     * description.
+     * 
+     * @return <code>null</code> description
+     */
+    public String getDescription()
+    {
+        return null;
     }
 }
