46a9b32276dc289ffd91ba6d6decdc713bb53885
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2004-07-09 14:41:45 +0000

    Added: more tests
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@317 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- components/carrot2-filter-lingo-reloaded/src-test/com/stachoodev/carrot/filter/lingo/algorithm/LingoTest.java
+++ components/carrot2-filter-lingo-reloaded/src-test/com/stachoodev/carrot/filter/lingo/algorithm/LingoTest.java
@@ -1,5 +1,12 @@
 /*
- * LingoTest.java Created on 2004-06-23
+ * Carrot2 Project
+ * Copyright (C) 2002-2004, Dawid Weiss
+ * Portions (C) Contributors listed in carrot2.CONTRIBUTORS file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the CVS checkout or at:
+ * http://www.cs.put.poznan.pl/dweiss/carrot2.LICENSE
  */
 package com.stachoodev.carrot.filter.lingo.algorithm;
 
@@ -7,11 +14,15 @@ import java.util.*;
 
 import com.dawidweiss.carrot.core.local.clustering.*;
 import com.dawidweiss.carrot.util.tokenizer.*;
+import com.stachoodev.carrot.filter.lingo.model.*;
 
 import junit.framework.*;
 
 /**
- * @author stachoo
+ * Simple unit tests for the Lingo algorithm.
+ * 
+ * @author Stanislaw Osinski
+ * @version $Revision$
  */
 public class LingoTest extends TestCase
 {
@@ -30,7 +41,7 @@ public class LingoTest extends TestCase
     }
 
     /**
-     * 
+     *  
      */
     public void testNoClusters()
     {
@@ -47,12 +58,126 @@ public class LingoTest extends TestCase
         Lingo lingo = new Lingo();
         lingo.setCandidateLabelsCount(10);
         List clusters = lingo.cluster(documentList);
-        
+
         assertEquals("No clusters generated", 0, clusters.size());
     }
 
     /**
-     * 
+     *  
+     */
+    public void testIdfOmittedDocuments()
+    {
+        TokenizedDocument document00 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("", "value", "en"));
+        TokenizedDocument document01 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "large scale singular value computations", "en"));
+        TokenizedDocument document02 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "software library for the sparse singular value decomposition",
+                "en"));
+        TokenizedDocument document03 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "value introduction to modern information retrieval", "en"));
+        TokenizedDocument document04 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet(
+                "",
+                "using linear algebra for intelligent information value retrieval",
+                "en"));
+        TokenizedDocument document05 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "value of matrix computations", "en"));
+        TokenizedDocument document06 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "singular value analysis of cryptograms", "en"));
+        TokenizedDocument document07 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "automatic information value organization", "en"));
+
+        List documentList = Arrays.asList(new TokenizedDocument []
+        { document00, document01, document02, document03, document04,
+         document05, document06, document07 });
+
+        Map parameters = new HashMap();
+        parameters.put(Lingo.PARAMETER_TD_MATRIX_BUILDING_STRATEGY,
+            new TfIdfTdMatrixBuildingStrategy());
+
+        Lingo lingo = new Lingo(parameters);
+        lingo.setCandidateLabelsCount(2);
+        List clusters = lingo.cluster(documentList);
+
+        // Just check if the first document ended up in the Other Topics group
+        assertTrue("document00 in the Other Topics group",
+            ((RawCluster) clusters.get(clusters.size() - 1)).getDocuments()
+                .contains(
+                    document00
+                        .getProperty(TokenizedDocument.PROPERTY_RAW_DOCUMENT)));
+    }
+
+    /**
+     *  
+     */
+    public void testTfOmittedDocuments()
+    {
+        TokenizedDocument document00 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("", "library cryptograms", "en"));
+        TokenizedDocument document08 = snippetTokenizer
+        .tokenize(new RawDocumentSnippet("", "analysis decomposition", "en"));
+        TokenizedDocument document01 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "large scale singular value computations", "en"));
+        TokenizedDocument document02 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "software library for the sparse singular value decomposition",
+                "en"));
+        TokenizedDocument document03 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "introduction to modern information retrieval", "en"));
+        TokenizedDocument document04 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "using linear algebra for intelligent information retrieval",
+                "en"));
+        TokenizedDocument document05 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("", "matrix computations", "en"));
+        TokenizedDocument document06 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "singular value analysis of cryptograms", "en"));
+        TokenizedDocument document07 = snippetTokenizer
+            .tokenize(new RawDocumentSnippet("",
+                "automatic information organization", "en"));
+
+        List documentList = Arrays.asList(new TokenizedDocument []
+        { document01, document00, document02, document03, document04,
+         document05, document08, document06, document07 });
+
+        Map parameters = new HashMap();
+        parameters.put(Lingo.PARAMETER_TD_MATRIX_BUILDING_STRATEGY,
+            new TfIdfTdMatrixBuildingStrategy());
+
+        Lingo lingo = new Lingo(parameters);
+        lingo.setCandidateLabelsCount(2);
+        List clusters = lingo.cluster(documentList);
+
+        // Documents 00 08 05 in the Other Topics group
+        assertTrue("document00 in the Other Topics group",
+            ((RawCluster) clusters.get(clusters.size() - 1)).getDocuments()
+                .contains(
+                    document00
+                        .getProperty(TokenizedDocument.PROPERTY_RAW_DOCUMENT)));
+        assertTrue("document08 in the Other Topics group",
+            ((RawCluster) clusters.get(clusters.size() - 1)).getDocuments()
+                .contains(
+                    document08
+                        .getProperty(TokenizedDocument.PROPERTY_RAW_DOCUMENT)));
+        assertTrue("document05 in the Other Topics group",
+            ((RawCluster) clusters.get(clusters.size() - 1)).getDocuments()
+                .contains(
+                    document05
+                        .getProperty(TokenizedDocument.PROPERTY_RAW_DOCUMENT)));
+    }
+
+    /**
+     *  
      */
     public void testLabelDiscovery()
     {
@@ -87,10 +212,7 @@ public class LingoTest extends TestCase
         lingo.setCandidateLabelsCount(10);
         List clusters = lingo.cluster(documentList);
 
-        for (Iterator iter = clusters.iterator(); iter.hasNext();)
-        {
-            LingoRawCluster cluster = (LingoRawCluster) iter.next();
-            System.out.println(cluster.getFullInfo() + "\n");
-        }
+        // No more than three clusters generated
+        assertTrue("No more than three clusters", clusters.size() <= 3);
     }
 }
