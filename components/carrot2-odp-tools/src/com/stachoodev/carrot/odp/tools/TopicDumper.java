7654135610e311525bbc9d451b3852f28b9e6555
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2004-07-10 21:08:31 +0000

    Initiali checkin
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@340 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ components/carrot2-odp-tools/src/com/stachoodev/carrot/odp/tools/TopicDumper.java
@@ -0,0 +1,162 @@
+/*
+ * Carrot2 Project
+ * Copyright (C) 2002-2004, Dawid Weiss
+ * Portions (C) Contributors listed in carrot2.CONTRIBUTORS file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the CVS checkout or at:
+ * http://www.cs.put.poznan.pl/dweiss/carrot2.LICENSE
+ */
+package com.stachoodev.carrot.odp.tools;
+
+import java.io.*;
+import java.util.*;
+
+import com.stachoodev.carrot.odp.*;
+import com.stachoodev.carrot.odp.index.*;
+
+/**
+ * Dumps some information (catid, size, path) about ODP topics to a file.
+ * 
+ * @author Stanislaw Osinski
+ * @version $Revision$
+ */
+public class TopicDumper
+{
+    /**
+     * @param odpIndexDir
+     * @param outFileBase
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public void dump(String odpIndexDir, String outFileBase)
+        throws IOException, ClassNotFoundException
+    {
+        ODPIndex.initialize(odpIndexDir);
+        PrimaryTopicIndex primaryIndex = ODPIndex.getPrimaryTopicIndex();
+
+        List dump = new ArrayList();
+        int i = 0;
+        for (Iterator paths = primaryIndex.getAllLocations(); paths.hasNext();)
+        {
+            Topic topic = ODPIndex.getTopic((String) paths.next());
+            Object [] line = new Object [3];
+            line[0] = topic.getCatid();
+            line[1] = new Integer(topic.getExternalPages().size());
+            line[2] = topic.getId();
+
+            dump.add(line);
+
+            if (i % 500 == 0)
+            {
+                System.out.println(i + " topics dupmed");
+            }
+            i++;
+        }
+
+        // Sort by size, path
+        System.out.println("Sorting by size...");
+        Collections.sort(dump, new Comparator()
+        {
+            public boolean equals(Object obj)
+            {
+                return false;
+            }
+
+            public int compare(Object o1, Object o2)
+            {
+                Object [] l1 = (Object []) o1;
+                Object [] l2 = (Object []) o2;
+
+                if (((Integer) l1[1]).intValue() > ((Integer) l2[1]).intValue())
+                {
+                    return 1;
+                }
+                else if (((Integer) l1[1]).intValue() < ((Integer) l2[1])
+                    .intValue())
+                {
+                    return -1;
+                }
+                else
+                {
+                    return ((String) l1[2]).compareToIgnoreCase((String) l2[2]);
+                }
+            }
+        });
+
+        System.out.println("Writing...");
+        PrintStream out = new PrintStream(new FileOutputStream(outFileBase
+            + "-size"));
+        for (Iterator iter = dump.iterator(); iter.hasNext();)
+        {
+            Object [] line = (Object []) iter.next();
+            out.println(line[0].toString() + "\t" + line[1].toString() + "\t"
+                + line[2].toString());
+        }
+        out.close();
+
+        // Sort by size, path
+        System.out.println("Sorting by path...");
+        Collections.sort(dump, new Comparator()
+        {
+            public boolean equals(Object obj)
+            {
+                return false;
+            }
+
+            public int compare(Object o1, Object o2)
+            {
+                Object [] l1 = (Object []) o1;
+                Object [] l2 = (Object []) o2;
+
+                int result = ((String) l1[2])
+                    .compareToIgnoreCase((String) l2[2]);
+                if (result != 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    return ((Integer) l1[1]).compareTo(l2[1]);
+                }
+            }
+        });
+
+        System.out.println("Writing...");
+        out = new PrintStream(new FileOutputStream(outFileBase + "-path"));
+        for (Iterator iter = dump.iterator(); iter.hasNext();)
+        {
+            Object [] line = (Object []) iter.next();
+            out.println(line[0].toString() + "\t" + line[1].toString() + "\t"
+                + line[2].toString());
+        }
+        out.close();
+    }
+
+    /**
+     * @param args
+     * @throws ClassNotFoundException
+     * @throws IOException
+     */
+    public static void main(String [] args) throws IOException,
+        ClassNotFoundException
+    {
+        String outFileBase = System.getProperty("odp.dump.out");
+        if (outFileBase == null)
+        {
+            throw new RuntimeException(
+                "Property 'odp.dump.out' must be defined to point to the output file path.");
+        }
+
+        String odpIndexDir = System.getProperty("odp.index.dir");
+        if (odpIndexDir == null)
+        {
+            throw new RuntimeException(
+                "Property 'odp.index.dir' must be defined to point an ODP index.");
+        }
+
+        TopicDumper topicDumper = new TopicDumper();
+        topicDumper.dump(odpIndexDir, outFileBase);
+    }
+}
