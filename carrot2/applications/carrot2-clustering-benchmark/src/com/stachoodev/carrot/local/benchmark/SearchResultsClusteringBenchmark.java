6da53049d43830ebd3be69bc9e8d11941b66696f
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2004-07-10 21:06:31 +0000

    Added: a large number of test queries
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@338 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/applications/carrot2-clustering-benchmark/src/com/stachoodev/carrot/local/benchmark/SearchResultsClusteringBenchmark.java
+++ carrot2/applications/carrot2-clustering-benchmark/src/com/stachoodev/carrot/local/benchmark/SearchResultsClusteringBenchmark.java
@@ -33,6 +33,9 @@ import com.stachoodev.matrix.factorization.seeding.*;
  * Benchmarks search results clustering algorithms available in Carrot2 using
  * Open Directory Project data.
  * 
+ * TODO: support for query description?
+ * TODO: option for turning on/off profiling for particular components?
+ * 
  * @author Stanislaw Osinski
  * @version $Revision$
  */
@@ -72,28 +75,125 @@ public class SearchResultsClusteringBenchmark
      */
     private void addQueries(Map queryMap)
     {
-        // 1244841 - web clustering
-
         warmUpQuery = "catid: 42951";
-        queryMap.put("separation-level-1",
-            "catid: 791558 905697 5843791 96938 26950 43585 468909");
-        queryMap.put("separation-level-2",
-            "catid: 337874 48435 171000 783469 289730 26975 210325");
-        queryMap
-            .put(
-                "separation-level-3",
-                "catid: 209353 592083 327 196267 240856 27078 303 27074 283016 27075 1139293 27073");
-        queryMap.put("separation-level-4",
-            "catid: 136387 504694 86425 96057 92335");
-        queryMap.put("separation-level-5",
-            "catid: 58029 449902 292594 115111 382391 110783 452210");
-        queryMap.put("ai-nn-people", "catid: 354439 5809");
-        queryMap.put("polska-level-1",
-            "catid: 339598 570229 212801 32496 232622 114095 365576 870296");
-        queryMap
-            .put(
-                "outlier-level-3",
-                "catid: 209353 592083 327 196267 240856 27078 303 27074 283016 27075 1139293 27073 1244841");
+//        queryMap.put("separation-level-1",
+//            "catid: 791558 905697 5843791 96938 26950 43585 468909");
+//        queryMap.put("separation-level-2",
+//            "catid: 337874 48435 171000 783469 289730 26975 210325");
+//        queryMap
+//            .put(
+//                "separation-level-3",
+//                "catid: 209353 592083 327 196267 240856 27078 303 27074 283016 27075 1139293 27073");
+//        queryMap.put("separation-level-4",
+//            "catid: 136387 504694 86425 96057 92335");
+//        queryMap.put("separation-level-5",
+//            "catid: 58029 449902 292594 115111 382391 110783 452210");
+//        queryMap.put("ai-nn-people", "catid: 354439 5809");
+//        queryMap.put("polska-level-1",
+//            "catid: 339598 570229 212801 32496 232622 114095 365576 870296");
+//        queryMap
+//            .put(
+//                "outlier-level-3",
+//                "catid: 209353 592083 327 196267 240856 27078 303 27074 283016 27075 1139293 27073 1244841");
+       
+        // Balanced size, separation level 1
+        queryMap.put("cats-2-bal-t-sep-1", "catid: 365639 287192");
+        queryMap.put("cats-3-bal-t-sep-1", "catid: 185352 7698 26775");
+        queryMap.put("cats-4-bal-t-sep-1", "catid: 6253 58375 8568 26954");
+        queryMap.put("cats-5-bal-t-sep-1", "catid: 41312 188623 7430 241 26948");
+        queryMap.put("cats-6-bal-t-sep-1", "catid: 325060 5462 84469 41436 93022 792595");
+        queryMap.put("cats-7-bal-t-sep-1", "catid: 899171 81121 98602 240716 468908 8830 27075");
+        queryMap.put("cats-8-bal-t-sep-1", "catid: 254684 1270973 817696 92618 472689 221 27160 283374");
+        
+        // Unbalanced size (+/- 50%), separation level 1
+        queryMap.put("cats-2-bal-f-sep-1", "catid: 69395 7210");
+        queryMap.put("cats-3-bal-f-sep-1", "catid: 58083 472742 48499");
+        queryMap.put("cats-4-bal-f-sep-1", "catid: 398551 435847 109413 41938");
+        queryMap.put("cats-5-bal-f-sep-1", "catid: 280473 196121 185 119014 423185");
+        queryMap.put("cats-6-bal-f-sep-1", "catid: 205549 7450 41300 6621 26841 505967");
+        queryMap.put("cats-7-bal-f-sep-1", "catid: 276670 7207 472697 52215 5800938 41936 26963");
+        queryMap.put("cats-8-bal-f-sep-1", "catid: 399658 280285 295 486788 26839 175622 129488 254784");
+
+        // Balanced size, separation level 2
+        queryMap.put("cats-2-bal-t-sep-2", "catid: 27082 282106");
+        queryMap.put("cats-3-bal-t-sep-2", "catid: 198214 83564 58419");
+        queryMap.put("cats-4-bal-t-sep-2", "catid: 8372 401 272162 58779");
+        queryMap.put("cats-5-bal-t-sep-2", "catid: 58123 887744 5339 82155 437987");
+        queryMap.put("cats-6-bal-t-sep-2", "catid: 69478 577433 26881 26946 106806 96023");
+        queryMap.put("cats-7-bal-t-sep-2", "catid: 8421 40632 178528 140175 59800 350389 540280");
+        queryMap.put("cats-8-bal-t-sep-2", "catid: 6122 4902 110507 133 452210 5431 589495 198647");
+
+        // Unbalanced size (+/- 50%), separation level 2
+        queryMap.put("cats-2-bal-f-sep-2", "catid: 6620 5307");
+        queryMap.put("cats-3-bal-f-sep-2", "catid: 5822171 7460 7706");
+        queryMap.put("cats-4-bal-f-sep-2", "catid: 198926 123154 349908 95762");
+        queryMap.put("cats-5-bal-f-sep-2", "catid: 6378 6163 6055 452806 4918");
+        queryMap.put("cats-6-bal-f-sep-2", "catid: 8888 220801 8090 707465 40022 43581");
+        queryMap.put("cats-7-bal-f-sep-2", "catid: 7322 7455 53786 1257637 392525 8887 7606");
+        queryMap.put("cats-8-bal-f-sep-2", "catid: 292999 317940 1157346 57931 4808 6142 4993 1243179");
+
+        // Balanced size, separation level 3
+        queryMap.put("cats-2-bal-t-sep-3", "catid: 48472 26621");
+        queryMap.put("cats-3-bal-t-sep-3", "catid: 197724 5465 332889");
+        queryMap.put("cats-4-bal-t-sep-3", "catid: 429194 397702 791675 5347");
+        queryMap.put("cats-5-bal-t-sep-3", "catid: 145491 5877762 287353 69526 112582");
+        queryMap.put("cats-6-bal-t-sep-3", "catid: 53264 812644 8955 52128 110835 8872");
+        queryMap.put("cats-7-bal-t-sep-3", "catid: 128323 841187 320908 413656 791254 382727 392113");
+        queryMap.put("cats-8-bal-t-sep-3", "catid: 108421 452582 6099 285920 124090 5451 459492 6072");
+
+        // Unbalanced size (+/- 50%), separation level 3
+        queryMap.put("cats-2-bal-f-sep-3", "catid: 59328 585483");
+        queryMap.put("cats-3-bal-f-sep-3", "catid: 851854 472726 473433");
+        queryMap.put("cats-4-bal-f-sep-3", "catid: 26914 388662 80718 1211428");
+        queryMap.put("cats-5-bal-f-sep-3", "catid: 811341 287353 396899 80887 320");
+        queryMap.put("cats-6-bal-f-sep-3", "catid: 559969 1274009 359908 7667 110037 51052");
+        queryMap.put("cats-7-bal-f-sep-3", "catid: 170719 895110 4916 57507 5404 85675 4918");
+        queryMap.put("cats-8-bal-f-sep-3", "catid: 592083 327 868131 196267 27078 303 240856 210326");
+
+        // Unbalanced size, separation level 4
+        queryMap.put("cats-2-bal-f-sep-4", "catid: 57882 6083");
+        queryMap.put("cats-3-bal-f-sep-4", "catid: 185352 5779 463882");
+        queryMap.put("cats-4-bal-f-sep-4", "catid: 178332 26974 223614 26962");
+        queryMap.put("cats-5-bal-f-sep-4", "catid: 6524 530870 6476 5954 6012");
+        queryMap.put("cats-6-bal-f-sep-4", "catid: 904305 200591 216495 43371 5871416 347409");
+        queryMap.put("cats-7-bal-f-sep-4", "catid: 5878175 8345 80915 112077 8335 421910 8417");
+        queryMap.put("cats-8-bal-f-sep-4", "catid: 6163 57964 6198 399658 80614 6244 6238 6181");
+
+        // Unbalanced size, separation level 5
+        queryMap.put("cats-2-bal-f-sep-5", "catid: 7700 215778");
+        queryMap.put("cats-3-bal-f-sep-5", "catid: 5866 5867 5868");
+        queryMap.put("cats-4-bal-f-sep-5", "catid: 116067 6474 122008 6472");
+        queryMap.put("cats-5-bal-f-sep-5", "catid: 6523 142289 6524 107353 395536");
+        queryMap.put("cats-6-bal-f-sep-5", "catid: 71085 5830352 793306 96550 71084 71111");
+        queryMap.put("cats-7-bal-f-sep-5", "catid: 418653 418651 431700 116277 280883 139063 144344");
+        queryMap.put("cats-8-bal-f-sep-5", "catid: 452210 504547 382391 58001 449575 531880 435644 420716");
+
+        // Unbalanced size, separation level 6
+        queryMap.put("cats-2-bal-f-sep-6", "catid: 58008 1174851");
+        queryMap.put("cats-3-bal-f-sep-6", "catid: 335064 448461 448462");
+        queryMap.put("cats-4-bal-f-sep-6", "catid: 432203 432201 432200 781635");
+        queryMap.put("cats-5-bal-f-sep-6", "catid: 84491 57479 96503 110519 84479");
+        queryMap.put("cats-6-bal-f-sep-6", "catid: 6296 6297 5815462 6298 6301 5815457");
+        queryMap.put("cats-7-bal-f-sep-6", "catid: 1188811 6543 535289 533908 1140685 900626 1227509");
+        queryMap.put("cats-8-bal-f-sep-6", "catid: 901623 392036 203972 58004 58031 110219 504547 906183");
+
+        // A single outlier of different sizes
+        queryMap.put("sep-3-on-1-os-100", "catid: 429194 397702 791675 5347 556802");
+        queryMap.put("sep-3-on-1-os-50", "catid: 429194 397702 791675 5347 1257774");
+        queryMap.put("sep-3-on-1-os-40", "catid: 429194 397702 791675 5347 783404");
+        queryMap.put("sep-3-on-1-os-30", "catid: 429194 397702 791675 5347 94505");
+        queryMap.put("sep-3-on-1-os-20", "catid: 429194 397702 791675 5347 1126382");
+        queryMap.put("sep-3-on-1-os-15", "catid: 429194 397702 791675 5347 349824");
+        queryMap.put("sep-3-on-1-os-10", "catid: 429194 397702 791675 5347 59498");
+
+        // Two outliers of different sizes
+        queryMap.put("sep-3-on-2-os-100", "catid: 145491 5877762 287353 69526 112582 7433 8417");
+        queryMap.put("sep-3-on-2-os-50", "catid: 145491 5877762 287353 69526 112582 904279 783380");
+        queryMap.put("sep-3-on-2-os-40", "catid: 145491 5877762 287353 69526 112582 921041 933088");
+        queryMap.put("sep-3-on-2-os-30", "catid: 145491 5877762 287353 69526 112582 201009 8764");
+        queryMap.put("sep-3-on-2-os-20", "catid: 145491 5877762 287353 69526 112582 276657 26619");
+        queryMap.put("sep-3-on-2-os-15", "catid: 145491 5877762 287353 69526 112582 793986 209358");
+        queryMap.put("sep-3-on-2-os-10", "catid: 145491 5877762 287353 69526 112582 568954 1131193");
     }
 
     /**
@@ -443,9 +543,6 @@ public class SearchResultsClusteringBenchmark
         XMLReport mainReport = new XMLReport("report");
         List results = new ArrayList();
         long mainStart = System.currentTimeMillis();
-
-        //        LineNumberReader reader = new LineNumberReader(new
-        // InputStreamReader(System.in));
 
         // For each registered process
         List processNames = localController.getProcessIds();
