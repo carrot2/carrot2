1e3472bc69163601cf4f7e1c6374feed09acd6c7
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2004-08-23 14:25:44 +0000

    Initial checkin.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@419 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ carrot2/components/carrot2-util-matrix/src-test/com/stachoodev/matrix/EigenvalueCalculatorTest.java
@@ -0,0 +1,70 @@
+/*
+ * Carrot2 Project
+ * Copyright (C) 2002-2004, Dawid Weiss
+ * Portions (C) Contributors listed in carrot2.CONTRIBUTORS file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the CVS checkout or at:
+ * http://www.cs.put.poznan.pl/dweiss/carrot2.LICENSE
+ */
+package com.stachoodev.matrix;
+
+import java.util.*;
+
+import junit.framework.*;
+import junitx.framework.*;
+import cern.colt.matrix.*;
+import cern.colt.matrix.impl.*;
+import cern.colt.matrix.linalg.*;
+
+/**
+ * @author Stanislaw Osinski
+ * @version $Revision$
+ */
+public class EigenvalueCalculatorTest extends TestCase
+{
+    /** The test input matrix */
+    private DoubleMatrix2D A = NNIDoubleFactory2D.nni.make(new double [] []
+    {
+    { 1.00, 7.00, 0.00, 1.00, 0.00 },
+    { 4.00, 2.00, 0.00, 0.00, 0.00 },
+    { 0.00, 2.00, 3.00, 7.00, 9.00 },
+    { 1.00, 5.00, 4.00, 4.00, 3.00 },
+    { 0.00, 0.00, 6.00, 3.00, 5.00 } });
+
+    /**
+     *  
+     */
+    public void testSymmetrical()
+    {
+        DoubleMatrix2D Asym = A.zMult(A, null, 1, 0, true, false);
+        double [] expectedEigenvalues = new EigenvalueDecomposition(Asym)
+            .getRealEigenvalues().toArray();
+        Arrays.sort(expectedEigenvalues);
+
+        double [] eigenvalues = EigenvalueCalculator
+            .computeEigenvaluesSymmetricalNNI((DenseDoubleMatrix2D) Asym);
+        Arrays.sort(eigenvalues);
+
+        ArrayAssert.assertEquals("Equal real eigenvalues", expectedEigenvalues,
+            eigenvalues, 1e-6);
+    }
+
+    /**
+     *  
+     */
+    public void testAsymmetrical()
+    {
+        double [] eigenvalues = EigenvalueCalculator
+            .computeEigenvaluesNNI((DenseDoubleMatrix2D) A);
+        Arrays.sort(eigenvalues);
+
+        double [] expectedEigenvalues = new EigenvalueDecomposition(A)
+            .getRealEigenvalues().toArray();
+        Arrays.sort(expectedEigenvalues);
+
+        ArrayAssert.assertEquals("Equal real eigenvalues", expectedEigenvalues,
+            eigenvalues, 1e-6);
+    }
+}
