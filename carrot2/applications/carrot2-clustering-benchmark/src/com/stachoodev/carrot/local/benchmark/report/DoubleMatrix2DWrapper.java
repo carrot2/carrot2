dd680ddaa0605f0394e8d4b83a28279cb9da1ac4
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2004-08-23 14:07:53 +0000

    Minor refactorings.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@409 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ carrot2/applications/carrot2-clustering-benchmark/src/com/stachoodev/carrot/local/benchmark/report/DoubleMatrix2DWrapper.java
@@ -0,0 +1,74 @@
+/*
+ * Carrot2 Project
+ * Copyright (C) 2002-2004, Dawid Weiss
+ * Portions (C) Contributors listed in carrot2.CONTRIBUTORS file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the CVS checkout or at:
+ * http://www.cs.put.poznan.pl/dweiss/carrot2.LICENSE
+ */
+package com.stachoodev.carrot.local.benchmark.report;
+
+import java.util.*;
+
+import cern.colt.matrix.*;
+
+/**
+ * @author Stanislaw Osinski
+ * @version $Revision$
+ */
+public class DoubleMatrix2DWrapper
+{
+    /** */
+    private DoubleMatrix2D matrix;
+
+    /** */
+    private List rowLabels;
+    
+    /** */
+    private List columnLabels;
+    
+    /**
+     * @param matrix
+     * @param rowLabels
+     * @param columnLabels
+     */
+    public DoubleMatrix2DWrapper(DoubleMatrix2D matrix, List rowLabels,
+        List columnLabels)
+    {
+        this.matrix = matrix;
+        this.rowLabels = rowLabels;
+        this.columnLabels = columnLabels;
+    }
+    
+    /**
+     * Returns this DoubleMatrix2DWrapper's <code>matrix</code>.
+     * 
+     * @return 
+     */
+    public DoubleMatrix2D getMatrix()
+    {
+        return matrix;
+    }
+    
+    /**
+     * Returns this DoubleMatrix2DWrapper's <code>rowLabels</code>.
+     * 
+     * @return 
+     */
+    public List getRowLabels()
+    {
+        return rowLabels;
+    }
+    
+    /**
+     * Returns this DoubleMatrix2DWrapper's <code>columnLabels</code>.
+     * 
+     * @return 
+     */
+    public List getColumnLabels()
+    {
+        return columnLabels;
+    }
+}
