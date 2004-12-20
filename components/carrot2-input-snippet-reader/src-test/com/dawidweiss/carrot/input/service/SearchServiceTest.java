422d7ffec3f7d3cf753c151c765b84c63d318b55
Dawid Weiss <dawid.weiss@carrotsearch.com>
2004-12-20 21:53:51 +0000

    Refactoring of the snippet reader component started. Moving to Dom4J, a somewhat cleaner API will also follow.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@462 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ components/carrot2-input-snippet-reader/src-test/com/dawidweiss/carrot/input/service/SearchServiceTest.java
@@ -0,0 +1,70 @@
+package com.dawidweiss.carrot.input.service;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import com.dawidweiss.carrot.input.snippetreader.service.SearchService;
+import com.dawidweiss.carrot.util.common.StreamUtils;
+
+import junit.framework.TestCase;
+
+/**
+ * A test case for search service descriptor class.
+ * 
+ * @author Dawid Weiss
+ * @version $Revision$
+ */
+public class SearchServiceTest extends TestCase {
+
+    public SearchServiceTest(String s) {
+        super(s);
+    }
+
+    /**
+     * Tests only parsing of descriptor files.
+     * @throws IOException
+     */
+    public void testParsingDescriptorFiles() throws IOException {
+        File servicesDir = new File("web" + File.separator + "services");
+        if (!servicesDir.isDirectory()) {
+            throw new RuntimeException("Services folder not found: "
+                    + servicesDir.getAbsolutePath());
+        }
+        
+        File services [] = servicesDir.listFiles(new FilenameFilter() {
+			public boolean accept(File parent, String file) {
+                return file.endsWith(".xml");
+			}
+        });
+            
+        for (int i=0 ; i<services.length ; i++) {
+            new SearchService(services[i]);
+        }
+    }
+
+    /**
+     * Test generic page retrieval method.
+     */
+    public void testRetrievalOfAWebPage() throws Exception {
+        File serviceFile = new File("web" + File.separator + "services"
+                + File.separator + "google.xml");
+        if (!serviceFile.isFile()) {
+            throw new RuntimeException("Service file not found: "
+                    + serviceFile.getAbsolutePath());
+        }
+        
+        SearchService service = new SearchService(serviceFile);
+        HashMap params = new HashMap();
+        params.put("query.string", "apache ant");
+        params.put("query.startFrom", "0");
+        InputStream is = service.getRawPage(params);
+        assertNotNull(is);
+        byte [] page = StreamUtils.readFully(is);
+        String pageContent = new String(page, "UTF-8");
+        assertTrue(pageContent.indexOf("ant.apache.org") > 0);
+    }
+
+}
