485f93c1ae3d484c24445ded818607b30d323778
Dawid Weiss <dawid.weiss@carrotsearch.com>
2005-06-29 17:32:59 +0000

    Print dependencies now dumps a graph of component relationships in addition to their aggregated list. This should increase the transparency of what requires what.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@585 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/components/carrot2-ant-extensions/src/com/dawidweiss/carrot/ant/PrintDependencies.java
+++ carrot2/components/carrot2-ant-extensions/src/com/dawidweiss/carrot/ant/PrintDependencies.java
@@ -5,10 +5,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
@@ -117,6 +120,10 @@ public class PrintDependencies extends Task {
         if (this.outputFile == null && this.property == null)
             throw new BuildException("Either file or property attribute must be specified.");
     }
+    
+    protected static void indent(StringBuffer buf, int indent) {
+        for (int i=0; i<indent; i++) buf.append("    ");
+    }
 
     /**
      * Searches for components that are not up to date and updates
@@ -135,17 +142,26 @@ public class PrintDependencies extends Task {
             // load all dependencies pointed to by embedded filesets.
             HashMap components = new HashMap();
             components.put(component.getName(), component);
-            ComponentDependencyUtils.loadComponentDependencies( futils, getProject(), this.dependencies, components);
+            ComponentDependencyUtils.loadComponentDependencies(futils, getProject(), this.dependencies, components);
 
             ComponentInProfile [] dependencies = component
                 .getAllRequiredComponentDependencies(components, profile);
+            
             ComponentInProfile self = new ComponentInProfile(component, profile);
+            // Locate yourself again to get access to all dependencies.
+            for (int i = 0; i<dependencies.length; i++) {
+                if (dependencies[i].equals(self)) {
+                    self = dependencies[i];
+                    break;
+                }
+            }
 
             StringBuffer buf = new StringBuffer();
-            buf.append("Component '" + component.getDescription() + "'\n\n");
-            buf.append("Dependency profile: " + (profile == null ? "(default)" : "'" + profile + "'") + "\n"); 
-            buf.append("Dependencies:\n");
+            buf.append("Component '" + component.getDescription() + "'\n");
+            buf.append("Component built in profile: " + (profile == null ? "(default)" : "'" + profile + "'") + "\n\n"); 
 
+            // Dump dependencies linearily.
+            buf.append("Dependencies (aphabetical, aggregated):\n\n");
 			Arrays.sort(dependencies, new Comparator() {
                 public int compare(Object o1, Object o2) {
                 	ComponentInProfile d1 = (ComponentInProfile) o1;
@@ -154,22 +170,89 @@ public class PrintDependencies extends Task {
                             d2.component.getDescription());
                 }
 			});
-
-            for (int i=0; i<dependencies.length; i++) {
-                ComponentInProfile dependency = dependencies[i];
-                
-                if (dependency.equals(self)) {
-                	continue;
-                }
-
-                buf.append("   - " + dependency.component.getDescription());
-                if (dependency.profile != null) {
-                	buf.append(" [in profile: '" + dependency.profile + "']"); 
-                }
-                buf.append("\n");
-            }
             if (dependencies.length - 1 <= 0) {
-                buf.append("no dependencies.");
+                indent(buf, 1);
+                buf.append("[no dependencies]\n");
+            } else {
+	            for (int i=0; i<dependencies.length; i++) {
+	                final ComponentInProfile dependency = dependencies[i];
+	                if (dependency.equals(self)) {
+	                	continue;
+	                }
+	                indent(buf, 1);
+	                buf.append((i+1) + ") " + dependency.component.getDescription());
+                	buf.append(" [" + dependency.component.getName() + "]"); 
+	                if (dependency.profile != null) {
+	                	buf.append(" [in profile: '" + dependency.profile + "']"); 
+	                }
+	                buf.append("\n");
+	            }
+            }
+
+            // Dump dependencies as a graph.
+            buf.append("\nDependencies (graph):\n\n");
+            List deps = self.getDependencies();
+            if (deps.size() == 0) {
+                indent(buf, 1);
+                buf.append("[no dependencies]\n");
+            } else {
+                // Declare an inner class for recursion only.
+                final class Dumper {
+                    private ArrayList columns = new ArrayList();
+                    private final static String LINE = "line";
+                    private final static String ENDLINE = "endline";
+                    private final static String NOLINE = "noline";
+                    private final static String INLINE = "inline";
+
+                    public void dump(StringBuffer buf, int indent, ComponentInProfile component) {
+	                    while (indent > columns.size()) {
+                            columns.add(NOLINE);
+	                    }
+                        dumpInternal(buf, indent, component);
+                    }
+
+                    private void dumpInternal(StringBuffer buf, int indent, ComponentInProfile component) {
+                        String v;
+
+                        for (int i=0; i<indent; i++) {
+                            v = (String) columns.get(i);
+                            if (v == NOLINE) {
+                                buf.append("   ");
+                            } else if (v == LINE) {
+                                buf.append("|  ");
+                            } else if (v == INLINE) {
+                                buf.append("|--");
+                            } else if (v == ENDLINE) {
+                                buf.append("\\--");
+                            }
+                        }
+
+                        buf.append(component.component.getName());
+    	                if (component.profile != null) {
+    	                	buf.append(" [in profile: '" + component.profile + "']"); 
+    	                }
+    	                buf.append("\n");
+
+	                    while (indent + 1 >= columns.size()) {
+                            columns.add(NOLINE);
+	                    }
+	                    
+	                    if (indent > 0) {
+		                    v = (String) columns.get(indent-1);
+		                    if (v == INLINE) columns.set(indent-1, LINE);
+		                    else if (v == ENDLINE) columns.set(indent-1, NOLINE);
+	                    }
+
+    	                for (Iterator i = component.getDependencies().iterator(); i.hasNext();) {
+    	                    ComponentInProfile cip = (ComponentInProfile) i.next();
+    	                    columns.set(indent, i.hasNext() ? INLINE : ENDLINE);
+    	                    dumpInternal(buf, indent+1, cip);
+    	                }
+                    }
+                };
+
+                Dumper dumper = new Dumper();
+                dumper.dump(buf, 1, self);
             }
 
             if (this.outputFile != null) {
