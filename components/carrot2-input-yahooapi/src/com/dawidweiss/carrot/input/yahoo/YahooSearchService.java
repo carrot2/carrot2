0ca5bf5d8ac1ad9224467ef69d9016dfc2a3c6fd
Dawid Weiss <dawid.weiss@carrotsearch.com>
2005-10-05 21:11:24 +0000

    Replaced JDK's http form sender with Apache Commons HttpClient. It supports HTTP 1.1 and we can use compressed results from Yahoo.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@649 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- components/carrot2-input-yahooapi/src/com/dawidweiss/carrot/input/yahoo/YahooSearchService.java
+++ components/carrot2-input-yahooapi/src/com/dawidweiss/carrot/input/yahoo/YahooSearchService.java
@@ -4,16 +4,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.zip.GZIPInputStream;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.HttpVersion;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
-import com.dawidweiss.carrot.util.net.http.HTTPFormSubmitter;
+import com.dawidweiss.carrot.util.net.http.FormActionInfo;
+import com.dawidweiss.carrot.util.net.http.FormParameters;
+import com.dawidweiss.carrot.util.net.http.Parameter;
 
 /**
  * A Yahoo Search Service.
@@ -37,7 +48,13 @@ public class YahooSearchService {
             final int requestedResults) throws IOException {
 
         final ArrayList result = new ArrayList(requestedResults); 
-        final HTTPFormSubmitter submitter = descriptor.getHttpSubmitter();
+
+        final MultiThreadedHttpConnectionManager connectionManager = new MultiThreadedHttpConnectionManager();
+        final HttpClient client = new HttpClient(connectionManager);
+        client.getParams().setVersion(HttpVersion.HTTP_1_1);
+
+        final FormActionInfo formActionInfo = descriptor.getFormActionInfo();
+
         InputStream is = null; 
 
         int startFrom = 1;
@@ -62,10 +79,46 @@ public class YahooSearchService {
                 mappedParameters.put("query.string", query);
                 mappedParameters.put("query.startFrom", Integer.toString(startFrom));
                 mappedParameters.put("query.results", Long.toString(perQueryResults));
-                is = submitter.submit(descriptor.getFormParameters(), mappedParameters, "UTF-8");
-                reader.parse(new InputSource(is));
-                is.close();
-                is = null;
+
+                // Convert from FormActionInfo/FormParameters to HttpClient. This could
+                // be implemented nicer perhaps.
+                FormParameters parameters = descriptor.getFormParameters();
+                PostMethod postMethod = new PostMethod(formActionInfo.getServiceURL().toExternalForm());
+                try {
+                    HashMap httpHeaders = formActionInfo.getHttpHeaders();
+                    for (Iterator i = httpHeaders.keySet().iterator(); i.hasNext();) {
+                        String header = (String) i.next();
+                        String value = (String) httpHeaders.get(header);
+                        postMethod.addRequestHeader(new Header(header, value));
+                    }
+                    postMethod.addRequestHeader(new Header("Accept-Encoding", "gzip"));
+    
+                    // Now convert parameters.
+                    for (Iterator i = parameters.getParametersIterator(); i.hasNext();) {
+                        final Parameter p = (Parameter) i.next();
+                        final String name = p.getName();
+                        final Object value = p.getValue(mappedParameters);
+                        if (value instanceof String) {
+                            postMethod.addParameter(new NameValuePair(name, (String) value));
+                        } else {
+                            throw new RuntimeException("Only String mapped parameters supported.");
+                        }
+                    }
+                    
+                    int statusCode = client.executeMethod(postMethod);
+                    if (statusCode == HttpStatus.SC_OK) {
+                        is = postMethod.getResponseBodyAsStream();
+                        Header encoded = postMethod.getResponseHeader("Content-Encoding");
+                        if (encoded != null && "gzip".equals(encoded.getValue())) {
+                            is = new GZIPInputStream(is);
+                        }
+                        reader.parse(new InputSource(is));
+                        is.close();
+                        is = null;
+                    }
+                } finally {
+                    postMethod.releaseConnection();
+                }
 
                 if (handler.firstResultPosition != startFrom && handler.resultsReturned > 0) {
                     throw new IOException("Assertion failed: returned startFrom different.");
@@ -86,13 +139,7 @@ public class YahooSearchService {
         } catch (SAXException e) {
             throw new RuntimeException("Problems parsing Yahoo output: " + e.toString(), e);
         } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                    // Ignore exceptions on close.
-                }
-            }
+            connectionManager.shutdown();
         }
 
         return (YahooSearchResult []) result.toArray(new YahooSearchResult [result.size()]);
