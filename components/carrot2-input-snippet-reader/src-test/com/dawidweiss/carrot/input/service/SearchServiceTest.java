5f5447744b567b303481df9a58191bced277b094
Dawid Weiss <dawid.weiss@carrotsearch.com>
2006-01-04 13:44:17 +0000

    - Snippet reader for Google removed (replaced by Google API). - AllTheWeb's descriptor updated.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@775 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- components/carrot2-input-snippet-reader/src-test/com/dawidweiss/carrot/input/service/SearchServiceTest.java
+++ components/carrot2-input-snippet-reader/src-test/com/dawidweiss/carrot/input/service/SearchServiceTest.java
@@ -12,16 +12,13 @@
  */
 package com.dawidweiss.carrot.input.service;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.HashMap;
+
+import junit.framework.TestCase;
 
 import com.dawidweiss.carrot.input.snippetreader.service.SearchService;
 import com.dawidweiss.carrot.util.common.StreamUtils;
-
-import junit.framework.TestCase;
 
 /**
  * A test case for search service descriptor class.
@@ -30,7 +27,6 @@ import junit.framework.TestCase;
  * @version $Revision$
  */
 public class SearchServiceTest extends TestCase {
-
     public SearchServiceTest(String s) {
         super(s);
     }
@@ -62,7 +58,7 @@ public class SearchServiceTest extends TestCase {
      */
     public void testRetrievalOfAWebPage() throws Exception {
         File serviceFile = new File("web" + File.separator + "services"
-                + File.separator + "google.xml");
+                + File.separator + "alltheweb.xml");
         if (!serviceFile.isFile()) {
             throw new RuntimeException("Service file not found: "
                     + serviceFile.getAbsolutePath());
