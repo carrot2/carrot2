1cd141ace08fe95315fbf0eea810e41a1fb59314
Dawid Weiss <dawid.weiss@carrotsearch.com>
2006-01-18 13:20:03 +0000

    Java WebStart demo compilation.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@900 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- applications/carrot2-demo-browser/webstart/demo.php
+++ applications/carrot2-demo-browser/webstart/demo.php
@@ -26,57 +26,41 @@
 ?>
 
 <jnlp spec="1.0+" codebase="<?php echo $BASE_URL; ?>">
-   <information>
-      <title>Carrot2 Demo Application</title>
-      <vendor>Carrot2 Project (www.carrot2.org)</vendor>
-   </information>
+	<information>
+		<title>Carrot2 Demo Application</title>
+		<vendor>Carrot2 Project (www.carrot2.org)</vendor>
 
-   <resources>
-      <j2se version="1.4+"/>
-        <jar href="carrot2-demo-browser.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/demo-resources.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/bsh-2.0b4.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-demo-browser.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-filter-lingo.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-filter-stc.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-filter-trc.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-input-local-cache.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-input-yahooapi.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-local-controller.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-local-core.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-snowball-stemmers.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-stemmer-stempelator.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-util-common.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/carrot2-util-tokenizer.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/colt-1.0.3-trimmed.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/commons-codec-1.3.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/commons-collections-3.1-patched.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/commons-httpclient-3.0-rc3.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/commons-logging.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/commons-pool-1.1.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/dom4j-1.5.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/format.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/forms-1.0.5.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/jal.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/Jama-1.0.1-patched.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/jaxen-core.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/jaxen-dom4j.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/jdic.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/junit-3.8.1.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/log4j-1.2.11.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/looks-1.2.2.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/lucene-1.5-rc1-dev.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/saxpath.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/stempel-1.0-dw.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/stempelator-1.0.jar" />
-        <jar href="deps-carrot2-demo-browser-jar/violinstrings-1.0.2.jar" />
-   </resources>
+		<homepage href="http://www.carrot2.org/" />
 
-   <security>
-     <all-permissions/>
-   </security>
+		<description>Carrot2 Demo Application</description>
+		<description kind="short">Carrot2 Demo Application for tuning and testing purposes.</description>
+		<description kind="tooltip">Carrot2 Demo</description>
 
-   <application-desc main-class="carrot2.demo.DemoSwing">
-   		<argument>--resource</argument> 
-   </application-desc>
+		<icon href="img/carrot2-64x64.gif" width="64" height="64" />
+		<icon href="img/carrot2-splash.jpg" kind="splash" />
+
+		<offline-allowed />
+	</information>
+	
+	<resources>
+		<j2se version="1.4+" />
+		
+		<jar href="carrot2-demo-browser.jar" />
+		<jar href="lib/demo-resources.jar" />
+
+		<?php include("demo.inc"); ?>
+	</resources>
+
+	<resources os="Windows">
+		<jar href="lib/windows/jdic.jar"/> 
+		<nativelib href="lib/windows/jdic-native.jar"/>
+	</resources>
+
+	<security>
+		<all-permissions/>
+	</security>
+
+	<application-desc main-class="carrot2.demo.DemoSwing">
+		<argument>--resource</argument> 
+	</application-desc>
 </jnlp>
