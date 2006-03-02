c4d9c5fe22adbb4fa3aaada15118d714776afc3f
Karol Golembniak <man07@wp.pl>
2006-03-02 18:14:18 +0000

    Cluster description limit.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@982 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/components/carrot2-filter-haog/src/com/kgolembniak/carrot/filter/haog/algorithm/CombinedVertex.java
+++ carrot2/components/carrot2-filter-haog/src/com/kgolembniak/carrot/filter/haog/algorithm/CombinedVertex.java
@@ -1,8 +1,17 @@
 package com.kgolembniak.carrot.filter.haog.algorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+
+import com.dawidweiss.carrot.filter.stc.StcParameters;
+import com.dawidweiss.carrot.filter.stc.algorithm.BaseCluster;
+import com.dawidweiss.carrot.filter.stc.algorithm.Phrase;
+import com.dawidweiss.carrot.filter.stc.algorithm.StemmedTerm;
+import com.dawidweiss.carrot.filter.stc.suffixtree.ExtendedBitSet;
 
 /**
  * Class representing vertex, which can consist other vertex. This is usefull
@@ -14,6 +23,16 @@ public class CombinedVertex extends Vertex{
 	 * Vertices combined by this vertex.
 	 */
 	private List vertices;
+	
+	/**
+	 * Documents in this vertex.
+	 */
+	private ExtendedBitSet documents;
+	
+	/**
+	 * Phrases describing this vertex.
+	 */
+	private List phrases;
 	
 	public CombinedVertex(String label){
 		super(label);
@@ -189,7 +208,206 @@ public class CombinedVertex extends Vertex{
 		this.predList.addAll(predSet);
 	}
 	
-//Method used for tests only
+	/**
+	 * This method returns description of vertex, which is displayed
+	 * to user.
+	 * @param params - Parameters used for description limiting
+	 * @return description of this vertex as String.
+	 */
+	public String getVertexDescription(StcParameters params) {
+		phrases = getVertexPhrases(params);
+
+		String description = "";
+		for (int i1=0; i1<phrases.size(); i1++){
+			final Phrase phrase = (Phrase) phrases.get(i1);
+			description += phrase.userFriendlyTerms().trim();
+			//TODO get this parameter from params
+			if (i1>3) break;
+		}
+		
+		return description;
+	}	
+	
+	/**
+	 * This method decides which phrase is more important and orders
+	 * phrases. This method is taken from  
+	 * {@link com.dawidweiss.carrot.filter.stc.algorithm.MergedCluster.createDescriptionPhrases()} 
+	 * @param params - Parameters needed to decide about phrase importance.
+	 * @return List of phrases ordered descending by their importance.
+	 */
+	private List getVertexPhrases(StcParameters params) {
+		ArrayList phrases = new ArrayList();
+		if (documents==null){
+			getDocumentsFromBaseClusters();
+		}
+		
+		BaseCluster cluster;
+		Phrase phrase;
+		for (int i1=0; i1<vertices.size(); i1++){
+			cluster = ((Vertex) vertices.get(i1)).getRepresentedCluster();
+			phrase = cluster.getPhrase();
+			phrase.setCoverage(
+					(float) cluster.getNode().getSuffixedDocumentsCount() / 
+					documents.numberOfSetBits());
+			phrases.add(phrase);
+		}
+		
+		Phrase firstPhrase;
+		Phrase secondPhrase;
+		for (int i1=0; i1<phrases.size(); i1++){
+			firstPhrase = (Phrase) phrases.get(i1);
+			
+			phraseLoop:
+			for (int i2=0; i2<phrases.size(); i2++){
+				if (i1!=i2){
+					secondPhrase = (Phrase) phrases.get(i2);
+					
+					List terms = firstPhrase.getTerms();
+					for (int i3=0; i3<terms.size(); i3++){
+						final StemmedTerm term = (StemmedTerm) terms.get(i3);
+						if (!secondPhrase.getTerms().contains(term)){
+							continue phraseLoop;
+						}
+						
+						firstPhrase.mostSpecific = false;
+						secondPhrase.mostGeneral = false;
+					}
+				}
+			}
+		}
+		
+		for (int i1=0; i1<phrases.size(); i1++){
+			firstPhrase = (Phrase) phrases.get(i1);
+			if (firstPhrase.mostGeneral) {
+				thisPhraseIsUseFull:
+				for (int i2=0; i2<phrases.size(); i2++){
+					secondPhrase = (Phrase) phrases.get(i2);
+					if ((i1!=i2)&&(secondPhrase.mostSpecific)) {
+						List terms = firstPhrase.getTerms();
+						for (int i3=0; i3<terms.size(); i3++){
+							final StemmedTerm term = (StemmedTerm) terms.get(i3);
+							if ((!term.isStopWord())&&
+								(!secondPhrase.getTerms().contains(term))){
+								continue thisPhraseIsUseFull;
+							}
+							
+		                    if ((firstPhrase.getCoverage() - secondPhrase.getCoverage()) < 
+		                    	params.getMostGeneralPhraseCoverage()){
+		                    	firstPhrase.setSelected(false);
+		                        break;
+		                    }
+						}
+					}
+				}
+			}
+		}
+
+		for (int i1=0; i1<phrases.size(); i1++){
+			firstPhrase = (Phrase) phrases.get(i1);
+			for (int i2=0; i2<phrases.size(); i2++){
+				secondPhrase = (Phrase) phrases.get(i2);
+
+				if ((i1!=i2) && firstPhrase.isSelected() && 
+					secondPhrase.isSelected() && (firstPhrase.getCoverage() < 
+					secondPhrase.getCoverage())){
+
+                    float overlap = 0;
+                    float total = 0;
+					List terms = firstPhrase.getTerms();
+					for (int i3=0; i3<terms.size(); i3++){
+						final StemmedTerm term = (StemmedTerm) terms.get(i3); 
+						
+						if (!term.isStopWord()){
+							total += 1.0;
+							if (firstPhrase.getTerms().contains(term)) {
+								overlap += 1.0;
+							}
+						}
+					}
+
+					if ((overlap / total) > params.getMaxPhraseOverlap()){
+						firstPhrase.setSelected(false);
+                    }					
+				}
+			}
+		}
+
+		for (int i = 0; i < phrases.size(); i++){
+            Phrase current = (Phrase) phrases.get(i);
+            if (!current.mostGeneral && !current.mostSpecific){
+                current.setSelected(false);
+            }
+        }
+		
+        final Object [] objects = phrases.toArray();
+        Arrays.sort(
+            objects, 0, objects.length,
+            new Comparator()
+            {
+                public int compare(Object a, Object b)
+                {
+                    Phrase pa = (Phrase) a;
+                    Phrase pb = (Phrase) b;
+
+                    if ((pa.isSelected() && pb.isSelected()) || (!pa.isSelected() && !pb.isSelected()))
+                    {
+                        if (pa.getCoverage() > pb.getCoverage())
+                        {
+                            return -1;
+                        }
+                        else if (pa.getCoverage() < pb.getCoverage())
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                    else
+                    {
+                        if (pa.isSelected())
+                        {
+                            return -1;
+                        }
+                        else
+                        {
+                            return 1;
+                        }
+                    }
+                }
+            }
+        );
+
+        return java.util.Arrays.asList(objects);
+	}
+
+	/**
+	 * This method gets documents from all vertices combined by this vertex
+	 * and puts them to documents set in this vertex. All duplicates are
+	 * removed.
+	 */
+	private void getDocumentsFromBaseClusters() {
+		documents = new ExtendedBitSet();
+		BaseCluster cluster;
+		for (Iterator it = vertices.iterator(); it.hasNext();){
+			cluster = ((Vertex) it.next()).getRepresentedCluster();
+			documents.or(cluster.getNode().getInternalDocumentsRepresentation());
+		}
+	}
+	
+	/**
+	 * Returns documents contained in this vertex
+	 * @return documents
+	 */
+	public ExtendedBitSet getDocuments(){
+		if (documents==null){
+			getDocumentsFromBaseClusters();
+		}
+		return documents;
+	}
+
+	//Method used for tests only
 	public void printVertex(){
 		System.out.print(label);
 		if (vertices.size()>0){
