04ad08fbac76cabf969169861f4dc337f29b40fe
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2006-03-18 20:56:15 +0000

    Added sorting of process names in the process combo box.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@997 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/applications/carrot2-demo-browser/src/carrot2/demo/swing/util/MapComboModel.java
+++ carrot2/applications/carrot2-demo-browser/src/carrot2/demo/swing/util/MapComboModel.java
@@ -1,4 +1,3 @@
-
 /*
  * Carrot2 project.
  *
@@ -18,54 +17,76 @@ import java.util.*;
 import javax.swing.DefaultComboBoxModel;
 
 /**
- * A combo box model which displays a set of values
- * of a given hashmap, but also remembers the currently selected key.
+ * A combo box model which displays a set of values of a given hashmap, but also
+ * remembers the currently selected key.
  * 
  * @author Dawid Weiss
  */
-public final class MapComboModel extends DefaultComboBoxModel {
+public final class MapComboModel extends DefaultComboBoxModel
+{
     private final List ids;
     private final List values;
     int selected = 0;
 
-    public MapComboModel(Map processIdToProcessNameMap) {
+    public MapComboModel(Map processIdToProcessNameMap)
+    {
         final int size = processIdToProcessNameMap.size();
         this.ids = new ArrayList(size);
         this.values = new ArrayList(size);
-        for (Iterator i = processIdToProcessNameMap.entrySet().iterator(); i.hasNext();) {
-            final Map.Entry entry = (Map.Entry) i.next();
+
+        List entryList = new ArrayList(processIdToProcessNameMap.entrySet());
+        Collections.sort(entryList, new Comparator()
+        {
+            public int compare(Object o1, Object o2)
+            {
+                Map.Entry e1 = (Map.Entry) o1;
+                Map.Entry e2 = (Map.Entry) o2;
+                return e1.getValue().toString().compareTo(
+                    e2.getValue().toString());
+            }
+        });
+
+        for (Iterator iter = entryList.iterator(); iter.hasNext();)
+        {
+            final Map.Entry entry = (Map.Entry) iter.next();
             ids.add(entry.getKey());
             values.add(entry.getValue());
         }
     }
 
-    public void setSelectedItem(Object anItem) {
+    public void setSelectedItem(Object anItem)
+    {
         // We assume there is a small number of values,
         // so this inefficient lookup doesn't really matter.
         final int index = values.indexOf(anItem);
         if (index == -1)
-            throw new IllegalArgumentException("Process description is not defined: "
-                    + anItem);
+            throw new IllegalArgumentException(
+                "Process description is not defined: " + anItem);
         selected = index;
     }
 
-    public Object getSelectedItem() {
+    public Object getSelectedItem()
+    {
         return values.get(selected);
     }
 
-    public int getSize() {
+    public int getSize()
+    {
         return ids.size();
     }
 
-    public Object getElementAt(int index) {
+    public Object getElementAt(int index)
+    {
         return values.get(index);
     }
 
-    public Object getSelectedKey() {
+    public Object getSelectedKey()
+    {
         return ids.get(selected);
     }
-    
-    public Object getSelectedValue() {
+
+    public Object getSelectedValue()
+    {
         return getSelectedItem();
     }
 }
