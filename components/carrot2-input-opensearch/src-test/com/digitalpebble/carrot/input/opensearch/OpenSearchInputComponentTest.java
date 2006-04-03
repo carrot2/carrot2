b4346b1bc551e6025d93f536bbf317ae04c955ce
Dawid Weiss <dawid.weiss@carrotsearch.com>
2006-04-03 15:46:16 +0000

    OpenSearch input component. Initial version -- a bit cleaned up from the version submitted by Julien Nioche.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@1005 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ components/carrot2-input-opensearch/src-test/com/digitalpebble/carrot/input/opensearch/OpenSearchInputComponentTest.java
@@ -0,0 +1,83 @@
+
+/*
+ * Carrot2 project.
+ *
+ * Copyright (C) 2002-2006, Dawid Weiss, Stanisław Osiński.
+ * Portions (C) Contributors listed in "carrot2.CONTRIBUTORS" file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the repository checkout or at:
+ * http://www.carrot2.org/carrot2.LICENSE
+ */
+
+package com.digitalpebble.carrot.input.opensearch;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.dawidweiss.carrot.core.local.*;
+import com.dawidweiss.carrot.core.local.impl.DocumentsConsumerOutputComponent;
+
+/**
+ * A test case for OpenSearch input component.
+ * 
+ * @author Dawid Weiss
+ */
+public class OpenSearchInputComponentTest extends junit.framework.TestCase {
+    private final static Logger log = Logger.getLogger(OpenSearchInputComponentTest.class);
+
+    public OpenSearchInputComponentTest(String s) {
+        super(s);
+    }
+
+    /**
+     * Sends a sample query to icerocket's OpenSearch feed.
+     */
+    public void testIceRocket() throws Exception {
+        final LocalComponentFactory inputFactory = new LocalComponentFactoryBase() {
+            public LocalComponent getInstance() {
+                return new OpenSearchInputComponent("http://blogs.icerocket.com/search?q={searchTerms}&rss=1&os=1&p={startPage}&n={count}");
+            }
+        };
+
+        final LocalControllerBase controller = setUpController(inputFactory);
+        final String query = "blog";
+        final long start = System.currentTimeMillis();
+        final HashMap params = new HashMap();
+        params.put(LocalInputComponent.PARAM_REQUESTED_RESULTS, new Integer(50));
+        final List results = (List) controller.query(
+                "testprocess", query, params).getQueryResult();
+        final long end = System.currentTimeMillis();
+        log.info("Open Search query time: " + (end - start) + " ms.");
+
+        // the results should contain some documents.
+        assertEquals("Results acquired > 0?: " + results.size(), 50, results.size());
+    }
+
+	private LocalControllerBase setUpController(LocalComponentFactory inputFactory) throws Exception {
+		LocalControllerBase controller;
+		
+        // Some output component
+        LocalComponentFactory outputFactory = new LocalComponentFactoryBase() {
+            public LocalComponent getInstance() {
+                return new DocumentsConsumerOutputComponent();
+            }
+        };
+
+        // Register with the controller
+        controller = new LocalControllerBase();
+        controller.addLocalComponentFactory("output", outputFactory);
+        controller.addLocalComponentFactory("input", inputFactory);
+
+        // Create and register the process
+        LocalProcessBase process = new LocalProcessBase();
+        process.setInput("input");
+        process.setOutput("output");
+        controller.addProcess("testprocess", process);
+
+        return controller;
+	}
+}
---
+++ components/carrot2-input-opensearch/src/com/digitalpebble/carrot/input/opensearch/OpenSearchService.java
@@ -0,0 +1,122 @@
+
+/*
+ * Carrot2 project.
+ *
+ * Copyright (C) 2002-2006, Dawid Weiss, Stanisław Osiński.
+ * Portions (C) Contributors listed in "carrot2.CONTRIBUTORS" file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the repository checkout or at:
+ * http://www.carrot2.org/carrot2.LICENSE
+ */
+
+package com.digitalpebble.carrot.input.opensearch;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.dawidweiss.carrot.util.net.URLEncoding;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.fetcher.FeedFetcher;
+import com.sun.syndication.fetcher.FetcherException;
+import com.sun.syndication.fetcher.impl.HttpURLFeedFetcher;
+import com.sun.syndication.io.FeedException;
+
+/**
+ * A wrapper for an Open Search service.
+ * 
+ * @author Julien Nioche
+ */
+public class OpenSearchService {
+    private final static Logger log = Logger.getLogger(OpenSearchService.class);
+
+    private final String urlService;
+
+    private final FeedFetcher feedFetcher;
+
+    /**
+     * An OpenSearch service is initialized with a URLtemplate such as
+     * <tt>http://blogs.icerocket.com/search?q={searchTerms}&rss=1&os=1&p={startPage}&n={count}</tt>.
+     */
+    public OpenSearchService(String URLTemplate) {
+        urlService = URLTemplate;
+        this.feedFetcher = new HttpURLFeedFetcher();
+    }
+
+    /**
+     * Searches an OpenSearch Service and retrieves a maximum of
+     * <code>requestedResults</code> snippets. May throw an exception if
+     * service is no longer available.
+     * 
+     * @throws IOException If an I/O exception occurred.
+     * @throws FetcherException
+     * @throws FeedException
+     * @throws IllegalArgumentException
+     */
+    public OpenSearchResult[] query(final String query, final int requestedResults) throws IOException,
+            IllegalArgumentException, FeedException, FetcherException {
+
+        final ArrayList result = new ArrayList(requestedResults);
+
+        // convert the query to a UTF-8 URL
+        final String codedQuery = URLEncoding.encode(query, "UTF-8");
+
+        // builds queries until the number of results if found
+        // or there are no more results available
+        int pagenumber = 1;
+
+fetch:  while (true) {
+            // builds the URL by replacing the variables in the template
+            String feedUrl = urlService.replaceFirst("\\{searchTerms\\}", codedQuery);
+
+            // check whether the site supports startPage
+            // otherwise we'll rely on count to get the results
+            final boolean supportsPaging = (urlService.indexOf("startPage") != -1);
+
+            if (supportsPaging) {
+                // specify the page number in the query
+                final String pageNumber = "" + pagenumber;
+                feedUrl = feedUrl.replaceFirst("\\{startPage.?\\}", pageNumber);
+            }
+
+            // specify the number of results in the page
+            feedUrl = feedUrl.replaceFirst("\\{count.?\\}", "" + requestedResults);
+            log.info("Fetching: " + feedUrl);
+
+            // reads the Feed
+            final SyndFeed feed = feedFetcher.retrieveFeed(new URL(feedUrl));
+            final List entries = feed.getEntries();
+
+            if (entries.size() == 0) {
+                break fetch;
+            }
+
+            // Convert the entries into OpenSearchResults
+            for (int entry = 0; entry < entries.size(); entry++) {
+                // check that we did not exceed the number of results
+                if (result.size() >= requestedResults) {
+                    break fetch;
+                }
+                final SyndEntry sy = (SyndEntry) entries.get(entry);
+                final String syURL = sy.getLink();
+                final String syTITLE = sy.getTitle();
+                final String syDESCRIPTION = sy.getDescription().getValue();
+                final OpenSearchResult current = new OpenSearchResult(syURL, syTITLE, syDESCRIPTION);
+                result.add(current);
+            }
+            if (!supportsPaging) {
+                break fetch;
+            }
+            pagenumber++;
+        }
+
+        log.info("Found: " + result.size() + " results for " + query);
+        return (OpenSearchResult[]) result.toArray(new OpenSearchResult[result.size()]);
+    }
+}
