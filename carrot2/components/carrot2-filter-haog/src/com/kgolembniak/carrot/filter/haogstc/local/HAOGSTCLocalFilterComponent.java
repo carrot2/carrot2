8d9173fc28ef0f3ac8c3deb399b4c0f25e9a031a
Karol Golembniak <man07@wp.pl>
2006-04-26 21:40:08 +0000

    Refactoring
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1031 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/components/carrot2-filter-haog/src/com/kgolembniak/carrot/filter/haogstc/local/HAOGSTCLocalFilterComponent.java
+++ carrot2/components/carrot2-filter-haog/src/com/kgolembniak/carrot/filter/haogstc/local/HAOGSTCLocalFilterComponent.java
@@ -16,19 +16,25 @@ package com.kgolembniak.carrot.filter.haogstc.local;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import com.dawidweiss.carrot.core.local.*;
-import com.dawidweiss.carrot.core.local.clustering.*;
-import com.dawidweiss.carrot.filter.stc.algorithm.*;
+import com.dawidweiss.carrot.core.local.LocalComponent;
+import com.dawidweiss.carrot.core.local.LocalControllerContext;
+import com.dawidweiss.carrot.core.local.LocalFilterComponent;
+import com.dawidweiss.carrot.core.local.ProcessingException;
+import com.dawidweiss.carrot.core.local.RequestContext;
+import com.dawidweiss.carrot.core.local.clustering.RawClustersConsumer;
+import com.dawidweiss.carrot.core.local.clustering.RawClustersProducer;
+import com.dawidweiss.carrot.core.local.clustering.TokenizedDocument;
+import com.dawidweiss.carrot.core.local.clustering.TokenizedDocumentsConsumer;
+import com.dawidweiss.carrot.core.local.clustering.TokenizedDocumentsProducer;
 import com.dawidweiss.carrot.core.local.profiling.ProfiledLocalFilterComponentBase;
 import com.dawidweiss.carrot.filter.stc.StcParameters;
-import com.dawidweiss.carrot.filter.stc.suffixtree.ExtendedBitSet;
-import com.kgolembniak.carrot.filter.haog.algorithm.CombinedVertex;
-import com.kgolembniak.carrot.filter.haog.algorithm.GraphProcessor;
-import com.kgolembniak.carrot.filter.haog.algorithm.Vertex;
+import com.dawidweiss.carrot.filter.stc.algorithm.BaseCluster;
+import com.dawidweiss.carrot.filter.stc.algorithm.DocReference;
+import com.dawidweiss.carrot.filter.stc.algorithm.STCEngine;
+import com.kgolembniak.carrot.filter.haog.algorithm.STCGroupper;
 
 /**
  * Implementation of Hierarchical Arrangement of Overlapping Groups.
@@ -148,12 +154,10 @@ public class HAOGSTCLocalFilterComponent extends ProfiledLocalFilterComponentBas
 
         //HAOG part
         connectBaseClusters(clusters);
-        GraphProcessor processor = new GraphProcessor();
-        List graph = processor.getGraphFromBCList(clusters);
-		List cleanGraph = processor.removeCycle(graph);
-		List kernel  = processor.findKernel(cleanGraph);
-		createRawClusters(kernel);
-    
+        STCGroupper groupper = 
+        	new STCGroupper(clusters, documents, params, rawClustersConsumer);
+        groupper.process();
+   	
         stopTimer();
 
         super.endProcessing();
@@ -196,71 +200,5 @@ public class HAOGSTCLocalFilterComponent extends ProfiledLocalFilterComponentBas
             }
         }
     }
-
-    /**
-     * This method creates output raw claster from internal results representation.
-     * @throws ProcessingException 
-     */
-    private void createRawClusters(List kernel) throws ProcessingException {
-		CombinedVertex kernelVertex;
-		RawClusterBase rawCluster;
-		//Reverse makes more complex cluster to be first on the list.
-    	for (int i1=kernel.size()-1; i1>=0; i1--){
-    		kernelVertex = (CombinedVertex) kernel.get(i1);
-    		rawCluster = createRCFromCV(kernelVertex);
-    		rawClustersConsumer.addCluster(rawCluster);
-		}
-	}
-
-    /**
-     * This method converts internal algorithm representation of vertices
-     * to clusters suitalbe for displaying later.
-     * @param vertex
-     * @return cluster
-     */
-	private RawClusterBase createRCFromCV(CombinedVertex vertex) {
-		RawClusterBase rawCluster = new RawClusterBase();
-
-		RawDocument rawDocument;
-		TokenizedDocument tokenizedDoc;		
-		
-		rawCluster.addLabel(vertex.getVertexDescription(params));
-		
-		ExtendedBitSet docSet = vertex.getDocuments();
-		//for all documents in node(cluster)
-		for (Iterator doc = docSet.iterator(); doc.hasNext();){
-			final int docIndex = ((Integer) doc.next()).intValue();
-			tokenizedDoc = (TokenizedDocument) documents.get(docIndex);
-	        rawDocument = (RawDocument) tokenizedDoc.getProperty(
-	        		TokenizedDocument.PROPERTY_RAW_DOCUMENT);
-        	rawCluster.addDocument(rawDocument);
-		}
-
-		List successors = vertex.getSuccList();
-		RawClusterBase subCluster;
-		CombinedVertex successor;
-		CombinedVertex sibling;
-		
-		boolean belongsToKernel;
-		//for all children of this node
-		for (int i1=0; i1<successors.size(); i1++){
-			belongsToKernel = true;
-			successor = (CombinedVertex) successors.get(i1);
-			//Check if vertex isn't also grandchild =
-			//Check if vertex belongs to kernel of subgraph
-			for (int i2=0; i2<successors.size(); i2++) {
-				sibling = (CombinedVertex) successors.get(i2); 
-				if (sibling.getSuccList().contains(successor)){
-					belongsToKernel = false;
-				}
-			}
-			
-			if (belongsToKernel){
-				subCluster = createRCFromCV(successor);
-				rawCluster.addSubcluster(subCluster);
-			}
-		}
-		return rawCluster;
-	}
 
 }
