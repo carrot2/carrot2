99e7ff520748c3e4398beecf7af64cd32fb82ad2
Dawid Weiss <dawid.weiss@carrotsearch.com>
2006-04-26 07:58:10 +0000

    - Added Proguard obfuscation infrastructure to the project. - Several components now include their own proguard rulesets. These can be combined at build time from the dependency descriptor file. An example of how it's done is in the demo browser application. Try: ant clean dist obfuscate and you'll get the obfuscated version in tmp/dist-obfuscated. There are some comments in the code, I think the entire obfuscation target could be moved to the common master.xml file, but I postpone it for now. Let me know what you think -- my gut feeling from development is that it is a bit easier to work with the rule file now. Each component can decide which classes it must keep (it is mainly due to resources loaded with instance.getClass().getResource....).
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1016 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/applications/carrot2-demo-browser/demo.sh
+++ carrot2/applications/carrot2-demo-browser/demo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PATH=$PATH:tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux
-java $JAVA_OPTS -Djava.library.path=tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux -Djava.ext.dirs=tmp/dist/deps-carrot2-demo-browser-jar -cp tmp/dist/carrot2-demo-browser.jar carrot2.demo.DemoSwing
+java $JAVA_OPTS -Djava.library.path=tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux -Djava.ext.dirs=tmp/dist/.:tmp/dist/deps-carrot2-demo-browser-jar carrot2.demo.DemoSwing
