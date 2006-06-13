ce55a3985e066091a1e948a20b64ca4f5d0c55c6
Dawid Weiss <dawid.weiss@carrotsearch.com>
2006-06-13 08:53:33 +0000

    - Added FuzzyAnts to the demo browser.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1083 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ carrot2/applications/carrot2-demo-browser/src/carrot2/demo/settings/FuzzyAntsSettings.java
@@ -0,0 +1,61 @@
+
+/*
+ * Carrot2 project.
+ *
+ * Copyright (C) 2002-2006, Dawid Weiss, Stanisław Osiński.
+ * Portions (C) Contributors listed in "carrot2.CONTRIBUTORS" file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the repository checkout or at:
+ * http://www.carrot2.org/carrot2.LICENSE
+ */
+
+package carrot2.demo.settings;
+
+import java.awt.Frame;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JComponent;
+
+import carrot2.demo.ProcessSettings;
+import carrot2.demo.ProcessSettingsBase;
+
+import com.dawidweiss.carrot.filter.stc.StcParameters;
+
+import fuzzyAnts.FuzzyAntsParameters;
+
+/**
+ * Settings class for FuzzyAnts.
+ * 
+ * @author Dawid Weiss
+ */
+public class FuzzyAntsSettings extends ProcessSettingsBase implements ProcessSettings {
+
+    public FuzzyAntsSettings() {
+        this.params = new FuzzyAntsParameters().toMap();
+    }
+
+    private FuzzyAntsSettings(Map params) {
+        this.params = new HashMap(params);
+    }
+
+    public boolean hasSettings() {
+        return true;
+    }
+
+    public boolean isConfigured() {
+        return true;
+    }
+
+    public ProcessSettings createClone() {
+        synchronized (this) {
+            return new FuzzyAntsSettings(params);
+        }
+    }
+
+    public JComponent getSettingsComponent(Frame owner) {
+        return new FuzzyAntsSettingsDialog(this);
+    }
+}
