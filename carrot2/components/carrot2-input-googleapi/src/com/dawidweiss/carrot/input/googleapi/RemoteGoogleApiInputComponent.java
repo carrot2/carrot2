893e21b4c3466a6fe1eb992d329addc58732f202
Karol Golembniak <man07@wp.pl>
2006-06-22 19:14:33 +0000

    Fixed saving query text to xml.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1105 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/components/carrot2-input-googleapi/src/com/dawidweiss/carrot/input/googleapi/RemoteGoogleApiInputComponent.java
+++ carrot2/components/carrot2-input-googleapi/src/com/dawidweiss/carrot/input/googleapi/RemoteGoogleApiInputComponent.java
@@ -133,7 +133,7 @@ public class RemoteGoogleApiInputComponent
                 
                 output.write("<searchresult>\n");
                 output.write("<query requested-results=\"" + requestedResultsNumber + "\">");
-                serializer.toValidXmlText(query, false);
+                output.write(serializer.toValidXmlText(query, false));
                 output.write("</query>\n");
 
                 int id = 0;
