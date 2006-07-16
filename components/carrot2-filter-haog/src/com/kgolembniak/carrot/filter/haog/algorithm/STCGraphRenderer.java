ee647efcec792e77e7d23a41b9ca54d8772666cc
Karol Golembniak <man07@wp.pl>
2006-07-16 15:31:28 +0000

    Added methods based on kernel discovering to create hierarchy.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@1226 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- components/carrot2-filter-haog/src/com/kgolembniak/carrot/filter/haog/algorithm/STCGraphRenderer.java
+++ components/carrot2-filter-haog/src/com/kgolembniak/carrot/filter/haog/algorithm/STCGraphRenderer.java
@@ -21,8 +21,9 @@ import com.dawidweiss.carrot.core.local.clustering.RawClusterBase;
 import com.dawidweiss.carrot.core.local.clustering.RawClustersConsumer;
 import com.dawidweiss.carrot.core.local.clustering.RawDocument;
 import com.dawidweiss.carrot.core.local.clustering.TokenizedDocument;
-import com.dawidweiss.carrot.filter.stc.StcParameters;
 import com.dawidweiss.carrot.filter.stc.suffixtree.ExtendedBitSet;
+import com.kgolembniak.carrot.filter.haog.measure.Statistics;
+import com.kgolembniak.carrot.filter.stc.STCParameters;
 
 /**
  * This class renders {@link com.dawidweiss.carrot.core.local.clustering.RawCluster}
@@ -39,8 +40,16 @@ public class STCGraphRenderer implements GraphRenderer {
 	/**
 	 * Parameters for clusters creation.
 	 */
-	private StcParameters params;
-    
+	private STCParameters params;
+	
+	private GraphProcessor processor;
+	
+	private int repeatedClusters;
+	
+	public STCGraphRenderer(){
+		processor = new GraphProcessor();
+	}
+	
 	/**
      * This method creates output raw claster from internal results representation.
      * @throws ProcessingException 
@@ -48,41 +57,67 @@ public class STCGraphRenderer implements GraphRenderer {
     public void renderRawClusters(List kernel, List documents, Object params,
    	RawClustersConsumer consumer) throws ProcessingException {
     	this.documents = documents;
-    	this.params = (StcParameters) params;
+    	this.params = (STCParameters) params;
     	
     	CombinedVertex kernelVertex;
 		RawClusterBase rawCluster;
 		//Reverse makes more complex cluster to be first on the list.
     	for (int i1=kernel.size()-1; i1>=0; i1--){
     		kernelVertex = (CombinedVertex) kernel.get(i1);
-    		rawCluster = createRCFromCV(kernelVertex);
+    		if (this.params.getHierarchyCreationWay()==0) {
+        		rawCluster = createRCFromCVSimple(kernelVertex);
+    		} else {
+        		rawCluster = createRCFromCVFull(kernelVertex);
+    		}
     		consumer.addCluster(rawCluster);
 		}
+        Statistics.getInstance().setValue("Clusters repeated in hierarhy", 
+        		new Integer(this.repeatedClusters));
 	}
 
     /**
      * This method converts internal algorithm representation of vertices
-     * to clusters suitalbe for displaying later.
+     * to clusters suitalbe for displaying later. It uses kernel discovering 
+     * to decide to add cluster to hierarchy.
      * @param vertex
      * @return cluster
      */
-	private RawClusterBase createRCFromCV(CombinedVertex vertex) {
+    private RawClusterBase createRCFromCVFull(CombinedVertex vertex)
+    throws ProcessingException {
 		RawClusterBase rawCluster = new RawClusterBase();
 
-		RawDocument rawDocument;
-		TokenizedDocument tokenizedDoc;		
+		getDocumentsForCluster(vertex, rawCluster);		
 		
-		rawCluster.addLabel(vertex.getVertexDescription(params));
-		
-		ExtendedBitSet docSet = vertex.getDocuments();
-		//for all documents in node(cluster)
-		for (Iterator doc = docSet.iterator(); doc.hasNext();){
-			final int docIndex = ((Integer) doc.next()).intValue();
-			tokenizedDoc = (TokenizedDocument) documents.get(docIndex);
-	        rawDocument = (RawDocument) tokenizedDoc.getProperty(
-	        		TokenizedDocument.PROPERTY_RAW_DOCUMENT);
-        	rawCluster.addDocument(rawDocument);
-		}
+    	List subGraph = vertex.getSuccList();
+    	subGraph = processor.repairPredLists(subGraph);
+    	List subgraphKernel = processor.findKernel(subGraph);
+    	
+    	CombinedVertex subVertex = null;
+    	for (Iterator it = subgraphKernel.iterator(); it.hasNext();){
+    		subVertex = (CombinedVertex) it.next();
+    		if (subVertex.isUsed()) {
+    			repeatedClusters ++;
+    		} else {
+    			subVertex.setUsed(true);
+    		}
+    		RawClusterBase subCluster = createRCFromCVFull(subVertex);
+    		rawCluster.addSubcluster(subCluster);
+    	}
+    	
+		return rawCluster;
+	}
+
+    /**
+     * This method converts internal algorithm representation of vertices
+     * to clusters suitalbe for displaying later. It uses simple grandchild 
+     * criterion to decide to add cluster to hierarchy.
+     * @param vertex
+     * @return cluster
+     */
+	private RawClusterBase createRCFromCVSimple(CombinedVertex vertex) {
+		RawClusterBase rawCluster = new RawClusterBase();
+
+		getDocumentsForCluster(vertex, rawCluster);
 
 		List successors = vertex.getSuccList();
 		RawClusterBase subCluster;
@@ -104,11 +139,38 @@ public class STCGraphRenderer implements GraphRenderer {
 			}
 			
 			if (belongsToKernel){
-				subCluster = createRCFromCV(successor);
+	    		if (successor.isUsed()) {
+	    			repeatedClusters ++;
+	    		} else {
+	    			successor.setUsed(true);
+	    		}
+				subCluster = createRCFromCVSimple(successor);
 				rawCluster.addSubcluster(subCluster);
 			}
 		}
 		return rawCluster;
+	}
+
+	/**
+	 * This method adds documents contained in given vertex to given cluster.
+	 * @param vertex - Vertex containing documents
+	 * @param rawCluster - Cluster to add documents to.
+	 */
+	private void getDocumentsForCluster(CombinedVertex vertex, RawClusterBase rawCluster) {
+		RawDocument rawDocument;
+		TokenizedDocument tokenizedDoc;		
+		
+		rawCluster.addLabel(vertex.getVertexDescription(params));
+		
+		ExtendedBitSet docSet = vertex.getDocuments();
+		//for all documents in node(cluster)
+		for (Iterator doc = docSet.iterator(); doc.hasNext();){
+			final int docIndex = ((Integer) doc.next()).intValue();
+			tokenizedDoc = (TokenizedDocument) documents.get(docIndex);
+	        rawDocument = (RawDocument) tokenizedDoc.getProperty(
+	        		TokenizedDocument.PROPERTY_RAW_DOCUMENT);
+        	rawCluster.addDocument(rawDocument);
+		}
 	}
 
 }
