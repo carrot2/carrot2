d9ba3a61f4fda14fbbf3c81d1bd527d8435365b5
Dawid Weiss <dawid.weiss@carrotsearch.com>
2006-07-27 18:41:13 +0000

    repackaging to org.carrot2.* (unstable commit)
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1270 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ carrot2/components/carrot2-util-tokenizer/src/org/carrot2/util/tokenizer/languages/english/English.java
@@ -0,0 +1,96 @@
+
+/*
+ * Carrot2 project.
+ *
+ * Copyright (C) 2002-2006, Dawid Weiss, Stanisław Osiński.
+ * Portions (C) Contributors listed in "carrot2.CONTRIBUTORS" file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the repository checkout or at:
+ * http://www.carrot2.org/carrot2.LICENSE
+ */
+
+package org.carrot2.util.tokenizer.languages.english;
+
+import java.io.*;
+import java.util.*;
+
+import org.carrot2.core.linguistic.*;
+import org.carrot2.stemming.snowball.SnowballStemmersFactory;
+import org.carrot2.util.WordLoadingUtils;
+
+import com.dawidweiss.carrot.filter.snowball.*;
+import org.carrot2.util.tokenizer.languages.*;
+import org.carrot2.util.tokenizer.parser.*;
+import com.stachoodev.util.common.*;
+
+/**
+ * An implementation of {@link Language} interface
+ * for the English language.
+ * 
+ * <p>Requires <code>carrot2-stemmer-snowball</code> for
+ * stemming capabilities
+ * 
+ * @author Dawid Weiss
+ * @version $Revision$
+ */
+public class English extends StemmedLanguageBase {
+
+    /**
+     * A set of stopwords for this language.
+     */
+    private final static Set stopwords;
+    
+    /*
+     * Load stopwords from an associated resource.
+     */
+    static {
+        String resourceName = "/com/dawidweiss/carrot/util/tokenizer/languages/english/stopwords.en";
+        try {
+			stopwords = WordLoadingUtils.loadWordSet(
+                    resourceName, English.class.getResourceAsStream(resourceName));
+		} catch (IOException e) {
+            throw new RuntimeException("Could not load the required" +
+                    "resource: " + resourceName);
+		}
+    }
+
+    
+    /**
+     * Public constructor. 
+     */
+    public English() {
+        super.setStopwords(stopwords);
+    }
+    
+	/**
+     * Creates a new instance of a {@link LanguageTokenizer} for 
+     * this language.
+     * 
+	 * @see org.carrot2.util.tokenizer.languages.StemmedLanguageBase#createTokenizerInstanceInternal()
+	 */
+	protected LanguageTokenizer createTokenizerInstanceInternal() {
+        // TODO: This tokenizer is never returned to the pool, but actually
+        // all languages could share the same tokenizer pool
+        return WordBasedParserFactory.Default.borrowParser();
+	}
+
+	/**
+     * @return Language code: <code>en</code>
+	 * @see org.carrot2.core.linguistic.Language#getIsoCode()
+	 */
+	public String getIsoCode() {
+        return "en";
+	}
+
+    /** 
+     * Return an instance of a stemmer for this language
+     * from the Snowball component.
+     *  
+     * @see org.carrot2.util.tokenizer.languages.LanguageBase#createStemmerInstance()
+     */
+    protected Stemmer createStemmerInstance() {
+        return SnowballStemmersFactory.getInstance(getIsoCode());
+    }
+}
---
+++ carrot2/components/carrot2-util-tokenizer/src/org/carrot2/util/tokenizer/languages/polish/Polish.java
@@ -0,0 +1,93 @@
+
+/*
+ * Carrot2 project.
+ *
+ * Copyright (C) 2002-2006, Dawid Weiss, Stanisław Osiński.
+ * Portions (C) Contributors listed in "carrot2.CONTRIBUTORS" file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the repository checkout or at:
+ * http://www.carrot2.org/carrot2.LICENSE
+ */
+
+package org.carrot2.util.tokenizer.languages.polish;
+
+import java.io.*;
+import java.util.*;
+
+import org.carrot2.core.linguistic.*;
+import org.carrot2.util.WordLoadingUtils;
+
+import org.carrot2.util.tokenizer.languages.*;
+import org.carrot2.util.tokenizer.parser.*;
+import com.stachoodev.util.common.*;
+
+/**
+ * An implementation of {@link Language} interface
+ * for the Polish language.
+ * 
+ * <p>Requires <code>carrot2-stemmer-lametyzator</code> for
+ * stemming capabilities.
+ * 
+ * @author Dawid Weiss
+ * @version $Revision$
+ */
+public class Polish extends StemmedLanguageBase {
+
+    /**
+     * A set of stopwords for this language.
+     */
+    private final static Set stopwords;
+    
+    /*
+     * Load stopwords from an associated resource.
+     */
+    static {
+        String resourceName = "/com/dawidweiss/carrot/util/tokenizer/languages/polish/stopwords.pl";
+        try {
+			stopwords = WordLoadingUtils.loadWordSet(
+                    resourceName, Polish.class.getResourceAsStream(resourceName));
+		} catch (IOException e) {
+            throw new RuntimeException("Could not load the required" +
+                    "resource: " + resourceName);
+		}
+    }
+
+    
+    /**
+     * Public constructor. 
+     */
+    public Polish() {
+        super.setStopwords(stopwords);
+    }
+    
+	/**
+     * Creates a new instance of a {@link LanguageTokenizer} for 
+     * this language.
+     * 
+	 * @see org.carrot2.util.tokenizer.languages.StemmedLanguageBase#createTokenizerInstanceInternal()
+	 */
+	protected LanguageTokenizer createTokenizerInstanceInternal() {
+        // TODO: This tokenizer is never returned to the pool, but actually
+        // all languages could share the same tokenizer pool
+        return WordBasedParserFactory.Default.borrowParser();
+	}
+
+	/**
+     * @return Language code: <code>pl</code>
+	 * @see org.carrot2.core.linguistic.Language#getIsoCode()
+	 */
+	public String getIsoCode() {
+        return "pl";
+	}
+
+    /** 
+     * Return an instance of a Polish stemmer.
+     *  
+     * @see org.carrot2.util.tokenizer.languages.LanguageBase#createStemmerInstance()
+     */
+    protected Stemmer createStemmerInstance() {
+		return new org.carrot2.stemming.stempelator.Stempelator();
+    }
+}
