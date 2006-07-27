addcc2215179b70b0a0fc11b0a13490c3deefbc7
Dawid Weiss <dawid.weiss@carrotsearch.com>
2006-07-27 18:42:02 +0000

    repackaging to org.carrot2.* (unstable commit)
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1271 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ carrot2/components/carrot2-util-tokenizer/src-test/org/carrot2/util/tokenizer/languages/FinnishTest.java
@@ -0,0 +1,44 @@
+
+/*
+ * Carrot2 project.
+ *
+ * Copyright (C) 2002-2006, Dawid Weiss, Stanisław Osiński.
+ * Portions (C) Contributors listed in "carrot2.CONTRIBUTORS" file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the repository checkout or at:
+ * http://www.carrot2.org/carrot2.LICENSE
+ */
+
+package org.carrot2.util.tokenizer.languages;
+
+import org.carrot2.core.linguistic.Language;
+import org.carrot2.util.tokenizer.languages.finnish.Finnish;
+
+/**
+ * Test of the Finnish language.
+ *  
+ * @author Dawid Weiss
+ * @version $Revision$
+ */
+public class FinnishTest extends LanguageImplTestBase {
+
+	public FinnishTest() {
+		super();
+	}
+
+	public FinnishTest(String arg0) {
+		super(arg0);
+	}
+
+	protected Language getLanguageInstance() {
+        return new Finnish();
+	}
+
+    protected String [][] getWordsToCompare() {
+        return new String [][] {
+                /* TODO: Add words here. */
+        };
+    }
+}
