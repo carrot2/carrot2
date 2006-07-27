9e235560f611cf549ef3dce8e6a75a8a39b1e69a
Dawid Weiss <dawid.weiss@carrotsearch.com>
2006-07-27 15:27:29 +0000

    repackaging to org.carrot2.*
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@1261 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ components/carrot2-ant-extensions/src/org/carrot2/ant/tasks/BaseDependencyPathTask.java
@@ -0,0 +1,74 @@
+
+/*
+ * Carrot2 project.
+ *
+ * Copyright (C) 2002-2006, Dawid Weiss, Stanisław Osiński.
+ * Portions (C) Contributors listed in "carrot2.CONTRIBUTORS" file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the repository checkout or at:
+ * http://www.carrot2.org/carrot2.LICENSE
+ */
+
+package org.carrot2.ant.tasks;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.Path;
+import org.carrot2.ant.deps.ComponentDependency;
+import org.carrot2.ant.types.DependencyPath;
+
+
+/**
+ * A {@link Task} that accepts <code>dependencies</code> subelement
+ * with a path to <code>*.dep.xml</code> files. 
+ * 
+ * @author Dawid Weiss
+ */
+public class BaseDependencyPathTask extends Task {
+    
+    public BaseDependencyPathTask() {
+    }
+
+    /**
+     * A list of <code>FileSet</code> objects
+     * that point to component dependency descriptors.
+     * This list is used when searching for named
+     * dependencies. 
+     */
+    private LinkedList dependencies = new LinkedList();
+    
+    /**
+     * Crates a new path with a set of dependency files to scan.
+     */
+    public Path createDependencies() {
+        final Path newPath = new DependencyPath(getProject());
+        dependencies.add(newPath);
+        return newPath;
+    }
+
+    /**
+     * Returns a map of <code>identifier</code>-{@link ComponentDependency} entries
+     * corresponding to <code>*.dep.xml</code> files found in path.
+     */
+    public Map getAllComponents() throws IOException {
+        final HashMap all = new HashMap();
+        if (this.dependencies.size() == 0) {
+            // do nothing, empty map is returned.
+        } else if (this.dependencies.size() == 1) {
+            all.putAll(((DependencyPath) dependencies.getFirst()).getAllComponents());
+        } else {
+            for (Iterator i = dependencies.iterator(); i.hasNext();) {
+                final DependencyPath dep = (DependencyPath) i.next();
+                final HashMap comps = dep.getAllComponents();
+                for (Iterator j = comps.values().iterator(); j.hasNext();) {
+                    Utils.addComponentToMap((ComponentDependency) j.next(), all);
+                }
+            }
+        }
+        return all;
+    }
+}
---
+++ components/carrot2-ant-extensions/src/org/carrot2/ant/tasks/MostRecentFileDateTask.java
@@ -0,0 +1,220 @@
+
+/*
+ * Carrot2 project.
+ *
+ * Copyright (C) 2002-2006, Dawid Weiss, Stanisław Osiński.
+ * Portions (C) Contributors listed in "carrot2.CONTRIBUTORS" file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the repository checkout or at:
+ * http://www.carrot2.org/carrot2.LICENSE
+ */
+
+package org.carrot2.ant.tasks;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import org.apache.tools.ant.*;
+import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.util.FileUtils;
+
+/**
+ * An ANT task that sets the name of a given
+ * property to the timestamp of the least recently
+ * modified file in a provided <code>FileSet</code>.
+ * The timestamp is expressed in milliseconds since
+ * Jan 1, 1970.
+ */
+public class MostRecentFileDateTask extends Task {
+    private CustomFormat format;
+
+    /** Name of a property that will be initialized with formatted date
+     * of the most recent file. */
+    private String dateProperty;
+
+    /** A list of <code>FileSet</code> objects. */
+    private List fileSets = new LinkedList();
+    
+    /** Name of the property to set. */
+    private String millisProperty;
+    
+    /**
+     * If <code>true</code>, the result is stored in {@link #lastModified}
+     * field.
+     */
+    private boolean storeInField;
+    
+    /**
+     * If {@link #storeInField} is <code>true</code>, this field
+     * will contain the result of this task after invoking {@link #execute()}.
+     */
+    private long lastModified;
+
+    /** 
+     *  If <code>true</code> an exception is thrown
+     *  if no files are included in all of the filesets.
+     */
+    private boolean exceptionOnEmpty;
+
+    /**
+     * A simple inner class for providing custom date format.
+     */
+    public static class CustomFormat {
+
+        private String pattern;
+
+        public void setPattern(String pattern) {
+            this.pattern = pattern;
+        }
+
+        public String getFormatted(long millis) {
+            if (this.pattern == null)
+                throw new BuildException("Pattern attribute must be provided");
+            SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+            return sdf.format(new Date(millis));
+        }
+    }
+    
+    /**
+     * If set to true, the task will throw an exception
+     * if there are no files included in all of the 
+     * combined filesets.
+     */    
+    public void setExceptionOnEmpty(boolean value) {
+        this.exceptionOnEmpty = value;
+    }
+    
+    /** 
+     * Adds a <code>FileSet</code> to the
+     * list of files to check.
+     */
+    public void addFileset(FileSet fs) {
+        fileSets.add( fs );
+    }
+    
+    /**
+     * Sets the name of the property to initialize
+     * with milliseconds.
+     */
+    public void setMillisProperty(String name) {
+        this.millisProperty = name;
+    }
+    
+    /**
+     * Sets the name of the property to initialize
+     * with formatted date.
+     */
+    public void setDateProperty(String name) {
+        this.dateProperty = name;
+    }
+    
+    /**
+     * Sets {@link #storeInField}.
+     */
+    public void setStoreInField(boolean v) {
+        this.storeInField = v;
+    }
+    
+    public long getLastModified() {
+        return lastModified;
+    }
+
+    public CustomFormat createFormat() {
+        if (format != null)
+            throw new BuildException("Only one format child element is allowed.");
+
+        CustomFormat cts = new CustomFormat();
+        format = cts;
+        return cts;
+    } 
+
+    /**
+     * Verifies that all parameters are provided and correct;
+     * @throws BuildException
+     */
+    protected void checkParameters() throws BuildException {
+        if (this.fileSets.size() == 0) {
+            throw new BuildException("At least one embedded FileSet is required.");
+        }
+        
+        if (this.millisProperty == null && this.dateProperty == null && storeInField == false) {
+            throw new BuildException("dateProperty or millisProperty attribute must be provided.");
+        }
+    }
+
+    /**
+     * Verifies parameters and executes the task.
+     * @throws BuildException 
+     */
+	public void execute() throws BuildException {
+		super.execute();
+
+        checkParameters();
+        
+        FileUtils fileUtils = FileUtils.newFileUtils();
+        
+        int includedFiles = 0;
+        long lastModifiedDate = 0;
+        ArrayList lastModifiedFiles = new ArrayList();
+        
+        // Iterate filesets and get update the most recent 
+        // modification date accordingly.
+        for (Iterator fsIterator = this.fileSets.iterator(); fsIterator.hasNext(); ) {
+            FileSet fs = (FileSet) fsIterator.next();
+
+            DirectoryScanner ds = fs.getDirectoryScanner(getProject());
+            File fromDir = fs.getDir(getProject());
+            String [] srcFiles = ds.getIncludedFiles();
+
+            for (int fIndex = 0; fIndex < srcFiles.length; fIndex++) {
+                File file = fileUtils.resolveFile(fromDir, srcFiles[fIndex]);
+                if (includedFiles == 0) {
+                    lastModifiedDate = file.lastModified();
+                } else {
+                    if (lastModifiedDate == file.lastModified()) {
+                        lastModifiedFiles.add( file );
+                    } else if (lastModifiedDate < file.lastModified()) {
+                        lastModifiedDate = file.lastModified();
+                        lastModifiedFiles.clear();
+                        lastModifiedFiles.add(file);
+                    } 
+                }
+                includedFiles++;
+            }
+        }
+
+        if (includedFiles == 0) {
+            if (this.exceptionOnEmpty) {
+                throw new BuildException("No files included in the fileset.");
+            }
+            log("No files included.", Project.MSG_INFO);
+        } else {
+            log("Last modified "
+                + "[millis: " + lastModifiedDate + ", "
+                + "date: " + new Date( lastModifiedDate )
+                + "]: ", Project.MSG_VERBOSE);
+    
+            // set the property to the most recently modified file
+            if (this.millisProperty != null) {
+                getProject().setNewProperty(this.millisProperty,
+                    Long.toString(lastModifiedDate));
+            }
+            if (this.dateProperty != null) {
+                String out;
+                if (this.format != null) {
+                    out = format.getFormatted(lastModifiedDate);
+                } else {
+                    SimpleDateFormat sdf = new SimpleDateFormat();
+                    out = sdf.format(new Date(lastModifiedDate));
+                }
+                getProject().setNewProperty(this.dateProperty, out);
+            }
+            if (this.storeInField) {
+                this.lastModified = lastModifiedDate;
+            }
+        }
+	}
+}
