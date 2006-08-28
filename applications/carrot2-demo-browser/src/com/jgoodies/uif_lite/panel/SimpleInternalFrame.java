7986a32055bdae01f9798c95edca6e442b18ba94
Dawid Weiss <dawid.weiss@carrotsearch.com>
2006-08-28 14:42:11 +0000

    Licenses cleaned up. Carrot2 files - licenses updated.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@1373 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- applications/carrot2-demo-browser/src/com/jgoodies/uif_lite/panel/SimpleInternalFrame.java
+++ applications/carrot2-demo-browser/src/com/jgoodies/uif_lite/panel/SimpleInternalFrame.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003 JGoodies Karsten Lentzsch. All Rights Reserved.
+ * Copyright (c) 2000-2006 JGoodies Karsten Lentzsch. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
--- components/carrot2-util-gzip/src/com/planetj/servlet/filter/compression/EmptyEnumeration.java
+++ components/carrot2-util-gzip/src/com/planetj/servlet/filter/compression/EmptyEnumeration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 Sean Owen
+ * Copyright 2004-2006 Sean Owen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
