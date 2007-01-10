385391de8b0a9152539364feaa2410921ff3ab87
Dawid Weiss <dawid.weiss@carrotsearch.com>
2007-01-10 20:26:12 +0000

    Added a launcher application to the demo browser (avoids overriding java.ext.dirs).
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1557 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/applications/carrot2-demo-browser/demo.sh
+++ carrot2/applications/carrot2-demo-browser/demo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
 PATH=$PATH:tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux
-java $JAVA_OPTS -Djava.library.path=tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux -Djava.ext.dirs=tmp/dist/.:tmp/dist/deps-carrot2-demo-browser-jar org.carrot2.demo.DemoSwing
+
+java $JAVA_OPTS -Djava.library.path=tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux -jar tmp/dist/deps-carrot2-demo-browser-jar/carrot2-launcher.jar -cpdir tmp/dist/. -cpdir tmp/dist/deps-carrot2-demo-browser-jar org.carrot2.demo.DemoSwing
