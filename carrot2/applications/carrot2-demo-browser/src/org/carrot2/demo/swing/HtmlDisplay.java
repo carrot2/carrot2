e684f74f123698a96df2dc7f77033dcb290bd962
Dawid Weiss <dawid.weiss@carrotsearch.com>
2007-03-09 09:33:39 +0000

    - Upgrade of JDIC libraries (the old JDIC had hardcoded paths to WebStart caches, this could not work).
    Hopefully fixes issues: 
    http://issues.carrot2.org/browse/CARROT-38
    http://issues.carrot2.org/browse/CARROT-5
    
    
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1626 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/applications/carrot2-demo-browser/src/org/carrot2/demo/swing/HtmlDisplay.java
+++ carrot2/applications/carrot2-demo-browser/src/org/carrot2/demo/swing/HtmlDisplay.java
@@ -1,4 +1,3 @@
-
 /*
  * Carrot2 project.
  *
@@ -23,80 +22,86 @@ import org.carrot2.core.clustering.RawDocument;
 import org.carrot2.util.ArrayUtils;
 
 /**
- * All the functionality we need for the demo
- * browser is in this interface. We may then
- * use either Swing's {@link javax.swing.JEditorPane}
- * or JDIC's {@link org.jdesktop.jdic.browser.WebBrowser}
- * class to provide the implementation.
+ * All the functionality we need for the demo browser is in this interface. We may then use either Swing's
+ * {@link javax.swing.JEditorPane} or JDIC's {@link org.jdesktop.jdic.browser.WebBrowser} class to provide the
+ * implementation.
  * 
  * @author Dawid Weiss
  */
-public abstract class HtmlDisplay extends JPanel {
-    private final static boolean useNativeBrowser;
+public abstract class HtmlDisplay extends JPanel
+{
+    private final static Logger logger = Logger.getLogger(HtmlDisplay.class);
+   
+    /**
+     * If set, the native browser has been checked.
+     */
+    private static boolean nativeChecked;
 
-    static {
-        String osname = System.getProperty("os.name");
-        if (osname != null) {
-            osname = osname.toLowerCase(Locale.US);
-        } else {
-            // Ooops, no such property? Try to determine the OS from file separator.
-            if (File.separatorChar == '\\') {
-                osname = "windows?";
-            } else {
-                osname = "unix?";
-            }
-        }
-        
-        boolean nativeBrowser = (!"true".equals(System.getProperty("use.java.browser")) && osname.indexOf("windows") >= 0);
-        if (nativeBrowser) {
-            try {
-                new HtmlDisplayWithJDIC();
-            } catch (Throwable t) {
-                Logger.getLogger(HtmlDisplay.class).warn("Could not instantiate native browser component.", t);
-                nativeBrowser = false;
-            }
-        }
-        useNativeBrowser = nativeBrowser;
-    }
+    /**
+     * If set, the native browser is available and can be used.
+     */
+    private static boolean useNativeBrowser;
 
+    /**
+     * Sets the HTML content of this HTML display.
+     * 
+     * @param htmlContent
+     */
     public abstract void setContent(String htmlContent);
 
-    protected void appendHtmlHeader(StringBuffer buffer) {
+    /**
+     * 
+     */
+    protected void appendHtmlHeader(StringBuffer buffer)
+    {
         buffer.append("<html>");
         buffer.append("<meta><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></meta>\n");
         buffer.append("<body style=\"font-size: 10pt; font-family: Arial, Helvetica, sans-serif;\">");
     }
 
-    protected void appendHtmlTrailer(StringBuffer buffer) {
+    /**
+     * 
+     */
+    protected void appendHtmlTrailer(StringBuffer buffer)
+    {
         buffer.append("</body></html>");
     }
 
-    protected void appendHtmlFor(StringBuffer buffer, Object seqNum, RawDocument doc) {
+    /**
+     * 
+     */
+    protected void appendHtmlFor(StringBuffer buffer, Object seqNum, RawDocument doc)
+    {
         buffer.append("<a href=\"" + doc.getUrl() + "\"><b>");
 
-        if ((doc.getTitle() == null) || (doc.getTitle().length() == 0)) {
+        if ((doc.getTitle() == null) || (doc.getTitle().length() == 0))
+        {
             buffer.append("(no title)");
-        } else {
+        }
+        else
+        {
             buffer.append(doc.getTitle());
         }
         buffer.append("</b></a>");
 
-        if (seqNum != null) {
+        if (seqNum != null)
+        {
             buffer.append("<span style=\"color: gray;\"> [");
             buffer.append(seqNum);
             buffer.append("]</span>");
         }
 
-        if (doc.getProperty(RawDocument.PROPERTY_LANGUAGE) != null) {
-            buffer.append(" [" +
-                doc.getProperty(RawDocument.PROPERTY_LANGUAGE) + "]");
+        if (doc.getProperty(RawDocument.PROPERTY_LANGUAGE) != null)
+        {
+            buffer.append(" [" + doc.getProperty(RawDocument.PROPERTY_LANGUAGE) + "]");
         }
 
         buffer.append("<br>");
 
         String r = (String) doc.getProperty(RawDocument.PROPERTY_SNIPPET);
 
-        if (r != null) {
+        if (r != null)
+        {
             buffer.append(r);
             buffer.append("<br>");
         }
@@ -114,18 +119,62 @@ public abstract class HtmlDisplay extends JPanel {
         buffer.append("</font><br><br>");
     }
 
-    public static HtmlDisplay newHtmlDisplay() {
-        if (useNativeBrowser) {
-            try {
-                return new HtmlDisplayWithJDIC();
-            } catch (Throwable t) {
-                // Ignore, oh well.
+    /**
+     * @return Creates an instance and returns a {@link HtmlDisplay} relevant for the current
+     * platform (native or Swing).
+     */
+    public static synchronized HtmlDisplay newHtmlDisplay()
+    {
+        if (!nativeChecked)
+        {
+            final boolean useJavaBrowser = "true".equals(System.getProperty("use.java.browser"));
+
+            if (!useJavaBrowser && isWindows()) {
+                useNativeBrowser = true;
+                try {
+                    new HtmlDisplayWithJDIC();
+                } catch (Throwable t) {
+                    Logger.getLogger(HtmlDisplay.class).warn("Could not instantiate native browser component.", t);
+                    useNativeBrowser = false;
+                }
             }
+
+            nativeChecked = true;
         }
-        return new HtmlDisplayWithSwing();
+
+        if (useNativeBrowser)
+        {
+            return new HtmlDisplayWithJDIC();
+        }
+        else
+        {
+            return new HtmlDisplayWithSwing();
+        }
     }
 
-    public static boolean isNativeBrowserUsed() {
-        return useNativeBrowser;
+    /**
+     * @return Returns <code>true</code> if the current platform is Windows.
+     */
+    final static boolean isWindows() {
+        return getOsName().indexOf("windows") >= 0;
+    }
+
+    /**
+     *
+     */
+    private static String getOsName()
+    {
+        String osname = System.getProperty("os.name");
+        if (osname != null) {
+            osname = osname.toLowerCase(Locale.US);
+        } else {
+            // Ooops, no such property? Try to determine the OS from file separator.
+            if (File.separatorChar == '\\') {
+                osname = "windows?";
+            } else {
+                osname = "unix?";
+            }
+        }
+        return osname;
     }
 }
