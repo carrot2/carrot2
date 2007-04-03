5b7f68773e86a81543b9f3e58327d0518aa50526
Dawid Weiss <dawid.weiss@carrotsearch.com>
2007-04-03 19:11:05 +0000

    5 seconds timeout.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1656 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/applications/carrot2-demo-browser/demo.bat
+++ carrot2/applications/carrot2-demo-browser/demo.bat
@@ -2,4 +2,4 @@
 
 SET PATH=%PATH%;tmp\dist\deps-carrot2-demo-browser-jar\jdic_windows
 
-java %JAVA_OPTS% -Djava.library.path=tmp\dist\deps-carrot2-demo-browser-jar\jdic_windows -jar tmp\dist\deps-carrot2-demo-browser-jar\carrot2-launcher.jar -cpdir tmp\dist\. -cpdir tmp\dist\deps-carrot2-demo-browser-jar org.carrot2.demo.DemoSplash /org/carrot2/demo/carrot2-splash.png 4 org.carrot2.demo.DemoSwing
+java %JAVA_OPTS% -Djava.library.path=tmp\dist\deps-carrot2-demo-browser-jar\jdic_windows -jar tmp\dist\deps-carrot2-demo-browser-jar\carrot2-launcher.jar -cpdir tmp\dist\. -cpdir tmp\dist\deps-carrot2-demo-browser-jar org.carrot2.demo.DemoSplash /org/carrot2/demo/carrot2-splash.png 5 org.carrot2.demo.DemoSwing
