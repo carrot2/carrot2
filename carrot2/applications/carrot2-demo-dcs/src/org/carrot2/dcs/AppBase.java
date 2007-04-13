7cbfab05528a57efad68f27c63e3bf513b3bbab1
Dawid Weiss <dawid.weiss@carrotsearch.com>
2007-04-13 12:47:34 +0000

    Moved command-line Document Clustering Server from the commercial repository. Major rewrite on the way:    - added dynamic process/ component loading for flexibility.    - process/component errors upon loading are signalled in a user-friendly way and ignored if possible.    - configured logging and various minor tweaks.
    HTTP application (REST/XML-RPC) has been removed temporarily, I'll add it when it's fully functional.
    
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1660 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ carrot2/applications/carrot2-demo-dcs/src/org/carrot2/dcs/AppBase.java
@@ -0,0 +1,152 @@
+package org.carrot2.dcs;
+
+import java.io.File;
+
+import org.apache.commons.cli.*;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ * A base for command-line invoked applications.
+ */
+public abstract class AppBase
+{
+    /**
+     * Logger for the application.
+     */
+    protected final Logger logger;
+
+    /** Command line options. */
+    protected final Options cliOptions = new Options();
+
+    /** Application name. */
+    private final String appName;
+
+    /**
+     * Initializing constructor.
+     * 
+     * @param appName Application name (used in help and logger naming).
+     */
+    protected AppBase(String appName)
+    {
+        this.appName = appName;
+        this.logger = Logger.getLogger(appName);
+    }
+
+    /**
+     * Initializes application context.
+     */
+    protected final void go(String [] args)
+    {
+        initializeOptions(cliOptions);
+
+        if (args.length == 0)
+        {
+            printUsage();
+            return;
+        }
+
+        final Parser parser = new GnuParser();
+        final CommandLine line;
+        try
+        {
+            line = parser.parse(cliOptions, args);
+            go(line);
+        }
+        catch (MissingArgumentException e)
+        {
+            logger.log(Level.FATAL, "Provide the required argument for option " + e.getMessage());
+            printUsage();
+        }
+        catch (MissingOptionException e)
+        {
+            logger.log(Level.FATAL, "Provide the required option " + e.getMessage());
+            printUsage();
+        }
+        catch (UnrecognizedOptionException e)
+        {
+            logger.log(Level.FATAL, e.getMessage() + "\n");
+            printUsage();
+        }
+        catch (ParseException exp)
+        {
+            logger.log(Level.FATAL, "Could not parse command line: " + exp.getMessage());
+            printUsage();
+        }
+    }
+
+    /**
+     * Initializes the processing context.
+     * 
+     * @param descriptorsDir A directory with definitions of processes and descriptors.
+     * @throws Exception if the configuration fails for some reason.
+     */
+    protected ControllerContext initializeContext(File descriptorsDir) throws Exception
+    {
+        logger.info("Initializing components.");
+
+        final ControllerContext context = new ControllerContext();
+
+        if (descriptorsDir.exists() && !descriptorsDir.isDirectory())
+        {
+            throw new Exception("Components directory not found: " + descriptorsDir.getAbsolutePath());
+        }
+
+        context.initialize(descriptorsDir, logger);
+
+        logger.info("Finished initializing components.");
+        return context;
+    }
+
+    /**
+     * Prints usage (options).
+     */
+    protected void printUsage()
+    {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(getCommandName(), cliOptions, true);
+    }
+
+    /**
+     * Override and write your stuff using command line options.
+     */
+    protected abstract void go(CommandLine line);
+
+    /**
+     * Override and initialize options.
+     */
+    protected abstract void initializeOptions(Options options);
+
+    /**
+     * Override to provide command line app name.
+     * 
+     * @return command line app name, eg. c2http
+     */
+    protected String getCommandName()
+    {
+        return this.appName;
+    }
+
+    /**
+     * Returns the logger for this class.
+     */
+    protected final Logger getLogger()
+    {
+        return logger;
+    }
+
+    /**
+     * Get an option, if defined, or return the default value.
+     */
+    protected final Object getOption(CommandLine options, String optionName, Object defaultValue)
+    {
+        if (options.hasOption(optionName))
+        {
+            return options.getOptionObject(optionName);
+        }
+        else
+        {
+            return defaultValue;
+        }
+    }
+}
