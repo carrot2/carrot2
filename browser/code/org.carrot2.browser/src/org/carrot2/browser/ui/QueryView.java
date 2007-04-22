19e16bf23526db0fb8141a4285f1a981dd23da41
Urszula Krukar <ukrukar@gmail.com>
2007-04-22 17:37:06 +0000

    query view improved
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1687 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- browser/code/org.carrot2.browser/src/org/carrot2/browser/ui/QueryView.java
+++ browser/code/org.carrot2.browser/src/org/carrot2/browser/ui/QueryView.java
@@ -3,12 +3,13 @@ package org.carrot2.browser.ui;
 import org.carrot2.browser.Messages;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Scale;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
@@ -17,36 +18,56 @@ public class QueryView extends ViewPart {
 	public static String ID = "org.carrot2.browser.query"; //$NON-NLS-1$
 
 	public void createPartControl(Composite parent) {
-		FormLayout layout = new FormLayout();
+		GridLayout layout = new GridLayout();
 		layout.marginWidth = 5;
 		layout.marginHeight = 5;
+        layout.numColumns = 2;
 		parent.setLayout(layout);
 		
 		Font font = parent.getDisplay().getSystemFont();
-		
-		Label queryLabel = new Label(parent, SWT.LEAD);
+        
+        Label queryLabel = new Label(parent, SWT.LEFT);
 		queryLabel.setText(Messages.getString("queryview.query")); //$NON-NLS-1$
 		queryLabel.setFont(font);
-		FormData labelData = new FormData();
-		labelData.left = new FormAttachment(0, 5);
-		labelData.top = new FormAttachment(0, 5);
-		queryLabel.setLayoutData(labelData);
-		
-		Text queryText = new Text(parent, SWT.LEAD | SWT.BORDER);
+		queryLabel.setLayoutData(new GridData(SWT.LEFT, SWT.BOTTOM, false, false));
+        
+        Text queryText = new Text(parent, SWT.LEFT | SWT.BORDER);
 		queryText.setFont(font);
-		FormData textData = new FormData();
-		textData.left = new FormAttachment(queryLabel, 5);
-		textData.top = new FormAttachment(0, 5);
-		textData.right = new FormAttachment(100, -5);
-		queryText.setLayoutData(textData);
-		
+		queryText.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, false));
+        
+        Label sourceLabel = new Label(parent, SWT.LEFT);
+        sourceLabel.setText(Messages.getString("queryview.source")); //$NON-NLS-1$
+		sourceLabel.setFont(font);
+        sourceLabel.setLayoutData(new GridData(SWT.LEFT, SWT.BOTTOM, false, false));
+        
+        Combo sourceCombo = new Combo(parent, SWT.LEFT | SWT.BORDER);
+        sourceCombo.setFont(font);
+        sourceCombo.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, false));
+        
+        Label resultsLabel = new Label(parent, SWT.LEFT);
+        resultsLabel.setText(Messages.getString("queryview.results")); //$NON-NLS-1$
+        resultsLabel.setFont(font);
+        resultsLabel.setLayoutData(new GridData(SWT.LEFT, SWT.BOTTOM, false, false));
+        
+        Combo resultsCombo = new Combo(parent, SWT.LEFT | SWT.BORDER);
+        resultsCombo.setFont(font);
+        resultsCombo.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, false));
+        
+        Label algoLabel = new Label(parent, SWT.LEFT);
+        algoLabel.setText(Messages.getString("queryview.algorithm")); //$NON-NLS-1$
+        algoLabel.setFont(font);
+        algoLabel.setLayoutData(new GridData(SWT.LEFT, SWT.BOTTOM, false, false));
+        
+        Combo algoCombo = new Combo(parent, SWT.LEFT | SWT.BORDER);
+        algoCombo.setFont(font);
+        algoCombo.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, false));
+        
 		Button submitButton = new Button(parent, SWT.PUSH | SWT.CENTER);
 		submitButton.setText(Messages.getString("queryview.submit")); //$NON-NLS-1$
 		submitButton.setFont(font);
-		FormData buttonData = new FormData();
-		buttonData.left = new FormAttachment(queryText, 0, SWT.LEFT);
-		buttonData.top = new FormAttachment(queryText, 5);
-		submitButton.setLayoutData(buttonData);
+        GridData data = new GridData(SWT.RIGHT, SWT.BOTTOM, false, false);
+        data.horizontalSpan = 2;
+		submitButton.setLayoutData(data);
 	}
 
 	public void setFocus() {
