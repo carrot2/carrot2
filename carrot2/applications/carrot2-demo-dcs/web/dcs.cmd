45090bd6b7ad00a438c033d19d9d601d3cb125f9
Dawid Weiss <dawid.weiss@carrotsearch.com>
2007-05-11 12:31:51 +0000

    Modifications required for http://issues.carrot2.org/browse/LINGO-17.
    - the build process split into two profiles (demo, stripped).
    - launching scripts have an additional classpath entry (Lingo3G properties are read from WEB-INF/classes).
    
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@1736 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/applications/carrot2-demo-dcs/web/dcs.cmd
+++ carrot2/applications/carrot2-demo-dcs/web/dcs.cmd
@@ -3,4 +3,4 @@
 SET DEPLIB=WEB-INF/lib
 SET OPTS=-Xms128m -Xmx384m -XX:NewRatio=1
 
-java %OPTS% -jar %DEPLIB%\carrot2-launcher.jar -cpdir %DEPLIB% org.carrot2.dcs.http.DCSApp %1 %2 %3 %4 %5 %6 %7 %8 %9
+java %OPTS% -jar %DEPLIB%\carrot2-launcher.jar -cp WEB-INF/classes -cpdir %DEPLIB% org.carrot2.dcs.http.DCSApp %1 %2 %3 %4 %5 %6 %7 %8 %9
