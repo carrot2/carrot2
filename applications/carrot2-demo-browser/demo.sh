de4bea41e2d150aa14d59617deb3e8c5691cc9ea
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2007-05-15 18:03:19 +0000

    carrot2-demo-browser: unix launch script updated
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk/carrot2@1755 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- applications/carrot2-demo-browser/demo.sh
+++ applications/carrot2-demo-browser/demo.sh
@@ -3,4 +3,4 @@
 PATH=$PATH:tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux
 
-java $JAVA_OPTS -Djava.library.path=tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux -jar tmp/dist/deps-carrot2-demo-browser-jar/carrot2-launcher.jar -cpdir tmp/dist/. -cpdir tmp/dist/deps-carrot2-demo-browser-jar org.carrot2.demo.DemoSplash /org/carrot2/demo/carrot2-splash.png 5 org.carrot2.demo.DemoSwing
+java $JAVA_OPTS -Djava.library.path=tmp/dist/deps-carrot2-demo-browser-jar/jdic_linux -jar tmp/dist/deps-carrot2-demo-browser-jar/carrot2-launcher.jar -cpdir tmp/dist/. -cpdir tmp/dist/deps-carrot2-demo-browser-jar org.carrot2.demo.DemoSplash /res/browser-splash.png 5 org.carrot2.demo.DemoSwing
