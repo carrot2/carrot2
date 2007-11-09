8a48716863f2a97bbf8fa9f2f03bda6fe8c8a770
Dawid Weiss <dawid.weiss@carrotsearch.com>
2007-11-09 16:41:19 +0000

    Injection note.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@2022 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- code/carrot2-core/src/org/carrot2/core/Controller.java
+++ code/carrot2-core/src/org/carrot2/core/Controller.java
@@ -7,6 +7,19 @@ import java.util.Map;
  */
 public interface Controller
 {
+    /*
+     * Just a note for the future. I guess it would make a lot of sense
+     * to be able to specify injection dependencies as request parameters somehow.
+     * Example: the value of a parameter "tokenizer" (of type "ImplClassTypeMetadata")
+     * would point to the implementation class (or provider?) to use for the injection
+     * container. 
+     * 
+     * My preliminary reading of pico's documentation indicates this would be
+     * feasible and sensible. I'll write a draft solution in guice and pico
+     * tomorrow, we will see what they are like.
+     */
+    
+    
     // listener for documents
     ProcessingResult process(Map<String, Object> requestParameters,
         DocumentSource documentSource, ClusteringAlgorithm clusteringAlgorithm);
