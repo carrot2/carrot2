7da2108b40a16a44418dea8e096e8079c2b19287
Urszula Krukar <ukrukar@gmail.com>
2007-11-19 17:51:38 +0000

    + added browser editor
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@2042 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- browser/spike-solution/org.ukrukar.converter.core/src/org/ukrukar/converter/core/editors/MultiTextViewerEditor.java
+++ browser/spike-solution/org.ukrukar.converter.core/src/org/ukrukar/converter/core/editors/MultiTextViewerEditor.java
@@ -1,20 +1,20 @@
 package org.ukrukar.converter.core.editors;
 
-
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.internal.browser.BrowserViewer;
 import org.eclipse.ui.part.MultiPageEditorPart;
 import org.ukrukar.converter.core.views.DefaultViewerFactory;
 import org.ukrukar.converter.core.views.ITextViewer;
 
 public class MultiTextViewerEditor extends MultiPageEditorPart {
 
-	public static final String ID="org.ukrukar.converter.core.editors.MultiTextViewerEditor";
-	
+	public static final String ID = "org.ukrukar.converter.core.editors.MultiTextViewerEditor";
+
 	ITextViewer viewer;
-	
+
 	@Override
 	protected void createPages() {
 		DefaultViewerFactory factory = new DefaultViewerFactory();
@@ -22,6 +22,11 @@ public class MultiTextViewerEditor extends MultiPageEditorPart {
 		this.addPage(viewer.createControl(this.getContainer()));
 		this.setPageText(0, "Default");
 		viewer.refreshText(this.getEditorInput().getName());
+		BrowserViewer browser = new BrowserViewer(this.getContainer(), 0);
+		browser
+				.setURL("http://kb.adobe.com/selfservice/viewContent.do?externalId=tn_15507");
+		this.addPage(browser);
+		this.setPageText(1, "Browser");
 	}
 
 	@Override
@@ -30,7 +35,7 @@ public class MultiTextViewerEditor extends MultiPageEditorPart {
 		super.init(site, input);
 		this.setPartName(input.getName());
 	}
-	
+
 	@Override
 	public void doSave(IProgressMonitor arg0) {
 	}
@@ -43,5 +48,5 @@ public class MultiTextViewerEditor extends MultiPageEditorPart {
 	public boolean isSaveAsAllowed() {
 		return false;
 	}
-	
+
 }
