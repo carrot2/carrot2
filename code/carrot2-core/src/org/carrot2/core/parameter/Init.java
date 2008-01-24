8be90b1583d7677ef0ba7ab4ddb2268900fa5776
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2008-01-24 20:47:09 +0000

    Missing annotations added.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@2152 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ code/carrot2-core/src/org/carrot2/core/parameter/Init.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.carrot2.core.parameter;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Init
+{
+
+}
