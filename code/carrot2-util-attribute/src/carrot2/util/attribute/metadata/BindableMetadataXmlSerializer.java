940d241062742158b756e16f724c50ba071e99cc
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2008-02-28 21:11:19 +0000

    carrot2-util-attribute: http://issues.carrot2.org/browse/CARROT-149 fixed
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@2236 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- code/carrot2-util-attribute/src/carrot2/util/attribute/metadata/BindableMetadataXmlSerializer.java
+++ code/carrot2-util-attribute/src/carrot2/util/attribute/metadata/BindableMetadataXmlSerializer.java
@@ -16,10 +16,32 @@ public class BindableMetadataXmlSerializer
 
         for (final String path : args)
         {
-            builder.addSourceTree(new File(path));
+            // TODO: remove this nasty hack at some point
+            if (isClassName(path))
+            {
+                builder.addCommonMetadataSource(path);
+            }
+            else
+            {
+                builder.addSourceTree(new File(path));
+            }
         }
 
         builder.addListener(new XmlSerializerBindableMetadataBuilderListener());
         builder.buildAttributeMetadata();
+    }
+
+    private static boolean isClassName(String string)
+    {
+        try
+        {
+            Class.forName(string);
+        }
+        catch (ClassNotFoundException e)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
