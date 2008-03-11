e8641949b05042e73098eb52f86045612ea95dd0
Urszula Krukar <ukrukar@gmail.com>
2008-03-11 18:41:50 +0000

    Error log shown
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@2321 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- workbench/org.carrot2.workbench.core/src/org/carrot2/workbench/core/Perspective.java
+++ workbench/org.carrot2.workbench.core/src/org/carrot2/workbench/core/Perspective.java
@@ -9,13 +9,13 @@ public class Perspective implements IPerspectiveFactory
     public void createInitialLayout(IPageLayout layout)
     {
         // TODO: This code does not seem to be invoked (ever). What gives?!
-        layout.addStandaloneView(SearchView.ID, false, IPageLayout.TOP, 0.2f, 
-            layout.getEditorArea());
+        layout.addStandaloneView(SearchView.ID, false, IPageLayout.TOP, 0.2f, layout
+            .getEditorArea());
         layout.getViewLayout(SearchView.ID).setCloseable(false);
         layout.setEditorAreaVisible(true);
 
         // Display LogView for debugging.
-        // layout.addStandaloneView("org.eclipse.pde.runtime.LogView", true, IPageLayout.LEFT, .2f, 
-        //    layout.getEditorArea());
+        layout.addStandaloneView("org.eclipse.pde.runtime.LogView", true,
+            IPageLayout.BOTTOM, .2f, layout.getEditorArea());
     }
 }
