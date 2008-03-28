419c792ff5756648052bd9b403f596dcb4c58a2f
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2008-03-28 19:17:41 +0000

    carrot2-util-common: a utility for calculating "tick" values for sliders.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@2375 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ core/carrot2-util-common/src-test/org/carrot2/util/RangeUtilsTest.java
@@ -0,0 +1,100 @@
+package org.carrot2.util;
+
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Test cases for {@link RangeUtils}.
+ */
+public class RangeUtilsTest
+{
+    @Test
+    public void testIntZeroRange()
+    {
+        checkIntRange(10, 10, 1, 1);
+    }
+
+    @Test
+    public void testIntLessThan11Range()
+    {
+        checkIntRange(1, 5, 1, 1);
+        checkIntRange(0, 10, 1, 1);
+    }
+
+    @Test
+    public void testInt11To29Range()
+    {
+        checkIntRange(0, 11, 5, 5);
+        checkIntRange(0, 29, 5, 5);
+    }
+
+    @Test
+    public void testInt30To59Range()
+    {
+        checkIntRange(0, 30, 10, 5);
+        checkIntRange(0, 59, 10, 5);
+    }
+
+    @Test
+    public void testInt60To100Range()
+    {
+        checkIntRange(0, 60, 20, 10);
+        checkIntRange(0, 100, 20, 10);
+    }
+
+    @Test
+    public void testDoubleZeroRange()
+    {
+        checkDoubleRange(10.0, 10.0, 0.2, 0.1);
+    }
+
+    @Test
+    public void testDoubleLessThan1Range()
+    {
+        checkDoubleRange(0, 0.5, 0.2, 0.1);
+        checkDoubleRange(0, 1.0, 0.2, 0.1);
+    }
+
+    @Test
+    public void testDouble1To3Range()
+    {
+        checkDoubleRange(0, 1.5, 0.5, 0.25);
+        checkDoubleRange(0, 2.9, 0.5, 0.25);
+    }
+
+    @Test
+    public void testDouble3To6Range()
+    {
+        checkDoubleRange(0, 3.0, 1, 0.5);
+        checkDoubleRange(0, 5.9, 1, 0.5);
+    }
+
+    @Test
+    public void testDouble6To10Range()
+    {
+        checkDoubleRange(0, 6, 2, 1);
+        checkDoubleRange(0, 9.9, 2, 1);
+    }
+
+    private void checkIntRange(int min, int max, int expectedMajor, int expectedMinor)
+    {
+        assertEquals(expectedMajor, RangeUtils.getIntMajorTicks(min, max));
+        assertEquals(expectedMinor, RangeUtils.getIntMinorTicks(min, max));
+
+        // Check reverse
+        assertEquals(expectedMajor, RangeUtils.getIntMajorTicks(max, min));
+        assertEquals(expectedMinor, RangeUtils.getIntMinorTicks(max, min));
+    }
+
+    private void checkDoubleRange(double min, double max, double expectedMajor,
+        double expectedMinor)
+    {
+        assertEquals(expectedMajor, RangeUtils.getDoubleMajorTicks(min, max));
+        assertEquals(expectedMinor, RangeUtils.getDoubleMinorTicks(min, max));
+
+        // Check reverse
+        assertEquals(expectedMajor, RangeUtils.getDoubleMajorTicks(max, min));
+        assertEquals(expectedMinor, RangeUtils.getDoubleMinorTicks(max, min));
+    }
+}
