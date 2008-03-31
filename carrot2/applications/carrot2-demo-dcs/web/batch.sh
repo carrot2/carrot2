bbd2dbb6809a40c3e48d81de50c40bb67e0edaa0
Dawid Weiss <dawid.weiss@carrotsearch.com>
2008-03-31 09:50:41 +0000

    Added script-relative directory paths.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@2380 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- carrot2/applications/carrot2-demo-dcs/web/batch.sh
+++ carrot2/applications/carrot2-demo-dcs/web/batch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DEPLIB=WEB-INF/lib
+APPBASE=`dirname $0`
+DEPLIB=$APPBASE/WEB-INF/lib
 OPTS="-Xms128m -Xmx384m -XX:NewRatio=1"
 
-java $OPTS -jar ${DEPLIB}/carrot2-launcher.jar -cp WEB-INF/classes -cpdir ${DEPLIB} org.carrot2.dcs.cli.BatchApp $@
+java $OPTS -jar ${DEPLIB}/carrot2-launcher.jar -cp $APPBASE/WEB-INF/classes -cpdir ${DEPLIB} org.carrot2.dcs.cli.BatchApp $@
