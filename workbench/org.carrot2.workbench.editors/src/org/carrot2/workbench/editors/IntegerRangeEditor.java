224cbcefb2c1a6a58f046bb5473f19bbcf0ede3f
Urszula Krukar <ukrukar@gmail.com>
2008-04-15 20:17:18 +0000

    added spinner to IntegerRangeEditor
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@2484 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- workbench/org.carrot2.workbench.editors/src/org/carrot2/workbench/editors/IntegerRangeEditor.java
+++ workbench/org.carrot2.workbench.editors/src/org/carrot2/workbench/editors/IntegerRangeEditor.java
@@ -8,14 +8,16 @@ import org.carrot2.util.attribute.constraint.IntRange;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Scale;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
 
 public class IntegerRangeEditor extends AttributeEditorAdapter implements
     IAttributeEditor
 {
     private IntRange constraint;
     private Scale scale;
+    private Spinner spinner;
 
     @Override
     public void init(AttributeDescriptor descriptor)
@@ -33,19 +35,58 @@ public class IntegerRangeEditor extends AttributeEditorAdapter implements
     @Override
     public void createEditor(Composite parent, Object layoutData)
     {
-        scale = new Scale(parent, SWT.HORIZONTAL);
+        Composite holder = new Composite(parent, SWT.NULL);
+        holder.setLayoutData(layoutData);
+        GridLayout gl = new GridLayout(2, false);
+        gl.marginWidth = 0;
+        gl.marginHeight = 0;
+        holder.setLayout(gl);
+
+        createScale(holder);
+        GridData gd1 = new GridData();
+        gd1.horizontalAlignment = SWT.FILL;
+        gd1.grabExcessHorizontalSpace = true;
+        scale.setLayoutData(gd1);
+
+        createSpinner(holder);
+        GridData gd2 = new GridData();
+        gd2.minimumWidth = 30;
+        spinner.setLayoutData(gd2);
+    }
+
+    private void createSpinner(Composite holder)
+    {
+        spinner = new Spinner(holder, SWT.BORDER);
+        spinner.setMinimum(constraint.min());
+        spinner.setMaximum(constraint.max());
+        spinner.setIncrement(scale.getIncrement());
+        spinner.setPageIncrement(scale.getPageIncrement());
+        spinner.addSelectionListener(new SelectionAdapter()
+        {
+            @Override
+            public void widgetSelected(SelectionEvent e)
+            {
+                scale.setSelection(spinner.getSelection());
+                doEvent();
+            }
+        });
+    }
+
+    private void createScale(Composite holder)
+    {
+        scale = new Scale(holder, SWT.HORIZONTAL);
         scale.setMinimum(constraint.min());
         scale.setMaximum(constraint.max());
         scale.setIncrement(RangeUtils
             .getIntMinorTicks(constraint.min(), constraint.max()));
         scale.setPageIncrement(RangeUtils.getIntMajorTicks(constraint.min(), constraint
             .max()));
-        scale.setLayoutData(layoutData);
         scale.addSelectionListener(new SelectionAdapter()
         {
             @Override
             public void widgetSelected(SelectionEvent e)
             {
+                spinner.setSelection(scale.getSelection());
                 doEvent();
             }
         });
