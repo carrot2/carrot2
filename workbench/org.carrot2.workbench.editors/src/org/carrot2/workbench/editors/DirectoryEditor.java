5c938803eb820299f825065d5b719b60f2a4a58a
Urszula Krukar <ukrukar@gmail.com>
2008-04-21 18:09:04 +0000

    directory editor finished
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@2498 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- workbench/org.carrot2.workbench.editors/src/org/carrot2/workbench/editors/DirectoryEditor.java
+++ workbench/org.carrot2.workbench.editors/src/org/carrot2/workbench/editors/DirectoryEditor.java
@@ -1,7 +1,5 @@
 package org.carrot2.workbench.editors;
 
-import static org.apache.commons.lang.StringUtils.abbreviate;
-
 import java.io.File;
 
 import org.eclipse.swt.SWT;
@@ -14,29 +12,64 @@ public class DirectoryEditor extends AttributeEditorAdapter
 {
 
     private Image folderImage;
+    private Image clearImage;
     private Text pathText;
+    private File directory = null;
 
     @Override
     public void createEditor(Composite parent, Object layoutData)
     {
         Composite holder = new Composite(parent, SWT.NONE);
         holder.setLayoutData(layoutData);
-        GridLayout gl = new GridLayout(2, false);
+        GridLayout gl = new GridLayout(3, false);
         gl.marginHeight = 0;
         gl.marginWidth = 0;
+        gl.horizontalSpacing = 0;
         holder.setLayout(gl);
 
         pathText = new Text(holder, SWT.READ_ONLY | SWT.BORDER | SWT.SINGLE);
-        pathText.setTabs(50);
         GridData gd1 = new GridData();
         gd1.horizontalAlignment = SWT.FILL;
+        gd1.verticalAlignment = SWT.FILL;
         gd1.grabExcessHorizontalSpace = true;
         pathText.setLayoutData(gd1);
 
-        Button dialogButton = new Button(holder, SWT.PUSH | SWT.CENTER);
+        createDialogButton(holder);
+
+        createClearButton(holder);
+
+    }
+
+    private void createClearButton(Composite holder)
+    {
+        Button clearButton = new Button(holder, SWT.PUSH | SWT.CENTER | SWT.FLAT);
+        clearImage =
+            EditorsPlugin.getImageDescriptor("icons/delete_edit.gif").createImage();
+        clearButton.setImage(clearImage);
+        GridData gd2 = new GridData();
+        gd2.horizontalAlignment = SWT.FILL;
+        gd2.verticalAlignment = SWT.FILL;
+        clearButton.setLayoutData(gd2);
+
+        clearButton.addListener(SWT.Selection, new Listener()
+        {
+            public void handleEvent(Event event)
+            {
+                setCurrentDir(null);
+                doEvent();
+            }
+        });
+    }
+
+    private void createDialogButton(Composite holder)
+    {
+        Button dialogButton = new Button(holder, SWT.PUSH | SWT.CENTER | SWT.FLAT);
         folderImage = EditorsPlugin.getImageDescriptor("icons/folder.gif").createImage();
         dialogButton.setImage(folderImage);
-        dialogButton.setLayoutData(new GridData());
+        GridData gd2 = new GridData();
+        gd2.horizontalAlignment = SWT.FILL;
+        gd2.verticalAlignment = SWT.FILL;
+        dialogButton.setLayoutData(gd2);
 
         dialogButton.addListener(SWT.Selection, new Listener()
         {
@@ -49,22 +82,55 @@ public class DirectoryEditor extends AttributeEditorAdapter
                         DirectoryDialog dialog =
                             new DirectoryDialog(Display.getDefault().getActiveShell());
                         String newPath = dialog.open();
-                        pathText.setText(abbreviate(newPath, newPath.length() - 37, 40));
+                        if (newPath != null)
+                        {
+                            setCurrentDir(new File(newPath));
+                            doEvent();
+                        }
                     }
                 });
             }
         });
+    }
 
+    //TODO: add subclass with method doEvent :/
+    private void doEvent()
+    {
+        AttributeChangeEvent event = new AttributeChangeEvent(this);
+        fireAttributeChange(event);
     }
 
     @Override
     public void setValue(Object currentValue)
     {
-        if (currentValue != null)
+        setCurrentDir((File) currentValue);
+    }
+
+    @Override
+    public Object getValue()
+    {
+        return directory;
+    }
+
+    @Override
+    public void dispose()
+    {
+        folderImage.dispose();
+        clearImage.dispose();
+    }
+
+    private void setCurrentDir(File currentDir)
+    {
+        if (currentDir != null)
         {
-            File f = (File) currentValue;
-            pathText.setText(abbreviate(f.getAbsolutePath(),
-                f.getAbsolutePath().length() - 40, 40));
+            pathText.setText(currentDir.getName());
+            pathText.setToolTipText(currentDir.getAbsolutePath());
         }
+        else
+        {
+            pathText.setText("");
+            pathText.setToolTipText("");
+        }
+        directory = currentDir;
     }
 }
