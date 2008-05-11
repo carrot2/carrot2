22b5c0860da47b0b779c12f477e4e04f5546bc0f
Urszula Krukar <ukrukar@gmail.com>
2008-05-11 09:48:33 +0000

    - CARROT-229 done - SashFormEditorPart and ResultsEditor merged into one
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@2579 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- workbench/org.carrot2.workbench.core/src/org/carrot2/workbench/core/ui/ResultsEditor.java
+++ workbench/org.carrot2.workbench.core/src/org/carrot2/workbench/core/ui/ResultsEditor.java
@@ -1,5 +1,6 @@
 package org.carrot2.workbench.core.ui;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.carrot2.core.ProcessingResult;
 import org.carrot2.core.attribute.AttributeNames;
 import org.carrot2.workbench.core.helpers.Utils;
@@ -10,12 +11,18 @@ import org.carrot2.workbench.core.ui.clusters.ClusterTreeComponent;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.*;
-import org.eclipse.ui.forms.widgets.Section;
+import org.eclipse.ui.forms.widgets.*;
+import org.eclipse.ui.part.EditorPart;
 
-public class ResultsEditor extends SashFormEditorPart
+public class ResultsEditor extends EditorPart
 {
     public static final String ID = "org.carrot2.workbench.core.editors.results";
 
@@ -33,10 +40,43 @@ public class ResultsEditor extends SashFormEditorPart
         1, 3, 3
     };
 
+    private FormToolkit toolkit;
+
+    private Form rootForm;
+
+    private SashForm sashForm;
+
+    private IMemento state;
+
     @Override
-    protected int [] createControls(Composite parent)
+    public void createPartControl(Composite parent)
     {
+        toolkit = new FormToolkit(parent.getDisplay());
         sourceImage = getEditorInput().getImageDescriptor().createImage();
+        rootForm = toolkit.createForm(parent);
+        rootForm.setText(getPartName());
+        rootForm.setImage(getTitleImage());
+        toolkit.decorateFormHeading(rootForm);
+        sashForm = new SashForm(rootForm.getBody(), SWT.HORIZONTAL);
+        toolkit.adapt(sashForm);
+        int [] weights = createControls(sashForm);
+        int [] storedWeights = restoreWeightsFromState();
+        if (storedWeights == null)
+        {
+            sashForm.setWeights(weights);
+        }
+        else
+        {
+            sashForm.setWeights(weights);
+        }
+        GridLayout layout = GridLayoutFactory.swtDefaults().create();
+        rootForm.getBody().setLayout(layout);
+        sashForm.SASH_WIDTH = 5;
+        sashForm.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+    }
+
+    private int [] createControls(Composite parent)
+    {
         final ProcessingJob job =
             new ProcessingJob("Processing of a query",
                 (SearchParameters) getEditorInput());
@@ -44,9 +84,9 @@ public class ResultsEditor extends SashFormEditorPart
         {
             IProcessingResultPart part = parts[i];
             Section sec =
-                getToolkit().createSection(parent, Section.EXPANDED | Section.TITLE_BAR);
+                toolkit.createSection(parent, Section.EXPANDED | Section.TITLE_BAR);
             sec.setText(part.getPartName());
-            part.init(getSite(), sec, getToolkit(), job);
+            part.init(getSite(), sec, toolkit, job);
             sec.setClient(part.getControl());
         }
         job.addJobChangeListener(new JobChangeAdapter()
@@ -54,13 +94,13 @@ public class ResultsEditor extends SashFormEditorPart
             @Override
             public void aboutToRun(IJobChangeEvent event)
             {
-                getForm().setBusy(true);
+                setBusy(true);
             }
 
             @Override
             public void done(IJobChangeEvent event)
             {
-                getForm().setBusy(false);
+                setBusy(false);
                 if (job.getResult().isOK())
                 {
                     currentContent = ((ProcessingStatus) job.getResult()).result;
@@ -68,6 +108,8 @@ public class ResultsEditor extends SashFormEditorPart
                     {
                         public void run()
                         {
+                            //in case 'query' attribute is Output attribute (xml source e.g.)
+                            rootForm.setText(getPartName());
                             ResultsEditor.this.firePropertyChange(CURRENT_CONTENT);
                         }
                     });
@@ -79,34 +121,24 @@ public class ResultsEditor extends SashFormEditorPart
         return weights;
     }
 
+    private void setBusy(final boolean busy)
+    {
+        Utils.asyncExec(new Runnable()
+        {
+            public void run()
+            {
+                rootForm.setBusy(busy);
+            }
+        });
+    }
+
     @Override
     public void init(IEditorSite site, IEditorInput input) throws PartInitException
     {
         if (!(input instanceof SearchParameters)) throw new PartInitException(
             "Invalid Input: Must be SearchParameters");
-        super.init(site, input);
-    }
-
-    /*
-     * 
-     */
-    public void doSave(IProgressMonitor monitor)
-    {
-    }
-
-    /*
-     * 
-     */
-    public void doSaveAs()
-    {
-    }
-
-    /*
-     * 
-     */
-    public boolean isSaveAsAllowed()
-    {
-        return false;
+        setSite(site);
+        setInput(input);
     }
 
     /*
@@ -146,17 +178,13 @@ public class ResultsEditor extends SashFormEditorPart
     @Override
     public void setFocus()
     {
-    }
-
-    @Override
-    public boolean isDirty()
-    {
-        return false;
+        rootForm.setFocus();
     }
 
     @Override
     public void dispose()
     {
+        toolkit.dispose();
         sourceImage.dispose();
         for (int i = 0; i < parts.length; i++)
         {
@@ -169,5 +197,67 @@ public class ResultsEditor extends SashFormEditorPart
     public ProcessingResult getCurrentContent()
     {
         return currentContent;
+    }
+
+    private int [] restoreWeightsFromState()
+    {
+        if (state == null)
+        {
+            return null;
+        }
+        int weightsAmount = state.getInteger("weights-amount");
+        if (weightsAmount != sashForm.getChildren().length)
+        {
+            return null;
+        }
+        int [] weights = new int [0];
+        for (int i = 0; i < weightsAmount; i++)
+        {
+            ArrayUtils.add(weights, state.getInteger("w" + i));
+        }
+        return weights;
+    }
+
+    public void saveState(IMemento memento)
+    {
+        memento.putInteger("weights-amount", this.sashForm.getWeights().length);
+        for (int i = 0; i < this.sashForm.getWeights().length; i++)
+        {
+            int weight = this.sashForm.getWeights()[i];
+            memento.putInteger("w" + i, weight);
+        }
+    }
+
+    public void restoreState(IMemento memento)
+    {
+        state = memento;
+    }
+
+    /*
+     * 
+     */
+    public void doSave(IProgressMonitor monitor)
+    {
+    }
+
+    /*
+     * 
+     */
+    public void doSaveAs()
+    {
+    }
+
+    /*
+     * 
+     */
+    public boolean isSaveAsAllowed()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isDirty()
+    {
+        return false;
     }
 }
