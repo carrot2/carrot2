c03046e0430909a83a274ca67f5280fafccfe83a
Dawid Weiss <dawid.weiss@carrotsearch.com>
2008-06-29 09:57:32 +0000

    Inlining code that logically belongs to startup class.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@2682 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- workbench/org.carrot2.workbench.core/src/org/carrot2/workbench/core/WorkbenchCorePlugin.java
+++ workbench/org.carrot2.workbench.core/src/org/carrot2/workbench/core/WorkbenchCorePlugin.java
@@ -1,10 +1,17 @@
 package org.carrot2.workbench.core;
 
 import java.util.HashMap;
+import java.util.Properties;
 
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.carrot2.core.CachingController;
+import org.carrot2.core.Controller;
 import org.carrot2.core.DocumentSource;
-import org.carrot2.workbench.core.helpers.VelocityHelper;
+import org.carrot2.workbench.core.helpers.Utils;
+import org.eclipse.core.commands.operations.OperationStatus;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -33,9 +40,9 @@ public class WorkbenchCorePlugin extends AbstractUIPlugin
     public void start(BundleContext context) throws Exception
     {
         super.start(context);
-
         plugin = this;
-        VelocityHelper.init();
+
+        initVelocityEngine();
 
         controller = new CachingController(DocumentSource.class);
         controller.init(new HashMap<String, Object>());
@@ -79,5 +86,32 @@ public class WorkbenchCorePlugin extends AbstractUIPlugin
     public static ImageDescriptor getImageDescriptor(String path)
     {
         return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    }
+
+    /**
+     * Initialize Velocity engine.
+     */
+    private static void initVelocityEngine()
+    {
+        final Properties p = new Properties();
+        p.setProperty("resource.loader", "class");
+        p.setProperty("class.resource.loader.class", ClasspathResourceLoader.class
+            .getName());
+
+        // Disable separate Velocity logging.
+        p.setProperty(RuntimeConstants.RUNTIME_LOG, "");
+
+        try
+        {
+            Velocity.init(p);
+        }
+        catch (Exception e)
+        {
+            final IStatus status = new OperationStatus(IStatus.ERROR,
+                WorkbenchCorePlugin.PLUGIN_ID, -2,
+                "Error while initiating Velocity engine", e);
+            WorkbenchCorePlugin.getDefault().getLog().log(status);
+            Utils.showError(status);
+        }
     }
 }
