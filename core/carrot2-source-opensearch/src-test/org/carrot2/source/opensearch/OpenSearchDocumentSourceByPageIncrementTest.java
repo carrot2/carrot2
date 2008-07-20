96639c85d4068d637375b87f78ed60ea383b0c93
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2008-07-20 17:18:17 +0000

    carrot2-source-opensearch: CARROT-290: a test case for IceRocket, rome-fetcher downgraded
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@2769 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ core/carrot2-source-opensearch/src-test/org/carrot2/source/opensearch/OpenSearchDocumentSourceByPageIncrementTest.java
@@ -0,0 +1,48 @@
+package org.carrot2.source.opensearch;
+
+import org.carrot2.core.test.QueryableDocumentSourceTestBase;
+
+/**
+ * Test cases for {@link OpenSearchDocumentSource} with feeds where start result index is
+ * specified.
+ */
+public class OpenSearchDocumentSourceByPageIncrementTest extends
+    QueryableDocumentSourceTestBase<OpenSearchDocumentSource>
+{
+    @Override
+    public Class<OpenSearchDocumentSource> getComponentClass()
+    {
+        return OpenSearchDocumentSource.class;
+    }
+
+    @Override
+    protected int getLargeQuerySize()
+    {
+        return 120;
+    }
+
+    @Override
+    protected int getSmallQuerySize()
+    {
+        return 30;
+    }
+
+    @Override
+    protected boolean hasTotalResultsEstimate()
+    {
+        return false;
+    }
+
+    @Override
+    public void prepareComponent()
+    {
+        super.prepareComponent();
+
+        final String base = OpenSearchDocumentSource.class.getName();
+        initAttributes
+            .put(base + ".feedUrlTemplate",
+                "http://blogs.icerocket.com/search?q=${searchTerms}&rss=1&os=1&p=${startPage}&n=${count}&tab=blog");
+        initAttributes.put(base + ".resultsPerPage", 50);
+        initAttributes.put(base + ".maximumResults", 200);
+    }
+}
