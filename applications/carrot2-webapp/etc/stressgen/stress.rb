9641896790b52c71fe29dba8fb98733730f332f2
Dawid Weiss <dawid.weiss@carrotsearch.com>
2008-11-03 17:57:53 +0000

    http://issues.carrot2.org/browse/CARROT-413
    Scripts for re-deployment and stress testing of the webapp.
    
    
    
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@3083 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- applications/carrot2-webapp/etc/stressgen/stress.rb
+++ applications/carrot2-webapp/etc/stressgen/stress.rb
@@ -26,6 +26,14 @@ OptionParser.new do |opts|
     opts.on("-m N", "--mps N", Float, "Maximum requests per second (total). No limit if zero.") do |mps|
         options[:mps] = mps.to_i
     end
+    
+    opts.on("-q N", "--queries N", Float, "Number of queries to issue. No limit if zero.") do |queries|
+        options[:queries] = queries.to_i
+    end
+
+    opts.on("-u URI", "--uri URI", String, "URI prefix.") do |uri|
+        options[:uri] = uri
+    end
 
     opts.on("-h", "--help", "Show this message") do
       puts opts
@@ -39,15 +47,9 @@ end.parse!
 #
 # Set to a large value if no limits.
 #
-options[:mps] = 10000 if options[:mps] == 0 
-
-#
-# Base URI pattern.
-#
-host = 'http://localhost:8080';
-uri = '/search'
-qstring = 'source=#{source}&view=tree&skin=fancy-compact&results=100&algorithm=#{algorithm}&query=#{query}&type=CLUSTERS'
-$base_uri = host + uri + "?" + qstring
+options[:mps] = 100000 if options[:mps] == 0
+options[:queries] = 100000 unless options.has_key? :queries and options[:queries] > 0
+options[:uri] = 'http://localhost:8080' unless options.has_key? :uri
 
 #
 # Sources and algorithms for a given version of the engine
@@ -55,26 +57,29 @@ $base_uri = host + uri + "?" + qstring
 
 CARROT2_30 = {
 	:sources => ["web", "boss-web", "boss-images", "yahoo-news", "msn-web", "indeed", "pubmed", "icerocket"],
-	:algorithms => ["lingo", "stc", "url", "source"]
+	:algorithms => ["lingo", "stc", "url", "source"],
+    :uri_pattern => '/search?source=#{source}&view=tree&skin=fancy-compact&results=100&algorithm=#{algorithm}&query=#{query}&type=CLUSTERS'
 }
 
 #
 # Sources and algorithms for this run.
 #
 
-$sources = CARROT2_30[:sources]
-$algorithms = CARROT2_30[:algorithms]
+engine = CARROT2_30
+@sources = engine[:sources]
+@algorithms = engine[:algorithms]
+@base_uri = options[:uri] + engine[:uri_pattern]
 
 #
 # Queries are read from standard input, each query in a single line.
 #
-$queries = []
+@queries = []
 $stdin.each_line do |line|
 	q = line.chop
 	next if q.empty?
-	$queries << q
+	@queries << q
 end
-$stderr.puts "Read: #{$queries.length} queries"
+$stderr.puts "Read: #{@queries.length} queries"
 
 #
 # Take random element from an array
@@ -88,21 +93,27 @@ end
 #
 threads = []
 
+@queries_left = options[:queries]
 @waitroom = []
 @mutex = Mutex.new
 @queue = []
 
 (1..options[:threads]).each do |t|
     threads << Thread.new do
-        while true
+        while @queries_left > 0
             waiting = false
             @mutex.synchronize do
+                if @queries_left <= 0
+                    break
+                end
+
                 entry = { :time => Time.now, :thread => Thread.current }
                 if @queue.size + 1 > options[:mps]
                     waiting = true
                     @waitroom << entry
                 else
                     @queue << entry
+                    @queries_left = @queries_left - 1
                 end
             end
 
@@ -111,15 +122,18 @@ threads = []
                 next
             end
 
-            query = rand_elem($queries)
+            query = rand_elem(@queries)
             query = URI.encode(query)
 
-            source = rand_elem($sources)
-            algorithm = rand_elem($algorithms)
+            source = rand_elem(@sources)
+            algorithm = rand_elem(@algorithms)
 
-            uri = eval('"' << $base_uri << '"')
+            uri = eval('"' << @base_uri << '"')
             begin
                 res = Net::HTTP.get_response(URI.parse(uri))
+                if res.code != '200' 
+                    raise "HTTP error: #{res.code}, #{res.message}"
+                end
                 $stdout.print '.'
                 $stdout.flush
             rescue Exception => e
@@ -134,7 +148,7 @@ end
 # Main loop. Control the mps.
 #
 Thread.current.priority = 2
-while true
+while @queries_left > 0
     @mutex.synchronize do
         deadline = Time.now - 1
         while (not @queue.empty?) and @queue[0][:time] < deadline
---
+++ applications/carrot2-webapp/etc/stressgen/stress_redeploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Source webapp archive
+SOURCE_WAR=/home/dweiss/carrot2/carrot2.sf.proto-3.0/tmp/webapp/stable.war
+
+# Where to copy it for automatic deployment
+# TODO: we could use tomcat's deploy script here, would take shorter to refresh.
+DEPLOY_WAR=/home/dweiss/Applications/apache-tomcat-6.0.18/webapps/stable.war
+
+# Test URI to check if the application is available.
+TEST_URI=http://localhost:8080/stable/
+
+
+#
+# Loop deploy/short stress/undeploy
+#
+while true
+do
+    # undeploy
+    echo -n "U"
+    rm $DEPLOY_WAR
+    while `wget -O /dev/null -q $TEST_URI`
+    do
+        echo -n "."
+        sleep 1
+    done
+    
+    # deploy
+    echo -n "D"
+    cp $SOURCE_WAR $DEPLOY_WAR
+    while ! `wget -O /dev/null -q $TEST_URI`
+    do
+        echo -n "."
+        sleep 1
+    done
+
+    #
+    # Put some stress on the application. Change as needed. By default
+    # 40 queries are executed at random from the 'queries' file.
+    #
+    echo -n "S"
+    cat queries | ./stress.rb -t 5 -m 5 -u $TEST_URI --queries 40 2 > /dev/null    
+done
+
+
