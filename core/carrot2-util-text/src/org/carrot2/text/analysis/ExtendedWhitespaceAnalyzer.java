680e9a646d3127d592ddf326625d5d7a3e5050d0
Dawid Weiss <dawid.weiss@carrotsearch.com>
2008-11-06 09:21:03 +0000

    http://issues.carrot2.org/browse/CARROT-414
    Fix for Lucene's Analyzer using ThreadLocal. Should have no real impact on performance.
    
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@3094 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- core/carrot2-util-text/src/org/carrot2/text/analysis/ExtendedWhitespaceAnalyzer.java
+++ core/carrot2-util-text/src/org/carrot2/text/analysis/ExtendedWhitespaceAnalyzer.java
@@ -1,12 +1,9 @@
 package org.carrot2.text.analysis;
 
-import java.io.IOException;
 import java.io.Reader;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.Tokenizer;
-import org.carrot2.util.ExceptionUtils;
 
 /**
  * An {@link Analyzer} instance tokenizing using {@link ExtendedWhitespaceTokenizer}.
@@ -18,24 +15,13 @@ public final class ExtendedWhitespaceAnalyzer extends Analyzer
      */
     public TokenStream reusableTokenStream(String field, final Reader reader)
     {
-        Tokenizer tokenizer = (Tokenizer) getPreviousTokenStream();
-        if (tokenizer == null)
-        {
-            tokenizer = (Tokenizer) tokenStream(field, reader);
-            setPreviousTokenStream(tokenizer);
-        }
-        else
-        {
-            try
-            {
-                tokenizer.reset(reader);
-            }
-            catch (IOException e)
-            {
-                throw ExceptionUtils.wrapAsRuntimeException(e);
-            }
-        }
-        return tokenizer;
+        /*
+         * Avoid using ThreadLocal in Analyzer so that the context class loader's
+         * reference is not stored in the thread.
+         * 
+         * http://issues.carrot2.org/browse/CARROT-414
+         */
+        return tokenStream(field, reader);
     }
 
     /*
