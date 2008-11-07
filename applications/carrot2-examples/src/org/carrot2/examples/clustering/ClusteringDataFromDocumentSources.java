3b289c9c8823e7463189155c664824d32f7c2086
Dawid Weiss <dawid.weiss@carrotsearch.com>
2008-11-07 15:31:06 +0000

    License header updates, part I
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/branches/3.0-proto@3104 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- applications/carrot2-examples/src/org/carrot2/examples/clustering/ClusteringDataFromDocumentSources.java
+++ applications/carrot2-examples/src/org/carrot2/examples/clustering/ClusteringDataFromDocumentSources.java
@@ -1,6 +1,16 @@
-/**
+
+/*
+ * Carrot2 project.
  *
+ * Copyright (C) 2002-2008, Dawid Weiss, Stanisław Osiński.
+ * Portions (C) Contributors listed in "carrot2.CONTRIBUTORS" file.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the repository checkout or at:
+ * http://www.carrot2.org/carrot2.LICENSE
  */
+
 package org.carrot2.examples.clustering;
 
 import java.util.HashMap;
