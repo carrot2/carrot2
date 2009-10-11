fe25e8ccd4fd09f6681009239280a312043c366c
Dawid Weiss <dawid.weiss@carrotsearch.com>
2009-10-11 20:59:58 +0000

    http://issues.carrot2.org/browse/CARROT-568 .NET HTTP upload rewritten.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@3752 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- applications/carrot2-dcs/examples/csharp/src/MultipartFileUpload.cs
+++ applications/carrot2-dcs/examples/csharp/src/MultipartFileUpload.cs
@@ -1,252 +1,181 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Collections.Specialized;
 using System.Net;
 using System.IO;
 
 namespace Org.Carrot2
 {
-    //
-    // This class based on an example from:
-    // http://blog.mindbridge.com/?p=59
-    //
-    // With modifications.
-    //
+    /// <summary>
+    /// Stream data for the form.
+    /// </summary>
+    internal sealed class StreamData
+    {
+        internal Stream stream;
+        internal string streamName;
+
+        internal StreamData(string name, Stream stream)
+        {
+            this.stream = stream;
+            this.streamName = name;
+        }
+    }
+
+    /// <summary>
+    /// A simplified HTTP POST <code>multipart/form-data</code> uploader. 
+    /// </summary>
     public class MultipartFileUpload
     {
-        private Uri uri = null;
+        private static readonly uint BUFFER_SIZE = 1024 * 8;
 
-        private NameValueCollection parameters = null;
-        private NameValueCollection formData = null;
+        private Uri uri;
+        private IList<KeyValuePair<string, object>> formData 
+            = new List<KeyValuePair<string, object>>();
 
-        private String fileDisplayName = null;
-        private String fileParameterName = null;
-
-        private String boundary;
-
-        private FileInfo file = null;
-        private NetworkCredential networkCredential = null;
-        private readonly int BUFFER_SIZE = 500;
-
-        public MultipartFileUpload()
+        public MultipartFileUpload(Uri target)
         {
-            parameters = new NameValueCollection();
-            formData = new NameValueCollection();
+            this.uri = target;
         }
 
-        public MultipartFileUpload(Uri uri)
-            : this()
-        {
-            this.uri = uri;
-        }
-
-        #region getters and setters
         /// <summary>
-        /// the destination of the multipart file upload
+        /// Reset the object for reuse.
         /// </summary>
-        public Uri Uri
+        public void Reset()
         {
-            get { return this.uri; }
-            set { this.uri = value; }
+            formData.Clear();
         }
 
         /// <summary>
-        /// the parameters to be passed along with the post method — optional
-        /// </summary>
-        public NameValueCollection Parameters
-        {
-            get { return this.parameters; }
-            set { this.parameters = value; }
-        }
-
-        /// <summary>
-        /// any form data associated with the post method
-        /// </summary>
-        public NameValueCollection FormData
-        {
-            get { return this.formData; }
-            set { this.formData = value; }
-        }
-
-        /// <summary>
-        /// if your web server requires credentials to upload a file, set them here — optional
-        /// </summary>
-        public NetworkCredential NetworkCredential
-        {
-            get { return this.networkCredential; }
-            set { this.networkCredential = value; }
-        }
-
-        #endregion
-
-        #region public methods
-
-        /// <summary>
-        /// adds a parameter to the request, if the parameter already has a value,
-        /// it will be overwritten
-        /// </summary>
-        /// <param name="name">name of the parameter</param>
-        /// <param name="value">value of the parameter</param>
-        public void AddParameter(String name, String value)
-        {
-            if (this.parameters == null)
-            {
-                this.parameters = new NameValueCollection();
-            }
-            this.parameters.Set(name, value);
-        }
-
-        /// <summary>
-        /// adds a form value to the request, if the form field already exists, overwrite it
+        /// Adds a form value to the request, if the form field already exists, overwrite it.
         /// </summary>
         /// <param name="name">the name of the form field</param>
         /// <param name="value">value of the form field</param>
         public void AddFormValue(String name, String value)
         {
-            if (this.formData == null)
-            {
-                this.formData = new NameValueCollection();
-            }
-            this.formData.Set(name, value);
+            AddFormValueInternal(name, (object) value);
         }
 
         /// <summary>
-        /// attach a file to the post method, the parameter name and file can not be null
+        /// Adds stream data to the request's form data.
         /// </summary>
-        /// <param name="fileDisplayName">the name of the file as it should appear to the web server</param>
-        /// <param name="parameterName">the parameter that your web server expects to be associated with a file</param>
-        /// <param name="file">the actual file you want to upload</param>
-        /// <exception cref="ArgumentNullException">file can not be null, name of the parameter can’t be null</exception>
-        public void AttachFile(String fileDisplayName, String parameterName, FileInfo file)
+        /// <param name="name">the name of the form field</param>
+        /// <param name="value">string or StreamData object.</param>
+        private void AddFormValueInternal(String name, object value)
         {
-            if (file == null)
+            this.formData.Add(new KeyValuePair<string,object>(name, value));
+        }
+
+        /// <summary>
+        /// Attach a stream (file) to the post method, the parameter name and file can not be null.
+        /// </summary>
+        /// <param name="parameterName">the parameter that your web server expects to be associated with a file</param>
+        /// <param name="fileDisplayName">the name of the file as it should appear to the web server</param>
+        /// <param name="stream">the actual content of the file you want to upload</param>
+        /// <exception cref="ArgumentNullException">file can not be null, name of the parameter can’t be null</exception>
+        public void AddFormStream(String parameterName, String fileDisplayName, Stream stream)
+        {
+            if (stream == null || !stream.CanRead)
             {
-                throw new ArgumentNullException("file", "You must pass a reference to a file");
+                throw new ArgumentNullException("stream", "You must pass a reference to a readable stream");
             }
+
             if (parameterName == null)
             {
                 throw new ArgumentNullException("parameterName", "You must provide the name of the file parameter.");
             }
 
-            this.file = file;
-            this.fileParameterName = parameterName;
-            this.fileDisplayName = fileDisplayName;
+            AddFormValueInternal(parameterName, new StreamData(fileDisplayName, stream));
         }
 
         /// <summary>
-        /// performs the actual upload
+        /// Performs the actual upload.
         /// </summary>
         /// <returns>the response as a byte array</returns>
-        public byte[] UploadFileEx()
+        public byte[] Post()
         {
-            // generate boundary
-            boundary = "-----------boundary" + DateTime.Now.Ticks.ToString("x");
+            // generate parameter boundary
+            string boundaryRaw = "boundary" + DateTime.Now.Ticks.ToString("x");
+            string boundary = "--" + boundaryRaw;
 
-            // Tack on any parameters or just give us back the uri if there are no parameters
-            Uri targetUri = CreateUriWithParameters();
-            
-            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(targetUri);
-            webrequest.Credentials = networkCredential; //fine if it’s null
-            webrequest.ContentType = "multipart/form-data; boundary=" + boundary;
+            HttpWebRequest webrequest = (HttpWebRequest) WebRequest.Create(uri);
+            webrequest.ContentType = "multipart/form-data; boundary=" + boundaryRaw;
             webrequest.Method = "POST";
+            webrequest.KeepAlive = false;
 
-            // Encode form parameters
-            String postHeader = CreatePostDataString();
-            byte[] postHeaderBytes = Encoding.UTF8.GetBytes(postHeader);
-            byte[] endBoundaryBytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "--");
-
-            // Read in the file as a stream, if it exists.
-            long length = postHeaderBytes.Length;
-            FileStream fileStream = null;
-            if (file != null)
+            // Encode form parameters and push them to the stream.
+            using (Stream requestStream = webrequest.GetRequestStream())
             {
-                fileStream = file.Open(FileMode.Open, FileAccess.Read);
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int bytesRead = 0;
+                byte[] bytes;
 
-                length += fileStream.Length + endBoundaryBytes.Length;
-            }
-
-            webrequest.ContentLength = length;
-
-            Stream requestStream = webrequest.GetRequestStream();
-            requestStream.Write(postHeaderBytes, 0, postHeaderBytes.Length);
-
-            // Write out the file contents
-            byte[] buffer = new byte[BUFFER_SIZE];
-            int bytesRead = 0;
-            if (fileStream != null)
-            {
-                while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+                foreach (KeyValuePair<string, object> kv in formData)
                 {
-                    requestStream.Write(buffer, 0, bytesRead);
+                    string key = kv.Key;
+                    object value = kv.Value;
+
+                    if (value is string)
+                    {
+                        string part =
+                            boundary + "\r\n"
+                            + "content-disposition: form-data; name=\""
+                            + key + "\"\r\n\r\n"
+                            + (value as string)
+                            + "\r\n";
+
+                        bytes = Encoding.UTF8.GetBytes(part);
+                        requestStream.Write(bytes, 0, bytes.Length);
+                    }
+                    else if (value is StreamData)
+                    {
+                        StreamData sd = value as StreamData;
+                        string part =
+                            boundary + "\r\n"
+                            + "content-disposition: form-data; name=\""
+                            + key + "\"; filename=\"" + sd.streamName + "\"\r\n"
+                            + "content-type: application/octet-stream\r\n\r\n";
+
+                        bytes = Encoding.UTF8.GetBytes(part);
+                        requestStream.Write(bytes, 0, bytes.Length);
+
+                        // Copy stream contents.
+                        using (Stream s = sd.stream)
+                        {
+                            while ((bytesRead = s.Read(buffer, 0, buffer.Length)) != 0)
+                            {
+                                requestStream.Write(buffer, 0, bytesRead);
+                            }
+                        }
+
+                        requestStream.WriteByte((byte) '\r');
+                        requestStream.WriteByte((byte) '\n');
+                    }
+                    else
+                    {
+                        throw new Exception("Panic: object of unknown type: " + value);
+                    }
                 }
-                requestStream.Write(endBoundaryBytes, 0, endBoundaryBytes.Length);
-                fileStream.Close();
+
+                bytes = Encoding.UTF8.GetBytes(boundary + "--\r\n");
+                requestStream.Write(bytes, 0, bytes.Length);
+                requestStream.Close();
+
+                // Copy the response to a byte array in memory (so that we don't need
+                // to track web connection resources after returning).
+                WebResponse response = webrequest.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                MemoryStream memStream = new MemoryStream();
+                while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    memStream.Write(buffer, 0, bytesRead);
+                }
+
+                responseStream.Close();
+                response.Close();
+
+                Reset();
+                return memStream.ToArray();
             }
-
-            WebResponse response = webrequest.GetResponse();
-            Stream responseStream = response.GetResponseStream();
-            MemoryStream memStream = new MemoryStream();
-            while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) != 0)
-            {
-                memStream.Write(buffer, 0, bytesRead);
-            }
-
-            responseStream.Close();
-            response.Close();
-
-            return memStream.ToArray();
         }
-
-        #endregion
-
-        #region private methods
-
-        /// <summary>
-        /// helper method to tack on parameters to the request
-        /// </summary>
-        /// <returns>Uri with parameters, or the original uri if it’s null</returns>
-        private Uri CreateUriWithParameters()
-        {
-            if (uri == null) return null;
-            if (parameters == null || parameters.Count <= 0)
-            {
-                return this.uri;
-            }
-            String paramString = "?";
-            foreach (String key in parameters.Keys)
-            {
-                paramString += key + "=" + parameters.Get(key) + "&";
-            }
-            paramString = paramString.Substring(0, paramString.Length - 1); //strip off last &
-            return new Uri(uri.ToString() + paramString);
-        }
-
-        /// <summary>
-        /// post data as a string with the boundaries
-        /// </summary>
-        /// <returns>a string representing the form data</returns>
-        private String CreatePostDataString()
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < formData.Count; i++)
-            {
-                sb.Append("--" + boundary + "\r\n");
-                sb.Append("Content-Disposition: form-data; name=\"");
-                sb.Append(formData.GetKey(i) + "\"\r\n\r\n" + formData.Get(i) + "\r\n");
-            }
-
-            if (file != null)
-            {
-                sb.Append("--" + boundary + "\r\n");
-                sb.Append("Content-Disposition: form-data; name=\"" + fileParameterName + "\"; ");
-                sb.Append("filename=\"" + fileDisplayName + "\"\r\n");
-                sb.Append("Content-Type: application/octet-stream\r\n\r\n");
-            }
-
-            return sb.ToString();
-        }
-
-        #endregion
     }
 }
