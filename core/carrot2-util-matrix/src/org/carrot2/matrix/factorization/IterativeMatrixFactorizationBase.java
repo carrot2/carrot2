7aa2008cdb7162cdf409fe06c4b91b94498fa95e
Dawid Weiss <dawid.weiss@carrotsearch.com>
2010-01-14 21:19:15 +0000

    Debug stuff won't be useful -- it's a JVM bug: http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6435614
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@3898 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- core/carrot2-util-matrix/src/org/carrot2/matrix/factorization/IterativeMatrixFactorizationBase.java
+++ core/carrot2-util-matrix/src/org/carrot2/matrix/factorization/IterativeMatrixFactorizationBase.java
@@ -1,3 +1,4 @@
+
 /*
  * Carrot2 project.
  *
@@ -14,9 +15,7 @@ package org.carrot2.matrix.factorization;
 import org.carrot2.matrix.NNIDoubleFactory2D;
 import org.carrot2.matrix.factorization.seeding.RandomSeedingStrategy;
 import org.carrot2.matrix.factorization.seeding.ISeedingStrategy;
-import org.slf4j.LoggerFactory;
 
-import org.apache.mahout.math.Arrays;
 import org.apache.mahout.math.matrix.DoubleMatrix2D;
 import org.apache.mahout.math.matrix.doublealgo.Sorting;
 import org.apache.mahout.math.matrix.linalg.Algebra;
@@ -147,34 +146,10 @@ abstract class IterativeMatrixFactorizationBase extends MatrixFactorizationBase 
         // Need to make a copy of aggregates because they get sorted as well
         double [] aggregatesCopy = aggregates.clone();
 
-        try
-        {
-            V = NNIDoubleFactory2D.asNNIMatrix(Sorting.quickSort.sort(VT, aggregates)
-                .viewDice());
-        }
-        catch (ArrayIndexOutOfBoundsException e)
-        {
-            System.out.println("Aggregates1");
-            System.out.println(Arrays.toString(aggregates));
-            System.out.println("Matrix1");
-            System.out.println(VT);
-            throw new RuntimeException("Aggregates1: " + Arrays.toString(aggregates)
-                + "\nMatrix1: " + VT.toString());
-        }
-        try
-        {
-            U = NNIDoubleFactory2D.asNNIMatrix(Sorting.quickSort.sort(U.viewDice(),
-                aggregatesCopy).viewDice());
-        }
-        catch (ArrayIndexOutOfBoundsException e)
-        {
-            System.out.println("Aggregates2");
-            System.out.println(Arrays.toString(aggregatesCopy));
-            System.out.println("Matrix2");
-            System.out.println(U.viewDice());
-            throw new RuntimeException("Aggregates2: " + Arrays.toString(aggregatesCopy)
-                + "\nMatrix2: " + U.viewDice().toString());
-        }
+        V = NNIDoubleFactory2D.asNNIMatrix(Sorting.quickSort.sort(VT, aggregates)
+            .viewDice());
+        U = NNIDoubleFactory2D.asNNIMatrix(Sorting.quickSort.sort(U.viewDice(),
+            aggregatesCopy).viewDice());
 
         // Revert back to positive values of aggregates
         for (int i = 0; i < aggregates.length; i++)
