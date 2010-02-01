e39c18bcd159aac634b75e33c34b44b382927e1e
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2010-02-01 09:48:33 +0000

    carrot2-core: CARROT-625: fixed. When creating and initialized instance of a component, we need to set both @Init and @Processing attribute values loaded from the attribute set.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@3951 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- core/carrot2-core/src/org/carrot2/core/ProcessingComponentDescriptor.java
+++ core/carrot2-core/src/org/carrot2/core/ProcessingComponentDescriptor.java
@@ -218,7 +218,7 @@ public class ProcessingComponentDescriptor
         try
         {
             AttributeBinder
-                .bind(instance, initAttributes, false, Input.class, Init.class);
+                .bind(instance, initAttributes, false, Input.class);
 
             if (init)
             {
