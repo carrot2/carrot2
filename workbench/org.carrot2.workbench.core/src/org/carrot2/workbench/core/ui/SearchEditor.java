f96c7accd8e7d7a9624c3a6b30ddfb9f04a9a2c9
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2010-02-22 11:37:17 +0000

    workbench: a possible NPE: http://issues.carrot2.org/browse/CARROT-639
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@4046 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- workbench/org.carrot2.workbench.core/src/org/carrot2/workbench/core/ui/SearchEditor.java
+++ workbench/org.carrot2.workbench.core/src/org/carrot2/workbench/core/ui/SearchEditor.java
@@ -747,6 +747,7 @@ public final class SearchEditor extends EditorPart implements IPersistableEditor
         final Shell shell = this.getEditorSite().getShell();
         if (new SearchEditorSaveAsDialog(shell, newOptions).open() == Window.OK)
         {
+            this.saveOptions = newOptions;
             doSave(saveOptions);
         }
         this.saveOptions = newOptions;
