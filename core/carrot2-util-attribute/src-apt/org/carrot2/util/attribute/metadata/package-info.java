3af5d71889bc98b44007f93aee3aead72266be1b
Dawid Weiss <dawid.weiss@carrotsearch.com>
2010-08-13 11:50:05 +0000

    Extracted apt path define script, split paths for apt processor so that it is not part of the product jar (interferes with compilation).
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@4423 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ core/carrot2-util-attribute/src-apt/org/carrot2/util/attribute/metadata/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains an APT processor for extracting and generating {@link org.carrot2.util.attribute.metadata.BindableMetadata}
+ * metadata. The code is split into two source folders (src and src-apt) to allow
+ * separation between production code (src) and apt-generator code (src-apt).  
+ */
+package org.carrot2.util.attribute.metadata;
+
