81a12e090878d7954a73fab5534f56d351ddc09f
Dawid Weiss <dawid.weiss@carrotsearch.com>
2011-03-09 10:27:42 +0000

    Raw post data example updated (content-length removed, CURL will calculate it properly, something was wrong with the input file, don't know what.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@4827 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- applications/carrot2-dcs/examples/curl/clustering-with-raw-post.cmd
+++ applications/carrot2-dcs/examples/curl/clustering-with-raw-post.cmd
@@ -3,8 +3,7 @@ set DCS_URL=http://localhost:8080/dcs/rest
 set RESULT=clusters-from-raw-post.xml
 
 curl %DCS_URL% -# ^
-     --data-binary @clustering-with-raw-post-data.txt ^
-     -H "Content-Type: multipart/form-data; boundary=---------------------------191691572411478" ^
-     -H "Content-Length: 44389" ^
+     --data-binary @clustering-with-raw-post-data.bin ^
+     -H "Content-Type: multipart/form-data; boundary=----------------------------f223859fc094" ^
      -o %RESULT%
 echo Results saved to %RESULT%
