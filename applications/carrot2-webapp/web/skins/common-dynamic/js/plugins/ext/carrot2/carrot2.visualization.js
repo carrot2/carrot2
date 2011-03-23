019594ae6be6cab35a16d554d992800724a88d95
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2011-03-23 18:26:31 +0000

    carrot2-webapp: CARROT-744: upgrading Circles and FoamTree, using the new events for more effective document selection changing
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@4864 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- applications/carrot2-webapp/web/skins/common-dynamic/js/plugins/ext/carrot2/carrot2.visualization.js
+++ applications/carrot2-webapp/web/skins/common-dynamic/js/plugins/ext/carrot2/carrot2.visualization.js
@@ -9,7 +9,7 @@
       var flashvars = {
           startup_data_URL: $.visualization.dataUrl,
 
-          callback_onGroupSelection: "onGroupSelection",
+          callback_onSelectionChanged: "onGroupSelection",
           callback_onDocumentSelection: "onDocumentSelection",
           openDocumentsOnClick: true,
 
@@ -48,45 +48,13 @@
   });
 })(jQuery);
 
-/**
- * An array of selected cluster IDs in the visualization SWF.
- */
-var selectedClusters = [];
-
-/**
- * A cache of document identifiers for each cluster ID, as given by
- * the visualization. The same data is created in <code>carrot2.clusters.core.js</code>,
- * so perhaps we could reuse it?
- */
-var flattenedDocuments0 = {};
-
 // Callback function invoked by the visualization:
 // a group has been selected or deselected.
-function onGroupSelection(clusterId, isSelected, docList) {
-  var i  = jQuery.inArray(clusterId, selectedClusters);
-
-  // If re-selected, re-push at the top of the stack.
-  if (i != -1) selectedClusters.splice(i, 1);
-  if (isSelected) selectedClusters.unshift(clusterId);
-
-  if (selectedClusters.length == 0) {
+function onGroupSelection(clusterIds, documentIds) {
+  if (clusterIds.length == 0) {
     $("#clusters-panel").trigger("carrot2-clusters-selected-top");
   } else {
-    /*
-     * TODO: selectedClusters contains identifiers of clusters to be shown.
-     * one could either merge their documents, or display one cluster after
-     * another in the documents panel (my pick).
-     */
-    flattenedDocuments0[clusterId] = docList.split(",");
-
-    var cluster = selectedClusters[0];
-    var documentIndexes = flattenedDocuments0[cluster];
-    $("#clusters-panel").trigger("carrot2-clusters-selected", [ cluster, documentIndexes ]);
+    // For the time being, we're showing only the first selected cluster.
+    $("#clusters-panel").trigger("carrot2-clusters-selected", [ clusterIds[0], documentIds[0] ]);
   }
-}
-
-// Callback function invoked by the visualization:
-// selection has been cleared
-function selectionCleared() {
-  $("#clusters-panel").trigger("carrot2-clusters-selected-top");
 }
