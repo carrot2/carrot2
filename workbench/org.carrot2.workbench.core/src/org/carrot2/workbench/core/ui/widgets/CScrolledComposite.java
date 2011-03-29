9f7725d01a65214e645a6e611107121cd2954f78
Dawid Weiss <dawid.weiss@carrotsearch.com>
2011-03-29 09:27:42 +0000

    Removing custom scrollbar fix (apparently corrected in new SWT?).
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@4881 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- workbench/org.carrot2.workbench.core/src/org/carrot2/workbench/core/ui/widgets/CScrolledComposite.java
+++ workbench/org.carrot2.workbench.core/src/org/carrot2/workbench/core/ui/widgets/CScrolledComposite.java
@@ -12,9 +12,7 @@
 
 package org.carrot2.workbench.core.ui.widgets;
 
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.ScrollBar;
 import org.eclipse.ui.forms.widgets.SharedScrolledComposite;
 
 /**
@@ -28,22 +26,4 @@ public final class CScrolledComposite extends SharedScrolledComposite
         super(parent, style);
         this.setDelayedReflow(false);
     }
-
-    /**
-     * On reflow, update both vertical and horizontal scroller (for some reason the
-     * horizontal one is neglected in current version of Eclipse).
-     */
-    @Override
-    public void reflow(boolean flushCache)
-    {
-        super.reflow(flushCache);
-
-        final ScrollBar hbar = getHorizontalBar();
-        if (hbar != null)
-        {
-            final Rectangle clientArea = getClientArea();
-            final int increment = Math.max(clientArea.width - 5, 5);
-            hbar.setPageIncrement(increment);
-        }
-    }    
 }
