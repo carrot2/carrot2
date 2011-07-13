786825b80a999eadef5d2e6c6d956c829e4fd56d
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2011-07-13 08:43:47 +0000

    carrot2-util-text: exposing the identity stemmer so that it can be passed as an attribute to completely disable stemming.
    git-svn-id: file:///home/dweiss/carrot2/migration/carrot2.svn/trunk@4989 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- core/carrot2-util-text/src/org/carrot2/text/linguistic/IdentityStemmer.java
+++ core/carrot2-util-text/src/org/carrot2/text/linguistic/IdentityStemmer.java
@@ -16,7 +16,7 @@ package org.carrot2.text.linguistic;
  * An implementation of {@link IStemmer} that always returns <code>null</code> from
  * {@link #stem(CharSequence)}.
  */
-final class IdentityStemmer implements IStemmer
+public final class IdentityStemmer implements IStemmer
 {
     public CharSequence stem(CharSequence word)
     {
