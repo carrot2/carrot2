372bfd20a5cf1790c66da85ac21633e7601f3a6e
Dawid Weiss <dawid.weiss@carrotsearch.com>
2012-06-13 12:51:31 +0200

    CARROT-857: adding HTTP GET example.

--- applications/carrot2-dcs/examples/curl/clustering-from-document-source.cmd
+++ applications/carrot2-dcs/examples/curl/clustering-from-document-source.cmd
@@ -1,9 +1,14 @@
 @echo off
 set DCS_URL=http://localhost:8080/dcs/rest 
-set RESULT=clusters-from-document-source.xml 
 
+REM With HTTP POST
 curl %DCS_URL% -# ^
      -F "dcs.source=etools" ^
      -F "query=test" ^
-     -o %RESULT%
+     -o clusters-from-document-source-post.xml
+
+REM With HTTP GET
+curl "%DCS_URL%?dcs.source=etools&query=test" ^
+     -o clusters-from-document-source-get.xml
+
 echo Results saved to %RESULT%
--- applications/carrot2-dcs/examples/curl/clustering-from-document-source.sh
+++ applications/carrot2-dcs/examples/curl/clustering-from-document-source.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 DCS_HOST=http://localhost:8080/dcs/rest
-RESULT=clusters-from-document-source.xml
 
+# With HTTP POST
 curl $DCS_HOST -# \
      -F "dcs.source=etools" \
      -F "query=test" \
-     -o $RESULT
+     -o clusters-from-document-source-post.xml
+
+# With HTTP GET
+curl "${DCS_HOST}?dcs.source=etools&query=test" \
+     -o clusters-from-document-source-get.xml
+
 echo Results saved to $RESULT
