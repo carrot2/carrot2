d1e9e22bd1be8db94f07d64e1087f4a16c04d2fb
Dawid Weiss <dawid.weiss@carrotsearch.com>
2012-06-13 16:43:45 +0200

    CARROT-857: fixes for windows scripts.

--- applications/carrot2-dcs/examples/curl/clustering-from-document-source.cmd
+++ applications/carrot2-dcs/examples/curl/clustering-from-document-source.cmd
@@ -1,14 +1,16 @@
 @echo off
-set DCS_URL=http://localhost:8080/dcs/rest 
+set DCS_URL=http://localhost:8080/dcs/rest
 
 REM With HTTP POST
-curl %DCS_URL% -# ^
+curl -s http://localhost:8080/dcs/rest -# ^
      -F "dcs.source=etools" ^
      -F "query=test" ^
      -o clusters-from-document-source-post.xml
 
+echo Results saved to clusters-from-document-source-post.xml
+
 REM With HTTP GET
-curl "%DCS_URL%?dcs.source=etools&query=test" ^
+curl -s "http://localhost:8080/dcs/rest?dcs.source=etools&query=test" ^
      -o clusters-from-document-source-get.xml
 
-echo Results saved to %RESULT%
+echo Results saved to clusters-from-document-source-get.xml
--- applications/carrot2-dcs/examples/curl/clustering-from-local-file.cmd
+++ applications/carrot2-dcs/examples/curl/clustering-from-local-file.cmd
@@ -1,12 +1,14 @@
 @echo off
 set DCS_URL=http://localhost:8080/dcs/rest 
 
-curl %DCS_URL% -# ^
+curl -s %DCS_URL% -# ^
      -F "dcs.c2stream=@../shared/data-mining.xml" ^
      -o clusters-from-local-file-multipart.xml
 
-curl %DCS_URL% -# ^
+echo Results saved to clusters-from-local-file-multipart.xml
+
+curl -s %DCS_URL% -# ^
      --data-urlencode "dcs.c2stream@../shared/data-mining.xml" ^
      -o clusters-from-local-file-formencoded.xml
 
-echo Results saved to clusters-from-local-file-*
+echo Results saved to clusters-from-local-file-formencoded.xml
