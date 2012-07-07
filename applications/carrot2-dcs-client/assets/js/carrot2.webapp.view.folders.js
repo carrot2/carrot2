53267a8b050efc306a5c0a6e79b0010c49857e08
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2012-07-07 23:55:52 +0200

    Initial folder view.

--- applications/carrot2-dcs-client/assets/js/carrot2.webapp.view.folders.js
+++ applications/carrot2-dcs-client/assets/js/carrot2.webapp.view.folders.js
@@ -1,6 +1,19 @@
 (function($) {
   $.pluginhelper.make("foldersView", function(el, options, initialized) {
-    $(el).text("folders");
+    var $container = $(el);
+    var hasData = false;
+
+    // Templates
+    var folderTemplate = _.template(
+      "<li class='folded <%= clazz %>'>" +
+        "<a id='<%- cluster.id %>' href='#'>" +
+          "<span class='tree'></span>" +
+          "<span class='phrase'><%- cluster.phrases[0] %></span>" +
+          "<span class='size'>(<%- cluster.size %>)</span>" +
+        "</a>" +
+        "<%= clusters %>" +
+      "</li>");
+
 
     // Export public methods
     this.populate = populate;
@@ -16,13 +29,33 @@
     // Private methods
     //
     function populate(data) {
+      // Generate HTML
+      var html = "<a href='#'><span class='phrase'>All topics</span><span class='size'>(100)</span></a>" +
+        "<ul>" +
+          _.reduce(data.clusters, function reducer(html, cluster) {
+            html += folderTemplate({
+              cluster: cluster,
+              clazz: cluster.clusters ? "branch" : "",
+              clusters: cluster.clusters ? "<ul>" + _.reduce(cluster.clusters, reducer, "") + "</ul>" : ""
+            });
+            return html;
+          }, "") +
+        "</ul>";
+
+      $container.html(html).on("click", "a", function() {
+        $(this).parent().toggleClass("folded");
+      });
+
+      hasData = true;
     }
 
     function clear() {
+      $container.html("");
+      hasData = false;
     }
 
     function populated() {
-      return false;
+      return hasData;
     }
   });
 })(jQuery);
