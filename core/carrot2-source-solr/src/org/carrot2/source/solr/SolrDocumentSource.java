ae7247785684650dee51bcb39240d58d3200f820
Dawid Weiss <dawid.weiss@carrotsearch.com>
2013-03-29 18:44:32 +0100

    Revert "A take at CARROT-992 but will revert in a second; won't fly."
    This reverts commit 991d66e407feb209a3a9720b2909b9edc9b65cfb.

--- core/carrot2-source-solr/src/org/carrot2/source/solr/SolrDocumentSource.java
+++ core/carrot2-source-solr/src/org/carrot2/source/solr/SolrDocumentSource.java
@@ -100,18 +100,6 @@ public class SolrDocumentSource extends RemoteXmlSimpleSearchEngineBase
     }, strict = false)
     public IResource solrXsltAdapter = new ClassResource(SolrDocumentSource.class, "solr-to-c2.xsl");
 
-    /**
-     * If clusters are present in Solr's response they will be 
-     * parsed and exposed to components further down the processing chain.
-     */
-    @Input
-    @Init 
-    @Processing
-    @Attribute
-    @Label("Read clusters from Solr's response")
-    @Level(AttributeLevel.BASIC)
-    public boolean readClusters = false;
-
     @Override
     protected String buildServiceUrl()
     {
@@ -131,11 +119,9 @@ public class SolrDocumentSource extends RemoteXmlSimpleSearchEngineBase
     {
         final Map<String, String> parameters = Maps.newHashMap();
 
-        parameters.put("solr.title-field",   solrTitleFieldName);
+        parameters.put("solr.title-field", solrTitleFieldName);
         parameters.put("solr.summary-field", solrSummaryFieldName);
-        parameters.put("solr.url-field",     solrUrlFieldName);
-
-        parameters.put("solr.read-clusters", readClusters ? "true" : "false");
+        parameters.put("solr.url-field", solrUrlFieldName);
         
         return parameters;
     }
