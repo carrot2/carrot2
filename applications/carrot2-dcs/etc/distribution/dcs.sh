ac11a3d117e0e574074749733df3658c7a8b42ba
Dawid Weiss <dawid.weiss@carrotsearch.com>
2013-04-12 15:47:07 +0200

    Launcher script improvements.

--- applications/carrot2-dcs/etc/distribution/dcs.sh
+++ applications/carrot2-dcs/etc/distribution/dcs.sh
@@ -3,6 +3,5 @@
 #
 # Add extra JVM options to DCS_OPTS
 #
-DCS_OPTS="$DCS_OPTS -Xms64m -Xmx768m"
 
 java $DCS_OPTS -Ddcs.war=war/carrot2-dcs.war -jar invoker.jar -cpdir lib org.carrot2.dcs.DcsApp $@
