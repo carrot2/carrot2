67b12ba5399427e8d9c821c23562e7a9226db6ea
Dawid Weiss <dawid.weiss@carrotsearch.com>
2016-02-19 13:44:42 +0100

    Update (c) year.

--- core/carrot2-util-text/src/org/carrot2/text/linguistic/ILexicalData.java
+++ core/carrot2-util-text/src/org/carrot2/text/linguistic/ILexicalData.java
@@ -2,7 +2,7 @@
 /*
  * Carrot2 project.
  *
- * Copyright (C) 2002-2015, Dawid Weiss, Stanisław Osiński.
+ * Copyright (C) 2002-2016, Dawid Weiss, Stanisław Osiński.
  * All rights reserved.
  *
  * Refer to the full license file "carrot2.LICENSE"
