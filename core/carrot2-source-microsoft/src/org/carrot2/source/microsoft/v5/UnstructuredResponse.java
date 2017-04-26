da26795ff51133065410dd0d966b1a5f345eab2f
Dawid Weiss <dawid.weiss@carrotsearch.com>
2017-04-26 15:20:15 +0200

    Adding a missing import.

--- core/carrot2-source-microsoft/src/org/carrot2/source/microsoft/v5/UnstructuredResponse.java
+++ core/carrot2-source-microsoft/src/org/carrot2/source/microsoft/v5/UnstructuredResponse.java
@@ -12,6 +12,7 @@
 
 package org.carrot2.source.microsoft.v5;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
