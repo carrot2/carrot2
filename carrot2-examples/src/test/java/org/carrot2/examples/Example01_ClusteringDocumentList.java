5e1570624f2da72e8ce8e95d37f0ac1b4420254c
Dawid Weiss <dawid.weiss@carrotsearch.com>
2019-02-11 10:07:31 +0100

    Removed synthetic algorithms.

--- carrot2-examples/src/test/java/org/carrot2/examples/Example01_ClusteringDocumentList.java
+++ carrot2-examples/src/test/java/org/carrot2/examples/Example01_ClusteringDocumentList.java
@@ -18,7 +18,6 @@ import java.util.stream.Collectors;
 
 import org.carrot2.clustering.lingo.LingoClusteringAlgorithm;
 import org.carrot2.clustering.stc.STCClusteringAlgorithm;
-import org.carrot2.clustering.synthetic.ByUrlClusteringAlgorithm;
 import org.carrot2.core.Cluster;
 import org.carrot2.core.Controller;
 import org.carrot2.core.ControllerFactory;
@@ -62,46 +61,6 @@ public class Example01_ClusteringDocumentList
             "Outlines what knowledge discovery, the process of analyzing data from different perspectives and summarizing it into useful information, can do and how it works."
         },
     };
-
-    @Test
-    public void clusterByUrl()
-    {
-        /*
-         * <div>
-         * <p>
-         * The easiest way to get started with Carrot2 is to cluster a collection
-         * of {@link org.carrot2.core.Document}s. Each document can consist of:
-         * </p>
-         * 
-         * <ul>
-         * <li>document content: a query-in-context snippet, document abstract or full text,</li>
-         * <li>document title: optional, some clustering algorithms give more weight to document titles,</li>
-         * <li>document URL: optional, used by the {@link org.carrot2.clustering.synthetic.ByUrlClusteringAlgorithm}, 
-         * ignored by other algorithms.</li>
-         * </ul>
-         * 
-         * <p>
-         * To make the example short, the code shown below clusters only 5 documents. Use
-         * at least 20 to get reasonable clusters. If you have access to the query that generated
-         * the documents being clustered, you should also provide it to Carrot2 to get better clusters.
-         * </p>
-         * </div>
-         */
-
-        /* Prepare input documents */
-        final List<Document> documents = Arrays.stream(data)
-            .map(row -> new Document(row[1], row[2], row[0]))
-            .collect(Collectors.toList());
-
-        /* A controller to manage the processing pipeline. */
-        final Controller controller = ControllerFactory.createPooling();
-
-        /* Perform clustering by domain. In this case query is not useful, hence it is null. */
-        final List<Cluster> clustersByDomain =
-            controller.process(documents, null, ByUrlClusteringAlgorithm.class).getClusters();
-
-        ConsoleFormatter.displayClusters(clustersByDomain);
-    }
 
     @Test
     public void clusterWithLingoAlgorithm()
