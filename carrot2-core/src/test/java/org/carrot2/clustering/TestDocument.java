5ffe6ac00da229f4f7c6d270b9c295f902de5fcd
Dawid Weiss <dawid.weiss@carrotsearch.com>
2019-02-18 09:46:07 +0100

    Porting remaining tests.

--- carrot2-core/src/test/java/org/carrot2/clustering/TestDocument.java
+++ carrot2-core/src/test/java/org/carrot2/clustering/TestDocument.java
@@ -3,14 +3,25 @@ package org.carrot2.clustering;
 import java.util.function.BiConsumer;
 
 public class TestDocument implements Document {
-  public String title;
+  private String title;
+  private String snippet;
 
   public TestDocument(String title) {
     this.title = title;
   }
 
+  public TestDocument(String title, String snippet) {
+    this.title = title;
+    this.snippet = snippet;
+  }
+
   @Override
   public void visitFields(BiConsumer<String, String> fieldConsumer) {
-    fieldConsumer.accept("title", title);
+    if (title != null) {
+      fieldConsumer.accept("title", title);
+    }
+    if (snippet != null) {
+      fieldConsumer.accept("snippet", snippet);
+    }
   }
 }
--- carrot2-core/src/test/java/org/carrot2/clustering/stc/STCClusteringAlgorithmTest.java
+++ carrot2-core/src/test/java/org/carrot2/clustering/stc/STCClusteringAlgorithmTest.java
@@ -12,12 +12,130 @@
 
 package org.carrot2.clustering.stc;
 
-import org.carrot2.clustering.ClusteringAlgorithm;
-import org.carrot2.clustering.ClusteringAlgorithmTestBase;
+import org.assertj.core.api.Assertions;
+import org.carrot2.clustering.*;
+import org.carrot2.language.EnglishLanguageComponentsFactory;
+import org.carrot2.language.LanguageComponents;
+import org.junit.Test;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class STCClusteringAlgorithmTest extends ClusteringAlgorithmTestBase {
   @Override
-  protected ClusteringAlgorithm algorithm() {
+  protected STCClusteringAlgorithm algorithm() {
     return new STCClusteringAlgorithm();
+  }
+
+  // Clustering with a very high df threshold must not fail.
+  @Test
+  public void testClusteringWithDfThreshold() {
+    STCClusteringAlgorithm algorithm = algorithm();
+    algorithm.preprocessing.get().wordDfThreshold.set(20);
+
+    List<Cluster<Document>> clusters = algorithm.cluster(SampleDocumentData.DOCUMENTS_DATA_MINING.stream(),
+        LanguageComponents.get(EnglishLanguageComponentsFactory.NAME));
+    Assertions.assertThat(clusters.size()).isEqualTo(0);
+  }
+
+  @Test
+  public void testMaxClusters() {
+    STCClusteringAlgorithm algorithm = algorithm();
+    algorithm.maxClusters.set(9);
+
+    List<Cluster<Document>> clusters = algorithm.cluster(SampleDocumentData.DOCUMENTS_DATA_MINING.stream(),
+        LanguageComponents.get(EnglishLanguageComponentsFactory.NAME));
+
+    Assertions.assertThat(clusters.size())
+        .isEqualTo(9);
+  }
+
+  @Test
+  public void testComputeIntersection() {
+    int[] t1;
+
+    t1 = new int[]{0, 1, 2, 1, 2, 3};
+    Assertions.assertThat(STCClusteringAlgorithm.computeIntersection(t1, 0, 3, t1, 3, 3)).isEqualTo(2);
+
+    t1 = new int[]{0, 1, 2, 3, 5, 6};
+    Assertions.assertThat(STCClusteringAlgorithm.computeIntersection(t1, 0, 3, t1, 3, 3)).isEqualTo(0);
+
+    t1 = new int[]{0, 1, 2, -1, 2, 6};
+    Assertions.assertThat(STCClusteringAlgorithm.computeIntersection(t1, 0, 3, t1, 3, 3)).isEqualTo(1);
+
+    t1 = new int[]{0, 1, 2, 0};
+    Assertions.assertThat(STCClusteringAlgorithm.computeIntersection(t1, 0, 3, t1, 3, 1)).isEqualTo(1);
+  }
+
+  @Test
+  public void testMergingBaseClustersWithStemEquivalentPhrases() {
+    List<String> titles = Arrays.asList(
+        "good programs . foo1",
+        "foo2 good programs . foo2",
+        "good programs taste good",
+        "good programs are good",
+        "good programming . foo3",
+        "foo4 good programming . foo4",
+        "good programming makes you feel better");
+
+    STCClusteringAlgorithm algorithm = algorithm();
+    algorithm.minBaseClusterScore.set(0d);
+
+    Stream<TestDocument> documentStream = titles.stream().map(title -> new TestDocument(title));
+    List<Cluster<Document>> clusters = algorithm.cluster(documentStream,
+        LanguageComponents.get(EnglishLanguageComponentsFactory.NAME));
+
+    Assertions.assertThat(clusters.stream().flatMap(cluster -> cluster.getLabels().stream()))
+        .contains("Good Programs")
+        .doesNotContain("Good Programming");
+  }
+
+  /**
+   * CARROT-1008: STC is not using term stems.
+   */
+  @Test
+  public void testCarrot1008() throws Exception {
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    DocumentBuilder db = dbf.newDocumentBuilder();
+    Element root;
+    try (InputStream is = getClass().getResourceAsStream("CARROT-1008.xml")) {
+      root = db.parse(is).getDocumentElement();
+    }
+
+    Stream<TestDocument> docStream = elementStream(root, "document")
+        .map(docElement -> {
+          String title = elementStream(docElement, "title")
+              .map(e -> e.getTextContent()).collect(Collectors.joining(" . "));
+          String snippet = elementStream(docElement, "snippet")
+              .map(e -> e.getTextContent()).collect(Collectors.joining(" . "));
+          return new TestDocument(title, snippet);
+        });
+
+    STCClusteringAlgorithm algorithm = algorithm();
+    algorithm.maxClusters.set(30);
+
+    List<Cluster<Document>> clusters = algorithm.cluster(docStream,
+        LanguageComponents.get(EnglishLanguageComponentsFactory.NAME));
+
+    List<String> collect = clusters.stream().flatMap(c -> c.getLabels().stream())
+        .collect(Collectors.toList());
+    Assertions.assertThat(collect.contains("Guns") &&
+                          collect.contains("Gun")).isFalse();
+  }
+
+  private Stream<Element> elementStream(Element parent, String childName) {
+    NodeList list = parent.getElementsByTagName(childName);
+    return IntStream.range(0, list.getLength())
+        .mapToObj(i -> (Element) list.item(i));
   }
 }
