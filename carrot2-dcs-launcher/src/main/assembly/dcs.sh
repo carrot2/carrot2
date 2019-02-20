6e776ea5fc8c60a21a1537710187c1572fb0c4d7
Dawid Weiss <dawid.weiss@carrotsearch.com>
2019-02-20 11:16:59 +0100

    Removing dcs and examples temporarily.

--- carrot2-dcs-launcher/src/main/assembly/dcs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -z "$DCS_OPTS" ]; then
-  DCS_OPTS="-Xms64m -Xmx768m"
-fi
-
-java $DCS_OPTS -Ddcs.war=web/carrot2-dcs-service-@version@.war -jar lib/carrot2-dcs-launcher-@version@.jar $@
