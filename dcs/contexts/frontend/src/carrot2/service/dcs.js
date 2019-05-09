fdcb2c5df56859e5256703ff7f51d8a97a592734
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2019-05-09 07:30:29 +0200

    CARROT-1198: format, include subclusters in uniqueDocuments.

--- dcs/contexts/frontend/src/carrot2/service/dcs.js
+++ dcs/contexts/frontend/src/carrot2/service/dcs.js
@@ -4,41 +4,43 @@ export function fetchClusters(query, documents) {
   // Just pick the content fields we want to cluster. No IDs, URLs, or anything else.
   const fields = ['title', 'snippet'];
   const request = {
-      "documents": documents.map((doc) => {
-          return fields.reduce((obj, key) => {
-              return {
-                ...obj,
-                [key]: doc[key]
-              };
-          }, {});
-      })
+    "documents": documents.map((doc) => {
+      return fields.reduce((obj, key) => {
+        return {
+          ...obj,
+          [key]: doc[key]
+        };
+      }, {});
+    })
   };
 
   return fetch(config.dcsServiceUrl, {
     method: "POST",
     headers: {
-        "Content-Type": "application/json",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(request)
   }).then(function (response) {
     return response.json();
   }).then(function (json) {
-      let clusters = enrichClusters(json.clusters, "");
-      // console.log(JSON.stringify(clusters, null, "  "));
-      return clusters;
+    return enrichClusters(json.clusters, "");
   });
 
   // Assign unique IDs to clusters and compute additional information about
   // their deep size, etc. This is done in-place.
   function enrichClusters(clusters, prefix) {
-    var id = 0;
+    let id = 0;
     for (let cluster of clusters) {
       const subclusters = cluster.clusters || [];
       const documents = cluster.documents || [];
+
       enrichClusters(subclusters, prefix + id + ".");
-      cluster.uniqueDocuments = Array.from(new Set(subclusters.reduce((acc, val) => {
-        return ([...acc, ...val.documents]);
-      }, documents)));
+
+      cluster.uniqueDocuments = Array.from(subclusters.reduce(function fold(set, sc) {
+        (sc.subclusteres || []).reduce(set, fold);
+        for (const doc of sc.documents()) { set.add(doc); }
+        return set;
+      }, new Set(documents)));
       cluster.id = prefix + (id++);
       cluster.size = cluster.uniqueDocuments.length;
     }
