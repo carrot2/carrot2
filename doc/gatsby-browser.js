0e56f7fd73772d54ec3f4fdeb00c92516d33912e
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2019-08-01 09:43:48 +0200

    CARROT-1206: cloning APIdocs.

---
+++ doc/gatsby-browser.js
@@ -0,0 +1,1 @@
+import "./src/styles/custom.css";
---
+++ doc/gatsby-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  pathPrefix: "/doc",
+  siteMetadata: {
+    title: `Carrot2 docs`,
+    description: `User and developer manual for the Carrot2 text clustering engine.`,
+    lang: `en`,
+    indexAlias: `/getting-started/`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `@carrotsearch/gatsby-plugin-apidocs`,
+      options: {
+        navigation: `${__dirname}/src/navigation.json`,
+        logo:   `${__dirname}/src/logo.html`,
+        footer: `${__dirname}/src/footer.html`,
+        basePath: "src/content"
+      }
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#ffaa00`,
+        showSpinner: false
+      }
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-dark-mode`,
+    `@carrotsearch/gatsby-transformer-html`,
+    '@carrotsearch/gatsby-plugin-content-search',
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-catch-links`
+  ]
+};
