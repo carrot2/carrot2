df7e2cc251d375606ad23794b18d636b26696c3a
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2019-09-20 09:57:32 +0200

    CARROT-1206: use branding variable.

--- dcs/contexts/frontend/src/carrot2/config.js
+++ dcs/contexts/frontend/src/carrot2/config.js
@@ -2,5 +2,5 @@ export const dcsConfig = {
   dcsServiceUrl: process.env.REACT_APP_DCS_SERVICE_URL || "http://localhost:8080/service/cluster?template=frontend-default"
 };
 
-export const isCarrot2Distribution = () => false;//process.env.REACT_APP_DISTRIBUTION !== "lingo3g";
+export const isCarrot2Distribution = () => process.env.REACT_APP_DISTRIBUTION !== "lingo3g";
 
