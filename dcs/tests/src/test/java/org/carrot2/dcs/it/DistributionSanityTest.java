63f97f9458f19cb651a7d1a70c22d3b0daabff3c
Dawid Weiss <dawid.weiss@carrotsearch.com>
2019-09-26 11:19:49 +0200

    CARROT-1222: Line endings and access rights in dcs.sh

--- dcs/tests/src/test/java/org/carrot2/dcs/it/DistributionSanityTest.java
+++ dcs/tests/src/test/java/org/carrot2/dcs/it/DistributionSanityTest.java
@@ -13,8 +13,13 @@ package org.carrot2.dcs.it;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.PosixFilePermission;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
+import org.junit.AssumptionViolatedException;
 import org.junit.Test;
 
 public class DistributionSanityTest extends AbstractDistributionTest {
@@ -26,11 +31,31 @@ public class DistributionSanityTest extends AbstractDistributionTest {
 
   @Test
   public void checkShEols() throws IOException {
-    String content =
-        new String(
-            Files.readAllBytes(getDistributionDir().resolve("dcs.sh")), StandardCharsets.UTF_8);
-    if (content.indexOf('\r') >= 0) {
-      throw new AssertionError("dcs.sh contains carriage returns.");
+    try (Stream<Path> s = Files.walk(getDistributionDir())) {
+      for (Path p : s.collect(Collectors.toList())) {
+        if (p.getFileName().toString().endsWith(".sh")) {
+          String content = new String(Files.readAllBytes(p), StandardCharsets.UTF_8);
+          if (content.indexOf('\r') >= 0) {
+            throw new AssertionError(p + " contains carriage \r?");
+          }
+        }
+      }
+    }
+  }
+
+  @Test
+  public void checkFileMode() throws IOException {
+    try (Stream<Path> s = Files.walk(getDistributionDir())) {
+      for (Path p : s.collect(Collectors.toList())) {
+        if (p.getFileName().toString().endsWith(".sh")) {
+          try {
+            Assertions.assertThat(Files.getPosixFilePermissions(p))
+                .contains(PosixFilePermission.OWNER_EXECUTE);
+          } catch (UnsupportedOperationException e) {
+            throw new AssumptionViolatedException("Expected a posix-compliant file system: " + p, e);
+          }
+        }
+      }
     }
   }
 }
