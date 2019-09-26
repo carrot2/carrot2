9d178cf2672f8b3c8e2d3d0e080a48679baa6bab
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2019-09-26 06:59:38 +0200

    Revert back to the simpler version of service URL resolution.

--- dcs/contexts/frontend/src/carrot2/config.js
+++ dcs/contexts/frontend/src/carrot2/config.js
@@ -1,13 +1,4 @@
-export const dcsServiceUrl = () => {
-  const env = process.env.REACT_APP_DCS_SERVICE_URL || "auto";
-  if (env === "auto") {
-    const url = new URL(window.location);
-    const port = url.port.length > 0 ? `:${url.port}` : "";
-    return `${url.protocol}//${url.hostname}${port}/service/cluster?template=frontend-default` ;
-  } else {
-    return env;
-  }
-};
+export const dcsServiceUrl = () => new URL(process.env.REACT_APP_DCS_SERVICE_URL, window.location);
 
 export const isCarrot2Distribution = () => process.env.REACT_APP_DISTRIBUTION !== "lingo3g";
 
