4ec423ee0d8c513c7a71fe79d27682c5131a9385
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2019-09-27 10:16:05 +0200

    CARROT-1218: use localhost:8080 DCS in development.

--- dcs/contexts/frontend/src/carrot2/config.js
+++ dcs/contexts/frontend/src/carrot2/config.js
@@ -1,4 +1,7 @@
-export const dcsServiceUrl = () => new URL(process.env.REACT_APP_DCS_SERVICE_URL, window.location);
+export const dcsServiceUrl = () => {
+  const base = process.env.NODE_ENV === "production" ? window.location : "http://localhost:8080";
+  return new URL(process.env.REACT_APP_DCS_SERVICE_URL, base);
+};
 
 export const isCarrot2Distribution = () => process.env.REACT_APP_DISTRIBUTION !== "lingo3g";
 
