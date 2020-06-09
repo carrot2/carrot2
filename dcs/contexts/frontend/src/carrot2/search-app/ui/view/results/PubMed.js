a1a76bbc63c03fb4feebd4ede18cbb41ef6b4673
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2020-06-09 07:57:53 +0200

    Increasing the maximum PubMed results to 1000.

--- dcs/contexts/frontend/src/carrot2/search-app/ui/view/results/PubMed.js
+++ dcs/contexts/frontend/src/carrot2/search-app/ui/view/results/PubMed.js
@@ -118,7 +118,7 @@ export const PubMedSourceConfig = view((props) => {
   return (
     <div className="PubMedSourceConfig">
       <FormGroup inline={true} label="Max results" labelFor="pubmed-max-results">
-        <NumericInput id="pubmed-max-results" min={50} max={300} value={store.maxResults}
+        <NumericInput id="pubmed-max-results" min={50} max={1000} value={store.maxResults}
                       onValueChange={v => { store.maxResults = v; props.onChange(); } }
                       majorStepSize={100} stepSize={50} minorStepSize={10} />
       </FormGroup>
