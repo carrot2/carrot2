630d123da3d586493faa981ff7a8029c77ecbe76
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2020-11-23 10:19:40 +0100

    Remove useless code.

--- dcs/contexts/frontend/src/carrot2/apps/workbench/ui/WorkbenchApp.js
+++ dcs/contexts/frontend/src/carrot2/apps/workbench/ui/WorkbenchApp.js
@@ -2,21 +2,85 @@ import React from "react";
 
 import "./WorkbenchApp.css";
 
-import { routes } from "../../../routes.js";
+import { persistentStore } from "../../../../carrotsearch/store/persistent-store.js";
+import { view } from "@risingstack/react-easy-state";
+import { clusterStore, searchResultStore } from "../../../store/services.js";
+import { humanizeDuration } from "../../../../carrotsearch/lang/humanize.js";
+import { Stats } from "../../../../carrotsearch/ui/Stats.js";
+import { Views } from "../../../../carrotsearch/ui/Views.js";
+import { clusterViews, resultsViews } from "../../../config-views.js";
+import { Loading } from "../../../../carrotsearch/ui/Loading.js";
+import { sources } from "../../../config-sources.js";
+import { workbenchSourceStore } from "../store/source-store.js";
+import { WorkbenchSide } from "./WorkbenchSide.js";
 
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import { WorkbenchResults } from "./WorkbenchResults.js";
+const uiStore = persistentStore("workbench:ui", {
+  clusterView: "folders"
+});
+
+const ResultStats = view(() => {
+  const stats = [
+    {
+      id: "result-count",
+      value: searchResultStore.searchResult?.documents?.length,
+      label: "results"
+    },
+    {
+      id: "cluster-count",
+      value: clusterStore?.clusters?.length,
+      label: "clusters"
+    },
+    {
+      id: "clustered-docs",
+      value: (100.0 * clusterStore.getClusteredDocsRatio()).toFixed(1) + "%",
+      label: "clustered docs"
+    },
+    {
+      id: "processing-time",
+      value: humanizeDuration(clusterStore.serviceInfo?.clusteringTimeMillis),
+      label: "clustering time"
+    }
+  ];
+
+  return (
+      <div className="stats">
+        <Stats stats={stats} />
+      </div>
+  );
+});
+
+const WorkbenchMain = view(() => {
+  return (
+      <div className="WorkbenchMain">
+        <ResultStats />
+        <div className="clusters">
+          <Views
+              activeView={uiStore.clusterView}
+              views={clusterViews}
+              onViewChange={newView => (uiStore.clusterView = newView)}
+          >
+            <Loading store={clusterStore} />
+          </Views>
+        </div>
+        <div className="docs">
+          <Views
+              views={resultsViews}
+              activeView="list"
+              onViewChange={() => {}}
+              source={sources[workbenchSourceStore.source]}
+          >
+            <Loading store={searchResultStore} />
+          </Views>
+        </div>
+      </div>
+  );
+});
 
 export const WorkbenchApp = () => {
   return (
-    <React.Fragment>
       <div className="WorkbenchApp">
-        <Router>
-          <Switch>
-            <Route path={routes.workbench.path} component={WorkbenchResults} />
-          </Switch>
-        </Router>
+        <WorkbenchSide />
+        <WorkbenchMain />
       </div>
-    </React.Fragment>
   );
 };
