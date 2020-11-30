8a282ae48f2e6f793bdae0545692c6961e675c99
Dawid Weiss <dawid.weiss@carrotsearch.com>
2020-11-30 14:01:37 +0100

    Add missing file.

---
+++ core/src/main/java/org/carrot2/language/WordFilter.java
@@ -0,0 +1,24 @@
+/*
+ * Carrot2 project.
+ *
+ * Copyright (C) 2002-2020, Dawid Weiss, Stanisław Osiński.
+ * All rights reserved.
+ *
+ * Refer to the full license file "carrot2.LICENSE"
+ * in the root folder of the repository checkout or at:
+ * https://www.carrot2.org/carrot2.LICENSE
+ */
+package org.carrot2.language;
+
+/**
+ * A stop word filter.
+ *
+ * @see LexicalData
+ * @see Dictionaries
+ * @since 4.1.0
+ */
+@FunctionalInterface
+public interface WordFilter {
+  /** @return Return true if the provided term should be ignored in processing. */
+  boolean ignoreWord(CharSequence word);
+}
