654c3113f5e1edcb9b31bc08dd05b5c73fe76561
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2020-12-30 11:07:43 +0100

    An example customizer for sending anonymous stats to Plausible Analytics.

---
+++ dcs/contexts/frontend/customizers/plausible-analytics.js
@@ -0,0 +1,22 @@
+const head = document.getElementsByTagName("head")[0];
+const script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "https://plausible.io/js/plausible.js";
+script.dataset.domain = "search.carrot2.org";
+head.appendChild(script);
+
+export default app => {
+  app.on("clusteringRequested", e => {
+    const plausible = window["plausible"];
+    if (plausible) {
+      plausible("ClusteringRequested", {
+        props: {
+          source: e.source,
+          docs: Math.ceil(e.docs / 50) * 50, // bucket with the resolution of 50
+          app: e.app,
+          view: e.view
+        }
+      });
+    }
+  });
+};
