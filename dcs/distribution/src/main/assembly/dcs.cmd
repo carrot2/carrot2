5ce9799b1a4095ffb2d8144c41712a6487034894
Dawid Weiss <dawid.weiss@carrotsearch.com>
2021-12-03 11:48:26 +0100

    #142 Improve scripts to emit incompatible JVM information. Omit illegal-access option on JDK >= 17.

--- dcs/distribution/src/main/assembly/dcs.cmd
+++ dcs/distribution/src/main/assembly/dcs.cmd
@@ -22,15 +22,35 @@ IF NOT "%DCS_OPTS%"=="" GOTO optsSet
 SET DCS_OPTS=
 :optsSet
 
+REM Determine JVM version and set any extra JVM options accordingly.
+FOR /F "usebackq delims=" %%i IN (`java -jar "%%DCS_HOME%%\lib\progresso-jvmversion-1.7.3.jar"`) DO SET JVMVERSION=%%i
+
+IF "%JVMVERSION%"=="1.7"  GOTO jvmUnsupported
+IF "%JVMVERSION%"=="1.8"  GOTO jvmUnsupported
+IF "%JVMVERSION%"=="9"    GOTO jvmUnsupported
+IF "%JVMVERSION%"=="10"   GOTO jvmUnsupported
+
+SET JVMARGS=
+IF /I "%JVMVERSION%" GEQ "16"   GOTO jdk16OrLater
+SET JVMARGS=--illegal-access=deny
+:jdk16OrLater
+
+REM Set script name for help.
+SET SCRIPT_NAME=%~n0
+
 REM Use JAVA_CMD, if provided.
 IF NOT "%JAVA_CMD%"=="" GOTO javaSet
 SET JAVA_CMD=java
 :javaSet
 
 REM Launch DCS.
-%JAVA_CMD% --illegal-access=deny %JVMARGS% %DCS_OPTS% -jar "%DCS_HOME%\lib\dcs-launcher-${product.version}.jar" %*
+%JAVA_CMD% %JVMARGS% %DCS_OPTS% -jar "%DCS_HOME%\lib\dcs-launcher-${product.version}.jar" %*
 SET DCS_EXITVAL=%errorlevel%
 
 REM Set cmd's window title and return with the exit code.
 TITLE %comspec%
 exit /b %DCS_EXITVAL%
+
+:jvmUnsupported
+ECHO ^> [ERROR] ${product.name} requires at least Java 11 (%JVMVERSION% detected).
+exit /b 1
